db430a00acdb5931664269f3da468e04fc5e9258
Alex Sorafumo <alex@yuion.net>
2020-09-17 14:05:18 +1000

    chore(kiosk): add base project files for kiosk

--- projects/kiosk/src/app/app.component.spec.ts
+++ projects/kiosk/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'template-uis'`, () => {
+  it(`should have as title 'kiosk'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('template-uis');
+    expect(app.title).toEqual('kiosk');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('template-uis app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('kiosk app is running!');
   });
 });
