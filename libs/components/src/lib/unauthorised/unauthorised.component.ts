df4348883da3e11063ab48cf475a9f1da8e3f857
Alex Sorafumo <alex@yuion.net>
2020-10-19 15:48:27 +1100

    chore: minor tweaks

--- libs/components/src/lib/unauthorised/unauthorised.component.ts
+++ libs/components/src/lib/unauthorised/unauthorised.component.ts
@@ -1,39 +1,43 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-unauthorised',
-  template: `
-    <div class="unauthorised">
-        <h1>403</h1>
-        <h3 i18n="@@forbiddenLabel">Access forbidden.</h3>
-        <p i18n="@@invalidPermissionLabel">You do not have permission to view this page and your access attempt has been recorded.</p>
-        <p i18n="@@contactAdminLabel">Contact your Administrator if you feel that you should have access.</p>
-    </div>
+    selector: 'app-unauthorised',
+    template: `
+        <div
+            class="unauthorised rounded m-4 text-center bg-white text-black border border-gray-400 p-4 shadow"
+        >
+            <h1 class="text-4xl">403</h1>
+            <h3 class="text-2xl" i18n="@@forbiddenLabel">Access forbidden.</h3>
+            <p class="py-4" i18n="@@invalidPermissionLabel">
+                You do not have permission to view this page and your access
+                attempt has been recorded.
+            </p>
+            <p i18n="@@contactAdminLabel">
+                Contact your Administrator if you feel that you should have
+                access.
+            </p>
+        </div>
     `,
-    styles: [`
-        :host {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .unauthorised {
-            width: 24em;
-            background-color: #fff;
-            color: rgba(#000, .85);
-            border-radius: 4px;
-            border: 1px solid #ccc;
-            text-align: center;
-            padding: .5em 2em 2em;
-            margin: 1em;
-        }
-
-        @media (prefers-color-scheme: dark) {
-            .unauthorised {
-                background-color: #262626;
-                color: #fff;
+    styles: [
+        `
+            :host {
+                display: flex;
+                align-items: center;
+                justify-content: center;
             }
-        }
-    `]
+
+            .unauthorised {
+                width: 28em;
+                max-width: calc(100vw - 1rem);
+            }
+
+            @media (prefers-color-scheme: dark) {
+                .unauthorised {
+                    background-color: #262626;
+                    color: #fff;
+                }
+            }
+        `,
+    ],
 })
-export class UnauthorisedComponent { }
+export class UnauthorisedComponent {}
