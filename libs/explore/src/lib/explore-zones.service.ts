829cbf8ba7e3b764f71d014bff98175052639885
Alex Sorafumo <alex@yuion.net>
2020-10-22 10:17:33 +1100

    chore(ci): install nx cli globally

--- libs/explore/src/lib/explore-zones.service.ts
+++ libs/explore/src/lib/explore-zones.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { listen } from '@placeos/ts-client';
-import { BehaviorSubject } from 'rxjs';
+import { getModule, listen } from '@placeos/ts-client';
 
 import { BaseClass, HashMap, SettingsService } from '@user-interfaces/common';
-import { BuildingLevel, OrganisationService } from '@user-interfaces/organisation';
+import {
+    BuildingLevel,
+    OrganisationService,
+} from '@user-interfaces/organisation';
 
 import { ExploreStateService } from './explore-state.service';
 import { DEFAULT_COLOURS } from './explore-spaces.service';
@@ -11,7 +13,7 @@ import { DEFAULT_COLOURS } from './explore-spaces.service';
 @Injectable()
 export class ExploreZonesService extends BaseClass {
     private _level: BuildingLevel = null;
-    private _zones = new BehaviorSubject<HashMap<HashMap<number>>>({});
+    private _bindings: any[] = [];
     private _statuses: HashMap<string> = {};
 
     constructor(
@@ -25,23 +27,7 @@ export class ExploreZonesService extends BaseClass {
             this._state.level.subscribe((level) => {
                 this.clearBindings();
                 this._level = level;
-                this.bindToDesks();
-            })
-        );
-        this.subscription(
-            'changes',
-            this._zones.subscribe((zones) => {
-                if (!this._level) return;
-                const zone_map = zones[this._level.id] || {};
-                for (const id in zone_map) {
-                    this._statuses[id] =
-                        zone_map[id] < 40
-                            ? 'free'
-                            : zone_map[id] < 75
-                                ? 'pending'
-                                : 'busy';
-                }
-                this.timeout('update', () => this.updateStatus(), 100);
+                this.bindToZones();
             })
         );
     }
@@ -49,39 +35,49 @@ export class ExploreZonesService extends BaseClass {
     public clearBindings() {
         if (!this._level) return;
         this.unsub('zones');
+        this._bindings.forEach(b => b.unbind());
+        this._bindings = [];
         this._statuses = {};
     }
 
-    public bindToDesks() {
+    public bindToZones() {
         if (!this._level) return;
-        const building = this._org.buildings.find((bld) => bld.id === this._level.parent_id);
-        if (!building) {
-            return;
-        }
-        const system_id = building.systems.floor_management;
-        if (!system_id) {
-            return;
-        }
-        system_id;
-        const binding = { sys: system_id, mod: 'FloorManagement', index: 1 };
+        const building = this._org.buildings.find(
+            (bld) => bld.id === this._level.parent_id
+        );
+        if (!building) return;
+        const system_id = this._org.organisation.bindings.area_management;
+        if (!system_id) return;
+        const binding = getModule(system_id, 'AreaManagement').binding(`${this._level}:areas`);
         this.subscription(
             `zones`,
-            listen({
-                ...binding,
-                name: `usage`,
-            }).subscribe((d) => this._zones.next(d))
+            binding.listen().subscribe((d) => this.parseData(d))
         );
+        binding.bind();
+        this._bindings.push(binding);
+    }
+
+    public parseData(d) {
+        const value = d.value;
+        for (const zone of value) {
+            this._statuses[zone.area_id] =
+                zone.count < 40 ? 'free' : zone.count < 75 ? 'pending' : 'busy';
+        }
+        this.timeout('update', () => this.updateStatus(), 100);
     }
 
     private updateStatus() {
         const style_map = {};
-        const colours = this._settings.get('app.explore.colors') || DEFAULT_COLOURS;
+        const colours =
+            this._settings.get('app.explore.colors') || DEFAULT_COLOURS;
         for (const zone_id in this._statuses) {
             style_map[`#${zone_id}`] = {
-                fill: colours[`desk-${this._statuses[zone_id]}`] || colours[`${this._statuses[zone_id]}`],
+                fill:
+                    colours[`zone-${this._statuses[zone_id]}`] ||
+                    colours[`${this._statuses[zone_id]}`],
                 opacity: 0.6,
             };
         }
-        this._state.setStyles('desks', style_map);
+        this._state.setStyles('zones', style_map);
     }
 }
