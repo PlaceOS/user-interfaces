779aa48a86c92586341959855226d5722d2d7093
Alex Sorafumo <alex@yuion.net>
2020-10-24 16:09:31 +1100

    chore: minor fixes

--- apps/workplace/src/app/bookings/space-flow/space-flow-form.component.ts
+++ apps/workplace/src/app/bookings/space-flow/space-flow-form.component.ts
@@ -16,39 +16,74 @@ export const CAPACITY_OPTIONS = [
     selector: 'space-flow-form',
     template: `
         <div name="heading" class="p-4 w-full">
-            <h3 class="m-auto text-white uppercase width-tablet font-semibold text-xl">How would you like to search?</h3>
+            <h3
+                class="m-auto text-white uppercase width-tablet font-semibold text-xl"
+            >
+                How would you like to search?
+            </h3>
         </div>
         <div name="quick" class="p-4 w-full">
-            <h3 class="m-auto text-white uppercase width-tablet mb-2 font-semibold text-xl">Quick Search</h3>
-            <form class="flex width-tablet m-auto space-x-2 items-center flex-wrap sm:space-x-0" [formGroup]="form | async">
-                <mat-form-field class="flex-1 h-13" appearance="outline" overlay>
+            <h3
+                class="m-auto text-white uppercase width-tablet mb-2 font-semibold text-xl"
+            >
+                Quick Search
+            </h3>
+            <form
+                class="flex width-tablet m-auto space-x-2 items-center flex-wrap sm:space-x-0"
+                [formGroup]="form | async"
+            >
+                <mat-form-field
+                    class="flex-1 h-13"
+                    appearance="outline"
+                    overlay
+                >
                     <mat-select placeholder="Now" formControlName="date">
-                        <mat-option *ngFor="let option of time_options" [value]="option.id">
+                        <mat-option
+                            *ngFor="let option of time_options"
+                            [value]="option.id"
+                        >
                             {{ option.name }}
                         </mat-option>
                     </mat-select>
                 </mat-form-field>
-                <mat-form-field class="flex-1 h-13" appearance="outline" overlay>
+                <mat-form-field
+                    class="flex-1 h-13"
+                    appearance="outline"
+                    overlay
+                >
                     <mat-select
                         placeholder="Any Capacity"
                         [ngModel]="(filters | async)?.capacity"
                         (ngModelChange)="updateCapacity($event)"
                         [ngModelOptions]="{ standalone: true }"
                     >
-                        <mat-option *ngFor="let option of capacity_options" [value]="option.id">
+                        <mat-option
+                            *ngFor="let option of capacity_options"
+                            [value]="option.id"
+                        >
                             {{ option.name }}
                         </mat-option>
                     </mat-select>
                 </mat-form-field>
-                <a button mat-button style="margin-left: .5rem" [routerLink]="['/book', 'spaces', 'find']">
+                <a
+                    button
+                    mat-button
+                    style="margin-left: .5rem"
+                    [routerLink]="['/book', 'spaces', 'find']"
+                >
                     <div class="flex items-center justify-center ml-2">
-                        Go <app-icon class="text-lg" className="material-icons">arrow_forward</app-icon>
+                        Go
+                        <app-icon class="text-lg" className="material-icons"
+                            >arrow_forward</app-icon
+                        >
                     </div>
                 </a>
             </form>
         </div>
         <div name="divider" class="p-4 w-full bg-gray-200">
-            <h3 class="m-auto uppercase width-tablet font-semibold text-xl">Or</h3>
+            <h3 class="m-auto uppercase width-tablet font-semibold text-xl">
+                Or
+            </h3>
         </div>
         <form
             *ngIf="form | async"
@@ -57,19 +92,32 @@ export const CAPACITY_OPTIONS = [
             (ngSubmit)="$event.preventDefault()"
             [formGroup]="form | async"
         >
-            <h3 class="m-auto uppercase width-tablet pt-4 font-semibold text-xl mb-4">Detailed Booking</h3>
+            <h3
+                class="m-auto uppercase width-tablet pt-4 font-semibold text-xl mb-4"
+            >
+                Detailed Booking
+            </h3>
             <section class="mb-4">
-                <div class="m-auto width-tablet flex flex-wrap space-x-2 sm:space-x-0">
+                <div
+                    name="details"
+                    class="m-auto width-tablet flex flex-wrap space-x-2 sm:space-x-0"
+                >
                     <h4 class="mb-2 w-full font-semibold">Date &amp; Time</h4>
-                    <div class="field flex flex-1 flex-wrap m-0">
+                    <div class="flex flex-1 flex-col m-0">
                         <label for="date" class="w-full">Date</label>
-                        <a-date-field formControlName="date" class="flex-1 mb-4"></a-date-field>
+                        <a-date-field
+                            formControlName="date"
+                            class="flex-1 mb-4"
+                        ></a-date-field>
                     </div>
-                    <div class="field flex flex-1 flex-wrap">
+                    <div class="flex flex-1 flex-col">
                         <label for="date" class="w-full">Start Time</label>
-                        <a-time-field formControlName="date"></a-time-field>
+                        <a-time-field
+                            formControlName="date"
+                            class="mb-8 pt-1"
+                        ></a-time-field>
                     </div>
-                    <div class="field flex flex-1 flex-wrap">
+                    <div class="flex flex-1 flex-col ml-1">
                         <label for="date" class="w-full">End Time</label>
                         <a-duration-field
                             [time]="(form | async)?.controls.date?.value"
@@ -121,25 +169,64 @@ export const CAPACITY_OPTIONS = [
                             ></textarea>
                         </mat-form-field>
                         <ng-template #html_editor>
-                            <editor class="flex-1" formControlName="body"></editor>
+                            <editor
+                                class="flex-1"
+                                formControlName="body"
+                            ></editor>
                         </ng-template>
                     </div>
                 </div>
             </section>
             <div class="m-auto width-tablet mb-2 flex items-center">
-                <button mat-button class="inverse" type="button" (click)="clearForm()">
+                <button
+                    mat-button
+                    class="inverse"
+                    type="button"
+                    (click)="clearForm()"
+                >
                     <div class="flex items-center justify-center mr-2">
-                        <app-icon class="text-lg" [icon]="{ class: 'material-icons', content: 'delete' }"></app-icon> Clear Form
+                        <app-icon
+                            class="text-lg"
+                            [icon]="{
+                                class: 'material-icons',
+                                content: 'delete'
+                            }"
+                        ></app-icon>
+                        Clear Form
                     </div>
                 </button>
-                <button mat-button class="inverse" style="margin-left: .5rem" type="button">
+                <button
+                    mat-button
+                    class="inverse"
+                    style="margin-left: .5rem"
+                    type="button"
+                >
                     <div class="flex items-center justify-center mr-2">
-                        <app-icon class="text-lg" [icon]="{ class: 'material-icons', content: 'filter_list' }"></app-icon> Add Filter
+                        <app-icon
+                            class="text-lg"
+                            [icon]="{
+                                class: 'material-icons',
+                                content: 'filter_list'
+                            }"
+                        ></app-icon>
+                        Add Filter
                     </div>
                 </button>
-                <a button mat-button style="margin-left: .5rem" [routerLink]="['/book', 'spaces', 'find']">
+                <a
+                    button
+                    mat-button
+                    style="margin-left: .5rem"
+                    [routerLink]="['/book', 'spaces', 'find']"
+                >
                     <div class="flex items-center justify-center mr-2">
-                        <app-icon class="text-lg" [icon]="{ class: 'material-icons', content: 'search' }"></app-icon> Search
+                        <app-icon
+                            class="text-lg"
+                            [icon]="{
+                                class: 'material-icons',
+                                content: 'search'
+                            }"
+                        ></app-icon>
+                        Search
                     </div>
                 </a>
             </div>
@@ -164,11 +251,12 @@ export const CAPACITY_OPTIONS = [
                 background-color: #007ac8;
             }
 
-            [name='quick'] form > * + * {
-                margin-left: .5rem;
+            [name='quick'] form > * + *,
+            [name='details'] > * + *  {
+                margin-left: 0.5rem;
             }
 
-            [name="quick"] mat-form-field {
+            [name='quick'] mat-form-field {
                 max-height: 3.25rem;
             }
 
@@ -181,14 +269,14 @@ export const CAPACITY_OPTIONS = [
                 border-bottom: 1px solid #ccc;
             }
 
-            button, a[button] {
+            button,
+            a[button] {
                 min-width: 10em;
             }
 
             .field {
                 min-width: 12rem;
             }
-
         `,
     ],
 })
@@ -197,22 +285,34 @@ export class SpaceFlowFormComponent {
     public readonly form = this._service.form;
     /** Form filters for event */
     public readonly filters = this._service.filters;
-    public readonly updateCapacity = (c: number) => this._service.updateFilters({ capacity: c });
+    public readonly updateCapacity = (c: number) =>
+        this._service.updateFilters({ capacity: c });
     public readonly use_html = this._settings.get('app.booking.use_html');
     public readonly clearForm = () => this._service.clearForm();
     public readonly capacity_options = CAPACITY_OPTIONS;
     public readonly time_options = [
-        { id: roundToNearestMinutes(addMinutes(new Date(), 2)).valueOf(), name: 'Now' },
         {
-            id: addHours(roundToNearestMinutes(addMinutes(new Date(), 2)).valueOf(), 1),
+            id: roundToNearestMinutes(addMinutes(new Date(), 2)).valueOf(),
+            name: 'Now',
+        },
+        {
+            id: addHours(
+                roundToNearestMinutes(addMinutes(new Date(), 2)).valueOf(),
+                1
+            ),
             name: 'Later Today',
         },
         {
-            id: addDays(roundToNearestMinutes(addMinutes(new Date(), 2)).valueOf(), 1),
+            id: addDays(
+                roundToNearestMinutes(addMinutes(new Date(), 2)).valueOf(),
+                1
+            ),
             name: 'Tomorrow',
         },
     ];
 
-
-    constructor(private _service: SpaceFlowService, private _settings: SettingsService) {}
+    constructor(
+        private _service: SpaceFlowService,
+        private _settings: SettingsService
+    ) {}
 }
--- libs/components/src/lib/date-field/date-field.component.ts
+++ libs/components/src/lib/date-field/date-field.component.ts
@@ -8,52 +8,19 @@ import * as dayjs from 'dayjs';
 @Component({
     selector: 'a-date-field',
     template: `
-        <div
-            class="relative flex items-center w-full rounded p-1 bg-white text-black"
-            role="button"
-            form-field
-            [class.active]="is_open"
-            [attr.disabled]="disabled"
-        >
-            <div class="flex-1 p-2">{{ date_string }}</div>
-            <app-icon className="material-icons" class="m-2 text-2xl">
-                {{ is_open ? 'arrow_drop_up' : 'arrow_drop_down' }}
-            </app-icon>
-            <input matInput class="opacity-0 absolute inset-0" [ngModel]="date" (ngModelChange)="setValue($event)" [matDatepicker]="picker" />
+        <mat-form-field appearance="outline" overlay>
+            <input matInput [ngModel]="date" [disabled]="disabled" (ngModelChange)="setValue($event)" [matDatepicker]="picker" />
             <mat-datepicker-toggle
                 matSuffix
                 [for]="picker"
             ></mat-datepicker-toggle>
             <mat-datepicker #picker></mat-datepicker>
-        </div>
+        </mat-form-field>
     `,
     styles: [
         `
-            :host > div {
-                border: 1px solid #ccc;
-                box-shadow: inset 0 0 0 1px #fff;
-                transition: border 200ms, box-shadow 200ms;
-            }
-            :host > div:hover {
-                box-shadow: inset 0 0 0 1px #464646;
-                border-color: #464646;
-            }
-
-            :host > div:focus,
-            :host > div.active {
-                box-shadow: inset 0 0 0 1px $color-primary;
-                border-color: $color-primary;
-                outline-color: $color-primary;
-            }
-
-            :host > div:focus .icon,
-            :host > div.active .icon {
-                color: $color-primary;
-            }
-
-            [disabled='true'] {
-                pointer-events: none;
-                opacity: 0.35;
+            mat-form-field {
+                width: 100%;
             }
         `,
     ],
@@ -80,15 +47,12 @@ export class DateFieldComponent extends BaseClass
     /** Whether to show the calendar tooltip */
     public show_tooltip: boolean;
     /** Currently selected date */
-    public date: number;
+    public date: string;
 
     /** Form control on change handler */
     private _onChange: (_: number) => void;
     /** Form control on touch handler */
     private _onTouch: (_: number) => void;
-
-    @ViewChild(MatMenuTrigger, { static: true })
-    private _trigger: MatMenuTrigger;
 
     /** First allowed date on the calendar */
     public get from(): number {
@@ -103,13 +67,8 @@ export class DateFieldComponent extends BaseClass
         return dayjs(this.date).format('DD MMM YYYY');
     }
 
-    /** Whether date picker is open */
-    public get is_open(): boolean {
-        return this._trigger?.menuOpen;
-    }
-
     public ngOnInit() {
-        this.date = dayjs().valueOf();
+        this.date = new Date().toISOString();
     }
 
     /**
@@ -128,7 +87,7 @@ export class DateFieldComponent extends BaseClass
         if (new_date < this.from) {
             new_date = this.from;
         }
-        this.date = new_date;
+        this.date = new Date(new_date).toISOString();
         if (this._onChange) {
             this._onChange(new_date);
         }
@@ -141,7 +100,7 @@ export class DateFieldComponent extends BaseClass
      * @param value The new value for the component
      */
     public writeValue(value: number) {
-        this.date = value;
+        this.date = new Date(value).toISOString();
         this.show_tooltip = false;
     }
 
@@ -165,15 +124,5 @@ export class DateFieldComponent extends BaseClass
 
     public setDisabledState(disabled: boolean) {
         this.disabled = disabled;
-    }
-
-    /** Set the state of the tooltip to closed after delay */
-    public closeDatePicker() {
-        this._trigger.closeMenu();
-    }
-
-    /** Set the state of the tooltip to closed after delay */
-    public openDatePicker() {
-        this._trigger.openMenu();
     }
 }
