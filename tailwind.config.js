26e380b3a1785958f673ea5d5cdb3b1ff189a928
Alex Sorafumo <alex@yuion.net>
2020-11-23 11:34:16 +1100

    chore: add more specificity to tailwind selectors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = (isProd) => ({
     prefix: '',
+    important: '#placeos',
     future: {
-      removeDeprecatedGapUtilities: true,
-      purgeLayersByDefault: true
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
     },
     purge: {
-      enabled: isProd,
-      content: ['**/*.html', '**/*.ts']
+        enabled: isProd,
+        content: ['**/*.html', '**/*.ts'],
     },
-    theme: {}
+    theme: {
+        extend: {
+            colors: {
+                primary: '#C92366',
+                secondary: '#0A0D2E',
+                error: '#e53935',
+                pending: '#ffb300',
+                success: '#43a047',
+            },
+        },
+    },
 });
