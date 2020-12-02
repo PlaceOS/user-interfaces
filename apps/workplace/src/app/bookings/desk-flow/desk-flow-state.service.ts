50adffc9d18fcb47fe494fa6bd852621374fd1d8
Alex Sorafumo <alex@yuion.net>
2020-12-03 10:36:29 +1100

    chore(workplace): add check for existing desk bookings

--- apps/workplace/src/app/bookings/desk-flow/desk-flow-state.service.ts
+++ apps/workplace/src/app/bookings/desk-flow/desk-flow-state.service.ts
@@ -217,6 +217,14 @@ export class DeskFlowStateService extends BaseClass {
                 .toPromise(),
         ]);
         if (!success) return;
+        ref.componentInstance.loading = 'Checking for existing desk bookings...';
+        const desks = await this._bookings.query({
+            type: 'desk',
+            period_start: Math.floor(startOfDay(options.date || new Date()).valueOf() / 1000),
+            period_end: Math.floor(endOfDay(options.date || new Date()).valueOf() / 1000),
+        });
+        if (!desks?.length) return notifyError('You currently already have a desk booked for the selected date.');
+        ref.componentInstance.loading = 'Booking desk...';
         await this.makeDeskBooking(
             desk,
             options.date || new Date().valueOf(),
