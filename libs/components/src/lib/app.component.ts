4cf5818a9afad6f0ae4813fafd9589c34f91c850
Alex Sorafumo <alex@yuion.net>
2020-12-05 18:04:42 +1100

    chore(ci): update config

--- libs/components/src/lib/app.component.ts
+++ libs/components/src/lib/app.component.ts
@@ -73,7 +73,6 @@ export class AppComponent extends BaseClass implements OnInit {
 
     constructor(
         private _tracing: Sentry.TraceService,
-
         private _settings: SettingsService,
         private _org: OrganisationService, // For init
         private _spaces: SpacesService, // For init
