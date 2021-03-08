b09ba9e437b4bf5811cd68dad30d0c4e765bdabd
Alex Sorafumo <alex@yuion.net>
2021-03-08 19:03:27 +1100

    feat(components): add custom table component

--- libs/components/src/lib/components.module.ts
+++ libs/components/src/lib/components.module.ts
@@ -1,5 +1,7 @@
 import { NgModule, Type } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -11,6 +13,8 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { CdkTableModule } from '@angular/cdk/table';
 
 import { APipesModule } from '@acaprojects/ngx-pipes';
 
@@ -20,15 +24,14 @@ import { IconComponent } from './icon.component';
 import { PopoutMenuComponent } from './popout-menu.component';
 import { UserAvatarComponent } from './user-avatar.component';
 
-import { MatDialogModule } from '@angular/material/dialog';
 import { ConfirmModalComponent } from './confirm-modal.component';
-import { CommonModule } from '@angular/common';
 import { InteractiveMapComponent, MAP_FEATURE_DATA } from './interactive-map.component';
 import { MapPinComponent } from './map-pin.component';
 import { MapRadiusComponent } from './map-radius.component';
 import { SearchbarComponent } from './searchbar.component';
 import { StatusOverlayComponent } from './status-overlay.component';
 import { FixedPipe } from './fixed.pipe';
+import { CustomTableComponent } from './custom-table.component';
 
 export * from './action-icon.component';
 export * from './binding.directive';
@@ -44,6 +47,7 @@ export * from './map-radius.component';
 export * from './searchbar.component';
 export * from './misconfigured.component';
 export * from './status-overlay.component';
+export * from './custom-table.component';
 export * from './fixed.pipe';
 
 const MAT_MODULES: any[] = [
@@ -59,7 +63,9 @@ const MAT_MODULES: any[] = [
     MatInputModule,
     ClipboardModule,
     MatProgressSpinnerModule,
-    MatChipsModule
+    MatChipsModule,
+    MatPaginatorModule,
+    CdkTableModule
 ];
 
 const COMPONENTS: Type<any>[] = [
@@ -74,6 +80,7 @@ const COMPONENTS: Type<any>[] = [
     MapRadiusComponent,
     SearchbarComponent,
     StatusOverlayComponent,
+    CustomTableComponent,
 
     FixedPipe
 ];
