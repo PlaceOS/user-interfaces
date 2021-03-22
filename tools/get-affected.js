7cb60c5daeda374674d0d36a73dbd7736fc5de88
Alex Sorafumo <alex@yuion.net>
2021-03-22 20:33:50 +1100

    chore(ci): tweak logic to get affected apps

--- tools/get-affected.js
+++ tools/get-affected.js
@@ -1,7 +1,6 @@
 const execSync = require('child_process').execSync;
-const isMaster = process.argv[2] === 'False';
-const ref = process.argv[3] || 'origin/master';
-const baseSha = isMaster ? `${ref}~1` : ref;
+const ref = process.argv[2] || 'origin/master';
+const baseSha = ref;
 
 // prints an object with keys {lint1: [...], lint2: [...], lint3: [...], test1: [...], .... build3: [...]}
 console.log(
