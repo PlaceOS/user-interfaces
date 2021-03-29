98e1a568e21a6109c42dbaa75ca3e37a7950dc1d
Alex Sorafumo <alex@yuion.net>
2021-03-29 13:03:07 +1100

    chore(ci): update testing pipeline config

--- tools/get-affected.js
+++ tools/get-affected.js
@@ -1,10 +1,11 @@
 const execSync = require('child_process').execSync;
 const ref = process.argv[2] || 'origin/master';
 const baseSha = ref;
+const cmd = process.argv[3] || 'build';
 
 // prints an object with keys {lint1: [...], lint2: [...], lint3: [...], test1: [...], .... build3: [...]}
 console.log(
-  JSON.stringify(commands('build'))
+  JSON.stringify(commands(cmd))
 );
 
 function commands(target) {
