cafe53ac537143d154dab12567857956d88a17ef
Alex Sorafumo <alex@yuion.net>
2021-04-09 13:11:21 +1000

    test(components): add unit tests for unauthorised component

--- libs/components/src/tests/unauthorised.component.spec.ts
+++ libs/components/src/tests/unauthorised.component.spec.ts
@@ -1,15 +1,18 @@
-
 import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
 
 import { UnauthorisedComponent } from '../lib/unauthorised.component';
 
 describe('UnauthorisedComponent', () => {
     let spectator: Spectator<UnauthorisedComponent>;
-    const createComponent = createComponentFactory(UnauthorisedComponent)
+    const createComponent = createComponentFactory(UnauthorisedComponent);
 
     beforeEach(() => (spectator = createComponent()));
 
     it('should create component', () => {
         expect(spectator.component).toBeTruthy();
+    });
+
+    it('should match snapshot', () => {
+        expect(spectator.element).toMatchSnapshot();
     });
 });
