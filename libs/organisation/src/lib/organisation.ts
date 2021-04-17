7ab8a1928749062d84110ede6d0e6882fa352869
Alex Sorafumo <alex@yuion.net>
2021-04-17 12:15:04 +1000

    chore(org): remove reference to zone class

--- libs/organisation/src/lib/organisation.ts
+++ libs/organisation/src/lib/organisation.ts
@@ -1,9 +1,7 @@
-
 export * from './building.class';
 export * from './building.utilities';
 export * from './level.class';
 export * from './organisation.class';
 export * from './organisation.service';
 export * from './organisation.utilities';
-export * from './zone.class';
 export * from './desk.class';
