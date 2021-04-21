f0926a196a9d5b30cba0532cb5f89c8717b76abd
Alex Sorafumo <alex@yuion.net>
2021-04-21 10:03:33 +1000

    test(explore): add unit tests for zone state service

--- libs/explore/src/lib/explore-zones.service.ts
+++ libs/explore/src/lib/explore-zones.service.ts
@@ -1,24 +1,53 @@
 import { Injectable } from '@angular/core';
 import { getModule, showMetadata } from '@placeos/ts-client';
 import { ViewerLabel, Point } from '@placeos/svg-viewer';
+import { first, map } from 'rxjs/operators';
 
 import { BaseClass, HashMap, SettingsService } from '@placeos/common';
-import { BuildingLevel, OrganisationService } from '@placeos/organisation';
+import { OrganisationService } from '@placeos/organisation';
 
 import { ExploreStateService } from './explore-state.service';
 import { DEFAULT_COLOURS } from './explore-spaces.service';
-import { first } from 'rxjs/operators';
 
 const EMPTY_LABEL = { location: { x: -10, y: -10 }, content: '0% Usage' };
 
+export interface ZoneData {
+    /** ID of the zone */
+    area_id: string;
+    /** Number of devices in the zone */
+    count: number;
+}
+
 @Injectable()
 export class ExploreZonesService extends BaseClass {
-    private _level: BuildingLevel = null;
-    private _bindings: any[] = [];
     private _statuses: HashMap<string> = {};
     private _labels: HashMap<ViewerLabel> = {};
     private _location: HashMap<Point> = {};
     private _capacity: HashMap<number> = {};
+
+    private _bind = this._state.level.pipe(
+        map((lvl) => {
+            if (!lvl) return;
+            this._statuses = {};
+            const building = this._org.buildings.find(
+                (bld) => bld.id === lvl.parent_id
+            );
+            if (!building) return;
+            const system_id =
+                building.bindings.area_management ||
+                this._org.organisation.bindings.area_management;
+            if (!system_id) return;
+            const binding = getModule(system_id, 'AreaManagement').binding(
+                `${lvl.id}:areas`
+            );
+            console.log('Bind:', binding);
+            this.subscription(
+                `zones`,
+                binding.listen().subscribe((d) => this.parseData(d))
+            );
+            this.subscription('binding', binding.bind());
+        })
+    );
 
     constructor(
         private _state: ExploreStateService,
@@ -46,47 +75,11 @@ export class ExploreZonesService extends BaseClass {
                 }
             }
         }
-        this.subscription(
-            'spaces',
-            this._state.level.subscribe((level) => {
-                this.clearBindings();
-                this._level = level;
-                this.bindToZones();
-            })
-        );
+        this.subscription('bind', this._bind.subscribe());
     }
 
-    public clearBindings() {
-        if (!this._level) return;
-        this.unsub('zones');
-        this._bindings.forEach((b) => b.unbind());
-        this._bindings = [];
-        this._statuses = {};
-    }
-
-    public bindToZones() {
-        if (!this._level) return;
-        const building = this._org.buildings.find(
-            (bld) => bld.id === this._level.parent_id
-        );
-        if (!building) return;
-        const system_id =
-            building.bindings.area_management ||
-            this._org.organisation.bindings.area_management;
-        if (!system_id) return;
-        const binding = getModule(system_id, 'AreaManagement').binding(
-            `${this._level.id}:areas`
-        );
-        this.subscription(
-            `zones`,
-            binding.listen().subscribe((d) => this.parseData(d))
-        );
-        binding.bind();
-        this._bindings.push(binding);
-    }
-
-    public parseData(d) {
-        const value = d?.value || [];
+    public parseData(data?: { value: ZoneData[] }) {
+        const value = data?.value || [];
         const labels = [];
         for (const zone of value) {
             const filled = zone.count / (this._capacity[zone.area_id] || 100);
@@ -108,7 +101,7 @@ export class ExploreZonesService extends BaseClass {
         const style_map = {};
         const colours = this._settings.get('app.explore.colors') || {};
         for (const zone_id in this._statuses) {
-            if (!this._statuses.hasOwnProperty(zone_id)) continue;
+            if (!this._statuses[zone_id]) continue;
             style_map[`#${zone_id}`] = {
                 fill:
                     colours[`zone-${this._statuses[zone_id]}`] ||
