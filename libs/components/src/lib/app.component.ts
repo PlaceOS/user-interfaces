8f1582a08675c230d2ec53ecc02fad4e4265bb7c
Alex Sorafumo <alex@yuion.net>
2021-05-12 14:50:22 +1000

    chore: force build

--- libs/components/src/lib/app.component.ts
+++ libs/components/src/lib/app.component.ts
@@ -124,9 +124,7 @@ export class AppComponent extends BaseClass implements OnInit {
     }
 
     private onInitError() {
-        if (isMock() || currentUser()?.is_logged_in) {
-            return;
-        }
+        if (isMock() || currentUser()?.is_logged_in) return;
         console.error('Error initialising user.');
         invalidateToken();
         location.reload();
