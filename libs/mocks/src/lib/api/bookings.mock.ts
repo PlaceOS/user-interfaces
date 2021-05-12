e8b9aafd8bcf987cea6538dbd714fd541d1f258b
Alex Sorafumo <alex@yuion.net>
2021-05-12 14:49:33 +1000

    chore(mocks): add request handlers for various bookings endpoints

--- libs/mocks/src/lib/api/bookings.mock.ts
+++ libs/mocks/src/lib/api/bookings.mock.ts
@@ -1,3 +1,4 @@
+import { trigger } from '@angular/animations';
 import { predictableRandomInt, timePeriodsIntersect } from '@placeos/common';
 import { registerMockEndpoint } from '@placeos/ts-client';
 
@@ -22,6 +23,21 @@ registerMockEndpoint({
 });
 
 registerMockEndpoint({
+    path: '/api/staff/v1/bookings/:id',
+    metadata: {},
+    method: 'GET',
+    callback: (_) => {
+        const event = MOCK_BOOKINGS.find((e) => e.id === _.route_params.id);
+        if (!event)
+            throw {
+                status: 404,
+                message: `Unable to find booking with ID ${_.route_params.id}`,
+            };
+        return event;
+    },
+});
+
+registerMockEndpoint({
     path: '/api/staff/v1/bookings',
     metadata: {},
     method: 'POST',
@@ -32,5 +48,77 @@ registerMockEndpoint({
         };
         MOCK_BOOKINGS.push(new_event);
         return new_event;
+    },
+});
+
+const updateBooking = (id, data) => {
+    const index = MOCK_BOOKINGS.findIndex((e) => e.id === id);
+    if (index < 0)
+        throw {
+            status: 404,
+            message: `Unable to find booking with ID ${id}`,
+        };
+    const new_event = { ...data };
+    MOCK_BOOKINGS.splice(index, 1, new_event);
+    return new_event;
+};
+
+registerMockEndpoint({
+    path: '/api/staff/v1/bookings/:id',
+    metadata: {},
+    method: 'PUT',
+    callback: (req) => updateBooking(req.route_params.id, { ...req.body }),
+});
+
+registerMockEndpoint({
+    path: '/api/staff/v1/bookings/:id/approve',
+    metadata: {},
+    method: 'POST',
+    callback: (req) =>
+        updateBooking(req.route_params.id, {
+            ...req.body,
+            approved: true,
+            rejected: false,
+        }),
+});
+
+registerMockEndpoint({
+    path: '/api/staff/v1/bookings/:id/reject',
+    metadata: {},
+    method: 'POST',
+    callback: (req) =>
+        updateBooking(req.route_params.id, {
+            ...req.body,
+            approved: false,
+            rejected: true,
+        }),
+});
+
+registerMockEndpoint({
+    path: '/api/staff/v1/bookings/:id/check_in',
+    metadata: {},
+    method: 'POST',
+    callback: (req) =>
+        updateBooking(req.route_params.id, {
+            ...req.body,
+            checked_in: req.query_params.state === 'true',
+        }),
+});
+
+registerMockEndpoint({
+    path: '/api/staff/v1/bookings/:id',
+    metadata: {},
+    method: 'DELETE',
+    callback: (req) => {
+        const index = MOCK_BOOKINGS.findIndex(
+            (e) => e.id === req.route_params.id
+        );
+        if (index < 0)
+            throw {
+                status: 404,
+                message: `Unable to find booking with ID ${req.route_params.id}`,
+            };
+        MOCK_BOOKINGS.splice(index, 1);
+        return;
     },
 });
