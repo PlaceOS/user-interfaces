2bedb64b1e2ba4c646834a3cc96086c9f0865ae2
Alex Sorafumo <alex@yuion.net>
2021-05-19 11:55:58 +1000

    test(booking-panel): add unit tests for panel timeline component

--- apps/booking-panel/src/app/panel/panel.component.ts
+++ apps/booking-panel/src/app/panel/panel.component.ts
@@ -34,7 +34,7 @@ import { PanelStateService } from './panel-state.service';
                 ></panel-booking-list>
             </div>
             <panel-status></panel-status>
-            <panel-timeline></panel-timeline>
+            <!-- <panel-timeline></panel-timeline> -->
         </div>
     `,
     styles: [``],
