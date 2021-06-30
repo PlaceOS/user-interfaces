beab4b4adadd65b774ef92277817086c07e49339
Alex Sorafumo <alex@yuion.net>
2021-06-30 16:20:20 +1000

    test(workplace): add unit tests for detailed space booking form component

---
+++ apps/workplace/src/tests/book/space-flow/detailed-form.component.spec.ts
@@ -0,0 +1,40 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { CalendarEvent, generateEventForm } from '@placeos/events';
+import {
+    DateFieldComponent,
+    DurationFieldComponent,
+    TimeFieldComponent,
+    UserListFieldComponent,
+} from '@placeos/form-fields';
+import { DetailBookSpaceFormComponent } from 'apps/workplace/src/app/book/space-flow/detailed-form.component';
+import { MockComponent } from 'ng-mocks';
+
+describe('DetailBookSpaceFormComponent', () => {
+    let spectator: Spectator<DetailBookSpaceFormComponent>;
+    const createComponent = createComponentFactory({
+        component: DetailBookSpaceFormComponent,
+        declarations: [
+            MockComponent(DateFieldComponent),
+            MockComponent(TimeFieldComponent),
+            MockComponent(DurationFieldComponent),
+            MockComponent(UserListFieldComponent),
+        ],
+        imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+    });
+
+    beforeEach(() => (spectator = createComponent()));
+
+    it('should create component', () => {
+        expect(spectator.component).toBeTruthy();
+    });
+
+    it('should match snapshot', () => {
+        expect(spectator.element).toMatchSnapshot();
+        spectator.setInput({ form: generateEventForm(new CalendarEvent()) });
+        spectator.detectChanges();
+        expect(spectator.element).toMatchSnapshot();
+    });
+});
