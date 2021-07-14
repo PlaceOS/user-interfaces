e050f5a66ec26a5047c4fcca7ea89c7877b65b17
Alex Sorafumo <alex@yuion.net>
2021-07-14 10:46:52 +1000

    refactor(concierge): simplify dayview timeline component

--- apps/concierge/src/app/day-view/dayview-timeline.component.ts
+++ apps/concierge/src/app/day-view/dayview-timeline.component.ts
@@ -1,11 +1,4 @@
-import {
-    AfterViewInit,
-    Component,
-    ElementRef,
-    OnDestroy,
-    OnInit,
-    ViewChild,
-} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -21,167 +14,93 @@ const HOUR_BLOCKS = new Array(24).fill(0).map((_, idx) => {
 @Component({
     selector: 'dayview-timeline',
     template: `
-        <div class="time h-full overflow-hidden">
-            <div class="headers relative">
+        <div class="absolute inset-0 flex">
+            <div class="time h-full w-24 overflow-hidden bg relative">
+                <div header class="relative h-16 z-50">
+                    <div
+                        class="bg absolute top-0 left-0 right-0 bottom-8"
+                    ></div>
+                </div>
                 <div
-                    class="white-space absolute inset-0"
-                    style="bottom: 1em"
-                ></div>
-            </div>
-            <div
-                *ngFor="let time of blocks"
-                name="change-transform"
-                class="relative"
-                [style.transform]="'translateY(-' + scroll.y + 'px)'"
-            >
-                <div class="text absolute w-full text-xs">{{ time }}</div>
-                <div class="bar absolute"></div>
-            </div>
-        </div>
-        <div class="h-full flex-1 flex flex-col">
-            <div class="headers flex relative overflow-hidden">
-                <div class="bar absolute"></div>
-                <div
-                    *ngFor="let space of space_list | async"
-                    name="change-transform"
-                    class="relative w"
-                    [style.transform]="'translateX(-' + scroll.x + 'px)'"
+                    *ngFor="let time of blocks"
+                    change-transform
+                    class="relative h-16 border-r border-gray-300 z-10"
+                    [style.transform]="'translateY(-' + scroll.y + 'px)'"
                 >
-                    <div class="bar absolute"></div>
-                    <div class="name m-2 text-center">
-                        {{ space.display_name || space.name }}
+                    <div
+                        class="absolute text-center w-full text-xs top-0 transform -translate-y-1/2 opacity-40"
+                    >
+                        {{ time }}
+                    </div>
+                    <div
+                        class="absolute h-px w-2 top-0 right-0 bg-gray-300"
+                    ></div>
+                </div>
+                <div class="absolute h-8 w-px top-8 right-0 bg-gray-300"></div>
+            </div>
+            <div class="h-full flex-1 flex flex-col w-1/2">
+                <div
+                    header
+                    class="flex relative overflow-hidden border-b border-gray-300 border-opacity-50 bg h-16 w-full"
+                >
+                    <div
+                        *ngFor="let space of space_list | async"
+                        change-transform
+                        class="relative h-16 w-48 min-w-[12rem]"
+                        [style.transform]="'translateX(-' + scroll.x + 'px)'"
+                    >
+                        <div
+                            bar
+                            class="absolute h-8 w-px bottom-0 -left-px bg-gray-300"
+                        ></div>
+                        <div class="name m-2 text-center">
+                            {{ space.display_name || space.name }}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div
-                #scroll_el
-                class="content relative flex flex-1 overflow-auto"
-                (scroll)="onScroll($event)"
-            >
-                <div #ref class="ref absolute"></div>
-                <dayview-space
-                    *ngFor="let space of space_list | async"
-                    [space]="space"
-                ></dayview-space>
                 <div
-                    *ngFor="let time of blocks; let i = index"
-                    class="block absolute w-full"
-                    [style.width]="(space_list | async).length * 12 + 'em'"
-                    [style.top]="i * 4 + 'em'"
-                ></div>
+                    #scroll_el
+                    content
+                    class="relative flex flex-1 overflow-auto"
+                    (scroll)="onScroll($event)"
+                >
+                    <dayview-space
+                        *ngFor="let space of space_list | async"
+                        [space]="space"
+                        class="w-48 min-w-[12rem]"
+                    ></dayview-space>
+                    <div
+                        *ngFor="let time of blocks; let i = index"
+                        class="absolute bg-gray-300 h-px w-full left-0"
+                        [style.width]="(space_list | async).length * 12 + 'rem'"
+                        [style.top]="i * 4 + 'rem'"
+                    ></div>
+                </div>
             </div>
+            <mat-progress-bar
+                *ngIf="loading | async"
+                mode="indeterminate"
+                class="absolute bottom-0 left-0 right-0"
+            ></mat-progress-bar>
+            <view-event-details
+                *ngIf="event | async"
+                [event]="event | async"
+            ></view-event-details>
         </div>
-        <mat-progress-bar
-            *ngIf="loading | async"
-            mode="indeterminate"
-        ></mat-progress-bar>
-        <view-event-details
-            *ngIf="event | async"
-            [event]="event | async"
-        ></view-event-details>
     `,
     styles: [
         `
             :host {
-                display: flex;
-                align-items: center;
-                flex: 1;
-                min-height: 50%;
+                position: relative;
+                height: 100%;
+                width: 100%;
             }
 
-            .time {
-                width: 6em;
+            .bg {
+                background-color: #ecf1f5;
             }
 
-            .time > div:not(.headers) {
-                height: 4em;
-            }
-
-            .bar {
-                width: 1em;
-                top: 0;
-                right: 0;
-                height: 1px;
-                background-color: rgba(0, 0, 0, 0.1);
-                z-index: 0;
-            }
-
-            .headers {
-                height: 4em;
-                max-width: calc(100vw - 12em);
-            }
-
-            .white-space {
-                background-color: #f0f0f0;
-                z-index: 999;
-            }
-
-            .headers .bar {
-                width: 1px;
-                bottom: 0;
-                right: -1px;
-                height: 2em;
-                background-color: rgba(0, 0, 0, 0.1);
-                top: auto;
-            }
-
-            .headers > .bar:first-child {
-                width: 1px;
-                bottom: 0;
-                right: auto;
-                left: 0;
-                height: 2em;
-                background-color: rgba(0, 0, 0, 0.1);
-                top: auto;
-            }
-
-            .w,
-            dayview-space {
-                width: 12em;
-                min-width: 12em;
-            }
-
-            dayview-space {
-                height: 96em;
-            }
-
-            .block {
-                height: 4em;
-                border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-                pointer-events: none;
-                min-width: 100%;
-                z-index: 0;
-            }
-
-            .text {
-                text-align: center;
-                transform: translateY(-50%);
-                padding: 0 2.5em;
-                opacity: 0.65;
-            }
-
-            .ref {
-                top: 0;
-                left: 0;
-                height: 1px;
-                width: 1px;
-                height: 96em;
-            }
-
-            .content {
-                max-width: calc(100vw - 12em);
-                border-top: 1px solid rgba(0, 0, 0, 0.1);
-                border-left: 1px solid rgba(0, 0, 0, 0.1);
-            }
-
-            mat-progress-bar {
-                position: absolute;
-                left: 0;
-                right: 0;
-                bottom: 0;
-            }
-
-            [name='change-transform'] {
+            [change-transform] {
                 will-change: transform;
             }
         `,
@@ -189,13 +108,11 @@ const HOUR_BLOCKS = new Array(24).fill(0).map((_, idx) => {
 })
 export class DayviewTimelineComponent
     extends BaseClass
-    implements OnInit, OnDestroy, AfterViewInit {
+    implements OnInit, OnDestroy {
     /** Time blocks to display */
     public readonly blocks: string[] = HOUR_BLOCKS;
     /** Current scroll position of the content */
     public scroll: { x: number; y: number } = { x: 0, y: 0 };
-    /** Reference Box */
-    public box: ClientRect;
     /** Whether event data is loading */
     public readonly loading = this._state.loading;
     /** Whether event data is loading */
@@ -206,20 +123,16 @@ export class DayviewTimelineComponent
         this._spaces.list,
         this._state.zones,
     ]).pipe(
-        map((details) => {
-            const [bld, spaces, zones] = details;
-            return (
+        map(
+            ([bld, spaces, zones]) =>
                 spaces.filter(
                     (space) =>
                         space.zones.includes(bld.id) &&
                         (!zones?.length ||
                             space.zones.find((z) => zones.includes(z)))
                 ) || []
-            );
-        })
+        )
     );
-
-    @ViewChild('ref') private _ref_el: ElementRef;
 
     constructor(
         private _org: OrganisationService,
@@ -237,12 +150,7 @@ export class DayviewTimelineComponent
         this._state.stopPolling();
     }
 
-    public ngAfterViewInit() {
-        this.box = this._ref_el.nativeElement.getBoundingClientRect();
-    }
-
     public onScroll(e) {
-        if (!this._ref_el) return;
         requestAnimationFrame(
             () =>
                 (this.scroll = {
