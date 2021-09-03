7883934eea43d497220f8ced09b43e9eedfd41cd
Alex Sorafumo <alex@yuion.net>
2021-09-03 16:12:20 +1000

    fix(workplace): add proper mobile template for nav menu

--- apps/workplace/src/app/components/nav-menu.component.ts
+++ apps/workplace/src/app/components/nav-menu.component.ts
@@ -7,21 +7,16 @@ import { OrganisationService } from '@placeos/organisation';
     template: `
         <div class="absolute hidden sm:w-56"></div>
         <div
-            class="flex sm:flex-col bg-white overflow-auto text-black shadow divide-y divide-gray-100 w-full sm:w-auto h-auto sm:h-full border-t sm:border-t border-gray-300 relative p-2"
+            class="hidden sm:flex flex-col h-full bg-white overflow-auto text-black shadow divide-y divide-gray-100 relative p-2"
             (mouseenter)="hidden ? entered() : ''"
         >
-            <div class="sm:hidden block h-16 min-w-[4rem] relative -left-1 top-1">
-                <ng-container
-                    *ngTemplateOutlet="building_button"
-                ></ng-container>
-            </div>
             <a
                 mat-icon-button
-                class="h-16 w-16 sm:h-12 rounded sm:mb-2 sm:hover:bg-gray-200"
+                class="h-12 rounded mb-2 hover:bg-gray-200"
                 *ngFor="let item of menu_items"
                 [routerLink]="[item.route]"
-                [class.sm:w-56]="!hidden"
-                [class.sm:w-12]="hidden"
+                [class.w-56]="!hidden"
+                [class.w-12]="hidden"
                 routerLinkActive="active"
             >
                 <div
@@ -42,44 +37,39 @@ import { OrganisationService } from '@placeos/organisation';
                 </div>
             </a>
         </div>
-        <div class="hidden sm:block">
-            <ng-container *ngTemplateOutlet="building_button"></ng-container>
-        </div>
-        <ng-template #building_button>
-            <button
-                mat-icon-button
-                *ngIf="(buildings | async)?.length > 0"
-                [matMenuTriggerFor]="menu"
-                class="absolute bottom-1 left-1 h-16 w-16 sm:h-12 rounded sm:mb-2 sm:hover:bg-gray-200"
+        <button
+            mat-icon-button
+            *ngIf="(buildings | async)?.length > 0"
+            [matMenuTriggerFor]="menu"
+            class="absolute hidden sm:block bottom-1 left-1 h-16 w-16 sm:h-12 rounded sm:mb-2 sm:hover:bg-gray-200"
+            [class.sm:w-56]="!hidden"
+            [class.sm:w-12]="hidden"
+        >
+            <div
+                class="flex flex-col sm:flex-row items-center justify-start w-16 px-2"
                 [class.sm:w-56]="!hidden"
-                [class.sm:w-12]="hidden"
             >
+                <app-icon>business</app-icon>
                 <div
-                    class="flex flex-col sm:flex-row items-center justify-start w-16 px-2"
-                    [class.sm:w-56]="!hidden"
+                    text
+                    class="text-sm font-normal h-0 sm:h-auto overflow-hidden opacity-0 text-left flex items-center"
+                    [class.sm:w-0]="hidden"
+                    [class.sm:w-48]="!hidden"
+                    [class.sm:opacity-100]="!hidden"
+                    [class.sm:ml-2]="!hidden"
                 >
-                    <app-icon>business</app-icon>
+                    <div class="truncate flex-1 w-px">Building</div>
                     <div
-                        text
-                        class="text-sm font-normal h-0 sm:h-auto overflow-hidden opacity-0 text-left flex items-center"
-                        [class.sm:w-0]="hidden"
-                        [class.sm:w-48]="!hidden"
-                        [class.sm:opacity-100]="!hidden"
-                        [class.sm:ml-2]="!hidden"
+                        class="text-xs ml-2 px-2 py-1 bg-primary text-white rounded"
                     >
-                        <div class="truncate flex-1 w-px">Building</div>
-                        <div
-                            class="text-xs ml-2 px-2 py-1 bg-primary text-white rounded"
-                        >
-                            {{
-                                (building | async)?.display_name ||
-                                    (building | async)?.name
-                            }}
-                        </div>
+                        {{
+                            (building | async)?.display_name ||
+                                (building | async)?.name
+                        }}
                     </div>
                 </div>
-            </button>
-        </ng-template>
+            </div>
+        </button>
         <mat-menu #menu="matMenu">
             <button
                 mat-menu-item
@@ -106,6 +96,59 @@ import { OrganisationService } from '@placeos/organisation';
                 {{ hidden ? 'chevron_right' : 'chevron_left' }}
             </app-icon>
         </button>
+        <div class="flex items-center sm:hidden bg-white border-t border-gray-300">
+            <a
+                *ngFor="let item of menu_items | slice: 0:3"
+                [routerLink]="[item.route]"
+                class="flex items-center justify-center h-12 w-16 flex-1"
+                routerLinkActive="active"
+            >
+                <app-icon class="text-2xl" [icon]="item.icon"></app-icon>
+            </a>
+            <button
+                mat-icon-button
+                class="rounded h-12 w-16"
+                [matMenuTriggerFor]="mobile_menu"
+            >
+                <app-icon>more_vert</app-icon>
+            </button>
+            <mat-menu #mobile_menu="matMenu">
+                <a
+                    mat-menu-item
+                    *ngFor="let item of menu_items | slice: 3"
+                    [routerLink]="[item.route]"
+                routerLinkActive="active"
+                >
+                    <div class="flex items-center space-x-2">
+                        <app-icon class="text-xl" [icon]="item.icon"></app-icon>
+                        <div>{{ item.name }}</div>
+                    </div>
+                </a>
+                <button
+                    mat-menu-item
+                    *ngIf="(buildings | async)?.length > 0"
+                    [matMenuTriggerFor]="menu"
+                >
+                    <div class="flex items-center w-48 space-x-2">
+                        <app-icon class="text-xl">business</app-icon>
+                        <div
+                            text
+                            class="flex items-center flex-1"
+                        >
+                            <div class="truncate flex-1 w-px">Building</div>
+                            <div
+                                class="text-xs ml-2 px-2 py-1 bg-primary text-white rounded"
+                            >
+                                {{
+                                    (building | async)?.display_name ||
+                                        (building | async)?.name
+                                }}
+                            </div>
+                        </div>
+                    </div>
+                </button>
+            </mat-menu>
+        </div>
     `,
     styles: [
         `
