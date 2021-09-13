1aa1f7aaa0f2433f0478b69f8115dce058768775
Alex Sorafumo <alex@yuion.net>
2021-09-13 10:09:12 +1000

    fix(explore): default zone labels to center of zone

--- libs/explore/src/lib/explore-zones.service.ts
+++ libs/explore/src/lib/explore-zones.service.ts
@@ -78,7 +78,11 @@ export class ExploreZonesService extends BaseClass {
             if (areas) {
                 for (const area of areas) {
                     this._capacity[area.id] = area.properties?.capacity || 100;
-                    this._location[area.id] = area.properties?.label_location;
+                    this._location[area.id] =
+                        area.properties?.label_location ||
+                        area.geometry?.coordinates?.length
+                            ? getCenterPoint(area.geometry?.coordinates)
+                            : null;
                     this._draw[area.id] = !!area.properties?.draw_polygon;
                     this._points[area.id] = area.geometry?.coordinates || [];
                 }
