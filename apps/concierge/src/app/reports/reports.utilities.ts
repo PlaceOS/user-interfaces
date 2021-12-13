4dc3f48650d88e515e9ee3fff9dab3c82802e044
Alex Sorafumo <alex@yuion.net>
2021-12-14 09:56:44 +1100

    fix(concierge): limit report data to 2 decimal places

--- apps/concierge/src/app/reports/reports.utilities.ts
+++ apps/concierge/src/app/reports/reports.utilities.ts
@@ -8,7 +8,8 @@ export function generateReportForDeskBookings(
     counts: HashMap<number> = {}
 ) {
     const total = Object.keys(counts).reduce((c, i) => c + (counts[i] || 0), 0);
-    const utilisation = bookings.length / total / util_period;
+    const utilisation =
+        Math.floor((bookings.length / total / util_period) * 100) / 100;
     return {
         total,
         count: bookings.length,
@@ -24,13 +25,21 @@ export function generateReportForBookings(
     const total_users = bookings.reduce((c, i) => c + i.attendees.length, 0);
     const total_capacity = bookings.reduce((c, i) => c + i.system?.capacity, 0);
     const utilisation =
-        bookings.reduce((c, i) => c + i.duration, 0) / (util_period * 60);
-    const occupancy = total_users / total_capacity;
+        Math.floor(
+            (bookings.reduce((c, i) => c + i.duration, 0) /
+                (util_period * 60)) *
+                100
+        ) / 100;
+    const occupancy = Math.floor((total_users / total_capacity) * 100) / 100;
     return {
         count: bookings.length,
         avg_length:
-            bookings.reduce((c, i) => c + i.duration, 0) / bookings.length,
-        efficiency: (utilisation + occupancy) / 2,
+            Math.floor(
+                (bookings.reduce((c, i) => c + i.duration, 0) /
+                    bookings.length) *
+                    100
+            ) / 100,
+        efficiency: Math.floor(((utilisation + occupancy) / 2) * 100) / 100,
         total_users,
         total_capacity,
         occupancy,
