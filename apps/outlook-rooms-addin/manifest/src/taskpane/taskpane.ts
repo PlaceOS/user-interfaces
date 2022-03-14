2d533801fc445db5180c322b81717e7cf4f66f18
JulieGiles <julie_j_giles@hotmail.com>
2022-03-14 14:02:25 +0545

    refactor: worked on error message about webpack plugin

---
+++ apps/outlook-rooms-addin/manifest/src/taskpane/taskpane.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office */
+
+Office.onReady((info) => {
+    if (info.host === Office.HostType.Outlook) {
+        document.getElementById("sideload-msg").style.display = "none";
+        document.getElementById("app-body").style.display = "flex";
+        document.getElementById("run").onclick = run;
+    }
+});
+
+export async function run() {
+    /**
+     * Insert your Outlook code here
+     */
+    // Get a reference to the current message
+    var item = Office.context.mailbox.item;
+
+    // Write message property value to the task pane
+    document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
+}
