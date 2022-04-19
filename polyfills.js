4e6bd2103ebff7f9ecf1ca2d27bd91d5c1d3406a
MrYuion <alex@yuion.net>
2022-04-19 01:37:36 +0000

    build(caterer-ui): CI build from 8adedf8

--- polyfills.js
+++ polyfills.js
@@ -1,6 +1,6 @@
 "use strict";(self.webpackChunkcaterer_ui=self.webpackChunkcaterer_ui||[]).push([["polyfills"],{8923:(De,de,Te)=>{
 /**
-       * @license Angular v13.2.0
+       * @license Angular v13.3.3
        * (c) 2010-2022 Google LLC. https://angular.io/
        * License: MIT
        */
