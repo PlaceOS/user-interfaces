8fde3aa72ba208752f70adcfb5a263f5a916a11a
MrYuion <alex@yuion.net>
2022-04-19 01:37:51 +0000

    build(booking-panel): CI build from 8adedf8

--- polyfills.js
+++ polyfills.js
@@ -1,6 +1,6 @@
 "use strict";(self.webpackChunkbooking_panel=self.webpackChunkbooking_panel||[]).push([["polyfills"],{8431:(De,de,Te)=>{
 /**
-       * @license Angular v13.2.0
+       * @license Angular v13.3.3
        * (c) 2010-2022 Google LLC. https://angular.io/
        * License: MIT
        */
