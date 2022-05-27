60a84965d6caf8ce6ef9cc9b4caf2b4e20a5fd76
julie <julie_j_giles@hotmail.com>
2022-05-27 10:30:29 +1000

    refactor: setting properties and typing in existing-bookings service

--- apps/outlook-rooms-addin/src/app/rooms/existing-bookings.service.ts
+++ apps/outlook-rooms-addin/src/app/rooms/existing-bookings.service.ts
@@ -1,11 +1,36 @@
 import { Injectable } from '@angular/core';
 import { BaseClass } from '@placeos/common';
+import { queryBookings } from '@placeos/bookings';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { CalendarEvent } from '@placeos/events';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ExistingBookingsService extends BaseClass {
+    private _loading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+        false
+    );
+    public loading$: Observable<boolean> = this._loading.asObservable();
+    private _date: BehaviorSubject<number> = new BehaviorSubject(Date.now());
+
+    private _bookings: BehaviorSubject<CalendarEvent[]> = new BehaviorSubject<
+        CalendarEvent[]
+    >(null);
+    public bookings$: Observable<CalendarEvent[]> =
+        this._bookings.asObservable();
+
+    get bookings() {
+        return this._bookings.getValue();
+    }
+
+    set bookings(bookings: CalendarEvent[]) {
+        this._bookings.next(bookings);
+    }
+
     constructor() {
         super();
     }
+
+    getBookings() {}
 }
