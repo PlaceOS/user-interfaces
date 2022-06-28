1d6f2b48fe23423fe35bfc8a9bd4ffe132ec6723
julie <julie_j_giles@hotmail.com>
2022-06-28 19:25:33 +1000

    test(outlook addin): worked on spec for room-booking component but encountering error when trying to mock the form

--- apps/outlook-rooms-addin/src/app/rooms/room-booking.component.spec.ts
+++ apps/outlook-rooms-addin/src/app/rooms/room-booking.component.spec.ts
@@ -1,24 +1,43 @@
-import { DebugElement } from '@angular/core';
-import { createComponentFactory, Spectator } from '@ngneat/spectator';
-import { BookModule } from './book.module';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { EventFormService } from '@placeos/events';
 import { RoomBookingComponent } from './room-booking.component';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import {
+    FormsModule,
+    ReactiveFormsModule,
+    FormGroup,
+    FormControl,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { Router } from '@angular/router';
+import { MockComponent } from 'ng-mocks';
+import { EventFormService } from '@placeos/events';
 
 describe('RoomBookingComponent', () => {
     class routerStub {
         navigate = jest.fn();
     }
+
     let spectator: Spectator<RoomBookingComponent>;
+    let component: RoomBookingComponent;
+
+    let form = new FormGroup({
+        title: new FormControl(''),
+        date: new FormControl(new Date()),
+        duration: new FormControl(234),
+    });
+
+    // const form = new FormBuilder(\);
+
+    // class form {
+    //     patchValue = jest.fn((i: Object) => jest.fn());
+    // }
+
     const createComponent = createComponentFactory({
         component: RoomBookingComponent,
-        imports: [FormGroup, ReactiveFormsModule, MatFormFieldModule],
+        imports: [ReactiveFormsModule, FormsModule, MatFormFieldModule],
         providers: [
             {
                 provide: Router,
-                useClass: { routerStub },
+                useValue: routerStub,
             },
             {
                 provide: EventFormService,
@@ -33,6 +52,7 @@ describe('RoomBookingComponent', () => {
                     postForm: jest.fn(),
                     view: '',
                     last_success: null,
+                    form: form,
                 },
             },
         ],
@@ -40,7 +60,7 @@ describe('RoomBookingComponent', () => {
 
     beforeEach(() => (spectator = createComponent()));
 
-    it('should create', () => {
+    it('should create component', () => {
         expect(spectator.component).toBeTruthy();
     });
 });
