d7e05b25d8c052b51f756460b841bae5b03ece1d
Alex Sorafumo <alex@yuion.net>
2022-08-02 14:41:07 +1000

    fix: fix redirect component initial redirect

--- libs/components/src/lib/redirect.component.ts
+++ libs/components/src/lib/redirect.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { SettingsService } from "@placeos/common";
 import { OrganisationService } from "@placeos/organisation";
-import { take } from "rxjs/operators";
+import { first } from "rxjs/operators";
 
 @Component({
     selector: 'page-redirect',
@@ -13,11 +13,9 @@ export class RedirectComponent implements OnInit {
     constructor(private _settings: SettingsService, private _org: OrganisationService, private _router: Router) {}
 
     public async ngOnInit() {
-        await this._settings.initialised.pipe(take(1)).toPromise();
-        await this._org.initialised.pipe(take(1)).toPromise();
-        setTimeout(() => {
-            if (!this._settings.get('app.default_route')) return;
-            this._router.navigate(this._settings.get('app.default_route').split('/'));
-        }, 100);
+        await this._settings.initialised.pipe(first(_ => _)).toPromise();
+        await this._org.initialised.pipe(first(_ => _)).toPromise();
+        if (!this._settings.get('app.default_route')) return;
+        this._router.navigate(this._settings.get('app.default_route').split('/'));
     }
 }
