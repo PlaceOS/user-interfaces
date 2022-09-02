f8629a676d0bb115934dd1a63477e8fd3d47c139
julie <julie_j_giles@hotmail.com>
2022-09-02 14:04:15 +1000

    chore(survey): update project settings

--- apps/survey/src/app/app.module.ts
+++ apps/survey/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { CreateSurveyFormComponent } from './create-survey/create-survey-form/create-survey-form.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, CreateSurveyFormComponent],
     imports: [
         BrowserModule,
         RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
