99c6816a07f2cf566e17e6f16f7d9d8b4b6c87ed
Alex Sorafumo <alex@yuion.net>
2022-10-20 14:58:59 +1100

    fix(concierge): fix rendering of space report table

--- apps/concierge/src/app/reports/spaces/report-spaces-space-listing.component.ts
+++ apps/concierge/src/app/reports/spaces/report-spaces-space-listing.component.ts
@@ -29,10 +29,10 @@ import { ReportsStateService } from '../reports-state.service';
                 [template]="{ name: space_name, capacity: space_capacity }"
             ></custom-table>
             <ng-template #space_name let-row="row">
-                {{ (row.id | space)?.display_name || (row.id | space)?.name }}
+                {{ (row?.id | space)?.display_name || (row?.id | space)?.name }}
             </ng-template>
-            <ng-template #space_capacity>
-                {{ (row.id | space)?.capacity || 2 }}
+            <ng-template #space_capacity let-row="row">
+                {{ (row?.id | space)?.capacity || 2 }}
             </ng-template>
         </div>
     `,
