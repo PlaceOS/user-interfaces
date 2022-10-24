45a913adcea25a1f7e4b51d800ed4e67895b84e1
Alex Sorafumo <alex@yuion.net>
2022-10-24 15:54:35 +1100

    fix(spaces): tweak space pipe retrieving data

--- libs/spaces/src/lib/space.pipe.ts
+++ libs/spaces/src/lib/space.pipe.ts
@@ -1,40 +1,46 @@
-import { Pipe } from "@angular/core";
-import { querySystems, showSystem } from "@placeos/ts-client";
+import { Pipe } from '@angular/core';
+import { querySystems, showSystem } from '@placeos/ts-client';
 
-import { OrganisationService } from "libs/organisation/src/lib/organisation.service";
-import { Space } from "./space.class";
+import { OrganisationService } from 'libs/organisation/src/lib/organisation.service';
+import { Space } from './space.class';
 
 const SPACE_LIST: Space[] = [];
 
 const EMPTY_SPACE = new Space();
 
 @Pipe({
-    name: 'space'
+    name: 'space',
 })
 export class SpacePipe {
-
     constructor(private _org: OrganisationService) {}
     /**
      * Get details of the space with the given ID
      * @param space_id ID or Email of the space
      */
     public async transform(space_id: string): Promise<Space> {
+        const is_email = space_id.includes('@');
         if (!space_id) return EMPTY_SPACE;
-        let space = SPACE_LIST.find(({ id, email }) => id === space_id || email === space_id);
+        let space = SPACE_LIST.find(
+            ({ id, email }) => id === space_id || email === space_id
+        );
         if (space) return space;
-        const system = await showSystem(space_id).toPromise().catch(_ => null);
-        if (system) {
-            space = new Space({
-                ...system as any,
-                level: this._org.levelWithID([...system.zones]),
-            });
-            SPACE_LIST.push(space);
-            return space;
+        if (!is_email) {
+            const system = await showSystem(space_id)
+                .toPromise()
+                .catch((_) => null);
+            if (system) {
+                space = new Space({
+                    ...(system as any),
+                    level: this._org.levelWithID([...system.zones]),
+                });
+                SPACE_LIST.push(space);
+                return space;
+            }
         }
         const systems = (await querySystems({ q: space_id }).toPromise()).data;
         if (systems.length === 1) {
             space = new Space({
-                ...systems[0] as any,
+                ...(systems[0] as any),
                 level: this._org.levelWithID([...systems[0].zones]),
             });
             SPACE_LIST.push(space);
