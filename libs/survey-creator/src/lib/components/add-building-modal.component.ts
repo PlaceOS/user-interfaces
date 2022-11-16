41d9ab340c83a11b78153da86304d2c4ba8a2971
julie <julie_j_giles@hotmail.com>
2022-11-16 23:20:34 +1100

    style(survey): update template of Add Building Modal

--- libs/survey-creator/src/lib/components/add-building-modal.component.ts
+++ libs/survey-creator/src/lib/components/add-building-modal.component.ts
@@ -1,12 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
     selector: 'add-building-modal',
-    template: ``,
+    template: `
+        <section>
+            <div class="header">
+                <span class="dialog-title">Add Building</span>
+                <span class="close" (click)="closeDialog()">
+                    <mat-icon
+                        aria-hidden="false"
+                        aria-label="Material icon for closing dialog"
+                        class="icon"
+                        >close</mat-icon
+                    >
+                </span>
+            </div>
+            <main>
+                <div>
+                    <button
+                        mat-button
+                        class="add-another-question"
+                        (click)="addAnotherBuilding()"
+                    >
+                        Add another
+                    </button>
+                </div>
+            </main>
+            <footer>
+                <button
+                    mat-button
+                    class="cancel-button"
+                    color="basic"
+                    (click)="closeDialog()"
+                >
+                    Cancel
+                </button>
+                <button
+                    mat-button
+                    class="add-button"
+                    color="primary"
+                    (click)="addQuestion()"
+                >
+                    Add
+                </button>
+            </footer>
+        </section>
+    `,
     styles: [``],
 })
 export class AddBuildingModalComponent implements OnInit {
-    constructor() {}
+    constructor(public dialogRef: MatDialogRef<AddBuildingModalComponent>) {}
 
     ngOnInit(): void {}
+
+    closeDialog() {
+        this.dialogRef.close();
+    }
+
+    addAnotherBuilding(): void {
+        return;
+    }
 }
