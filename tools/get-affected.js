4fabf6e2f060ec30c073d2b141ce6fcb7796d378
Alex Sorafumo <alex@yuion.net>
2022-12-22 18:21:06 +1100

    ci: fix to change check

--- tools/get-affected.js
+++ tools/get-affected.js
@@ -1,9 +1,10 @@
 const execSync = require('child_process').execSync;
-const ref = process.argv[2] || 'origin/develop';
+const ref = (process.argv[2] || 'origin/develop').replace('heads/', 'origin/');
 const baseSha = ref;
 const cmd = process.argv[3] || 'build';
 
 // prints an object with keys {lint1: [...], lint2: [...], lint3: [...], test1: [...], .... build3: [...]}
+console.log(ref, cmd);
 console.log(JSON.stringify(commands(cmd)));
 
 function commands(target) {
@@ -11,6 +12,10 @@ function commands(target) {
     const base = release ? '' : `--base=${baseSha}~1`;
     const array = execSync(
         `npx nx print-affected --target=${target} --select=tasks.target.project ${base}`
-    ).toString().replace(/\n/g, '').split(', ').filter(_ => !!_);
+    )
+        .toString()
+        .replace(/\n/g, '')
+        .split(', ')
+        .filter((_) => !!_);
     return array;
 }
