7300c5edba96a5719f7206c9924a1420d0dc28de
Alex Sorafumo <alex@yuion.net>
2023-01-10 09:35:36 +1100

    fix(workplace): fix button styling for desk flow

--- apps/workplace/src/app/book/new-desk-flow/new-desk-flow-form.component.ts
+++ apps/workplace/src/app/book/new-desk-flow/new-desk-flow-form.component.ts
@@ -38,6 +38,7 @@ import { NewDeskFlowConfirmComponent } from './new-desk-flow-confirm.component';
                     class="flex flex-col sm:flex-row items-center sm:space-x-2 p-2 sm:px-16 sm:mb-2"
                 >
                     <button
+                        btn
                         matRipple
                         confirm
                         class="w-full sm:w-auto"
