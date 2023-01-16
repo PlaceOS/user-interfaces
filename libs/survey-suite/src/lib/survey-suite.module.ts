f58656a7e113a5b56c03856af8500d03d1019107
Marvin Yoong <marvin@place.technology>
2023-01-16 16:04:10 +1100

    chore: Replace survey-ts-client with ts-client

--- libs/survey-suite/src/lib/survey-suite.module.ts
+++ libs/survey-suite/src/lib/survey-suite.module.ts
@@ -1,34 +1,19 @@
-import { InjectionToken, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SurveyBuilderComponent } from './survey-builder/survey-builder.component';
-import { QuestionComponent } from './question/question.component';
-import { SingleLineTextComponent } from './question/single-line-text.component';
-import { MultiLineTextComponent } from './question/multi-line-text.component';
-import { RatingsComponent } from './question/rating.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SelectionComponent } from './question/selection.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSliderModule } from '@angular/material/slider';
-import { AnswersApi, Configuration, InvitationsApi, QuestionsApi, SurveysApi } from '@placeos/survey-ts-client';
-import { token } from '@placeos/ts-client';
-
-/** API Adapter */
-export const SURVEYS_API_ADAPTER = new InjectionToken<SurveysApi>(null);
-export const QUESTIONS_API_ADAPTER = new InjectionToken<QuestionsApi>(null);
-export const INVITATIONS_API_ADAPTER = new InjectionToken<InvitationsApi>(null);
-export const ANSWERS_API_ADAPTER = new InjectionToken<AnswersApi>(null);
-const ApiConfiguration: Configuration = new Configuration(
-    {basePath: '/', accessToken: token() }
-);
-const surveyApi = new SurveysApi(ApiConfiguration);
-const questionApi = new QuestionsApi(ApiConfiguration);
-const invitationApi = new InvitationsApi(ApiConfiguration);
-const answerApi = new AnswersApi(ApiConfiguration)
+import { MultiLineTextComponent } from './question/multi-line-text.component';
+import { QuestionComponent } from './question/question.component';
+import { RatingsComponent } from './question/rating.component';
+import { SelectionComponent } from './question/selection.component';
+import { SingleLineTextComponent } from './question/single-line-text.component';
+import { SurveyBuilderComponent } from './survey-builder/survey-builder.component';
 
 @NgModule({
     imports: [
@@ -50,24 +35,6 @@ const answerApi = new AnswersApi(ApiConfiguration)
         SelectionComponent,
         MultiLineTextComponent,
         RatingsComponent,
-    ],
-    providers:[
-        {
-            provide: SURVEYS_API_ADAPTER,
-            useValue: surveyApi,
-        },
-        {
-            provide: QUESTIONS_API_ADAPTER,
-            useValue: questionApi
-        },
-        {
-            provide: ANSWERS_API_ADAPTER,
-            useValue: answerApi
-        },
-        {
-            provide: INVITATIONS_API_ADAPTER,
-            useValue: invitationApi
-        }
     ],
     exports: [SurveyBuilderComponent, QuestionComponent],
 })
