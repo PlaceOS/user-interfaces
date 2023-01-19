ac0b684565607c9ec1cbec01f5c44ba849bb8979
Marvin Yoong <marvin@place.technology>
2023-01-19 18:36:01 +1100

    feat(survey): survey runner component

--- apps/concierge/src/app/surveys/services/run-survey.service.ts
+++ apps/concierge/src/app/surveys/services/run-survey.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { notifyError, notifySuccess } from "@placeos/common";
+import { UISurveyObj } from "@placeos/survey-suite";
+import { addAnswer, SurveyAnswer } from "@placeos/ts-client";
+import { BehaviorSubject, of } from "rxjs";
+import { catchError, finalize, first } from "rxjs/operators";
 import { Model } from 'survey-core'
 import { SurveyService } from "./survey.service";
 
@@ -15,7 +19,8 @@ export class RunSurveyService{
         this._loading.next(value);
     }
 
-    survey:Model;
+    surveyModel:Model;
+    uiSurvey:UISurveyObj;
     constructor(
         private _survey:SurveyService
     ){}
@@ -25,6 +30,43 @@ export class RunSurveyService{
         const uiSurvey = await this._survey.getSurveyDetails(survey_id);
         this.loading = '';
         if(!uiSurvey) return;
-        this.survey = new Model(uiSurvey);
+        this.uiSurvey = uiSurvey;
+        this.surveyModel = new Model(uiSurvey);
+        this.surveyModel.onComplete.add((sender:Model) => {
+            this.processSurveyAnswer(sender);
+        })
+    }
+
+    private async processSurveyAnswer(survey:Model){
+        const { id, pages } = this.uiSurvey;
+        const answersMap = survey.data;
+        let answers = [];
+        let questions = [];
+        pages.forEach(p => questions = [...questions,...(p.elements || [])]);
+        questions.forEach(q => {
+            if(answersMap[q.id]){
+                answers.push({
+                    question_id: q.id,
+                    type: q.type,
+                    survey_id: id,
+                    answer_json: answersMap[q.id]
+                })
+            }
+        });
+        console.log("Processed answers", answers);
+        this.saveSurveyAnswer(answers);
+    }
+
+    private async saveSurveyAnswer(answers: Partial<SurveyAnswer>[]){
+        this.loading = 'Saving survey answers...';
+        const res = await addAnswer(answers).pipe(
+            first(),
+            catchError(err => {
+                notifyError('Failed to save survey answers');
+                return of(null);
+            }),
+            finalize(() => this.loading = '')
+        ).toPromise();
+        if(res) notifySuccess('Survey answers have been recorded');
     }
 }
