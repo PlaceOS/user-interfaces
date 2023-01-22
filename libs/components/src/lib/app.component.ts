72bb08af24b2990161260e14f0fff38ea866848a
Alex Sorafumo <alex@yuion.net>
2023-01-23 09:14:06 +1100

    fix: catch errors with initialising locale

--- libs/components/src/lib/app.component.ts
+++ libs/components/src/lib/app.component.ts
@@ -199,22 +199,25 @@ export class AppComponent extends BaseClass implements OnInit {
     }
 
     private _initLocale() {
-        let locale = localStorage.getItem('PLACEOS.locale');
-        const locales = this._settings.get('app.locales') || [];
-        this._translate?.addLangs(locales.map((_) => _.id));
-        if (locale) {
-            this._translate?.use(locale);
-        } else {
-            const list = navigator.languages;
-            for (const lang of list) {
-                locale = locales.find((_) => _.id === lang);
-                if (!locale) locale = locales.find((_) => lang.includes(_.id));
-                if (locale) {
-                    this._translate?.use(lang);
-                    localStorage.setItem('PLACEOS.locale', lang);
-                    break;
+        try {
+            let locale = localStorage.getItem('PLACEOS.locale');
+            const locales = this._settings.get('app.locales') || [];
+            this._translate?.addLangs(locales.map((_) => _.id));
+            if (locale) {
+                this._translate?.use(locale);
+            } else {
+                const list = navigator.languages;
+                for (const lang of list) {
+                    locale = locales.find((_) => _.id === lang);
+                    if (!locale)
+                        locale = locales.find((_) => lang.includes(_.id));
+                    if (locale) {
+                        this._translate?.use(lang);
+                        localStorage.setItem('PLACEOS.locale', lang);
+                        break;
+                    }
                 }
             }
-        }
+        } catch {}
     }
 }
