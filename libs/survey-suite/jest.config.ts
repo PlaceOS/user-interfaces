a1b99ff745cf93faf6c19c72f6426674edebaaf0
Alex Sorafumo <alex@yuion.net>
2023-03-01 23:43:39 +1100

    chore: update project

--- libs/survey-suite/jest.config.ts
+++ libs/survey-suite/jest.config.ts
@@ -3,15 +3,16 @@ export default {
     displayName: 'survey-suite',
     preset: '../../jest.preset.js',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.spec.json',
-            stringifyContentPathRegex: '\\.(html|svg)$',
-        },
-    },
+    globals: {},
     coverageDirectory: '../../coverage/libs/survey-suite',
     transform: {
-        '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+        '^.+\\.(ts|mjs|js|html)$': [
+            'jest-preset-angular',
+            {
+                tsconfig: '<rootDir>/tsconfig.spec.json',
+                stringifyContentPathRegex: '\\.(html|svg)$',
+            },
+        ],
     },
     transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
     snapshotSerializers: [
