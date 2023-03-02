adc260162a62902fff892c0d3c1b83bc4648a4f2
Alex Sorafumo <alex@yuion.net>
2023-03-02 11:18:17 +1100

    test: fix TextEncoder error

--- apps/booking-panel/src/test-setup.ts
+++ apps/booking-panel/src/test-setup.ts
@@ -1,8 +1,11 @@
 import 'jest-preset-angular/setup-jest';
 import '@angular/localize/init';
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
-import { defineGlobalsInjections } from "@ngneat/spectator";
-import { MockModule } from "ng-mocks";
+import { defineGlobalsInjections } from '@ngneat/spectator';
+import { MockModule } from 'ng-mocks';
 import { TranslateModule } from '@ngx-translate/core';
 
 defineGlobalsInjections({
