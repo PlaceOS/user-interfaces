3896910ca9939d885559b21a21d5672dd8fd7e6f
julie <julie_j_giles@hotmail.com>
2023-05-03 11:45:44 +1000

    PPT-472 test: add spec to check menu opens next to each colleague when clicked

--- apps/workplace-e2e/src/integration/app.cy.ts
+++ apps/workplace-e2e/src/integration/app.cy.ts
@@ -13,4 +13,17 @@ describe('workplace', () => {
         cy.get('app-landing landing-colleagues').as('colleagueComponent');
         cy.get('@colleagueComponent').should('be.visible');
     });
+
+    it('should display a menu when the ellipse button is clicked next to each colleague', () => {
+        cy.get('global-loading');
+        cy.get('app-landing', { timeout: 6000 });
+        cy.get('landing-colleagues').then(($childComponent) => {
+            const childComponent = $childComponent[0];
+            cy.get('button[name="colleague-more"]', {
+                withinSubject: childComponent,
+            }).each(($btn) => {
+                cy.wrap($btn).click({ force: true });
+            });
+        });
+    });
 });
