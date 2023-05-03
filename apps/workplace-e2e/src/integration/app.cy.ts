156a3ef8c57e5e9d8ec66fd94d325a9a619f43b9
julie <julie_j_giles@hotmail.com>
2023-05-03 15:25:01 +1000

    PPT-472 test: add spec for opening and closing Add colleague modal

--- apps/workplace-e2e/src/integration/app.cy.ts
+++ apps/workplace-e2e/src/integration/app.cy.ts
@@ -6,7 +6,7 @@ describe('workplace', () => {
         cy.get('app-landing', { timeout: 6000 });
     });
 
-    //Side bar - colleagues tab
+    // Side bar - colleagues tab
     it('should display the colleague tab by default', () => {
         cy.get('global-loading');
         cy.get('app-landing', { timeout: 6000 });
@@ -17,12 +17,57 @@ describe('workplace', () => {
     it('should display a menu when the ellipse button is clicked next to each colleague', () => {
         cy.get('global-loading');
         cy.get('app-landing', { timeout: 6000 });
+        cy.get('.cdk-overlay-pane').should('not.exist');
         cy.get('landing-colleagues').then(($childComponent) => {
             const childComponent = $childComponent[0];
             cy.get('button[name="colleague-more"]', {
                 withinSubject: childComponent,
+            })
+                .first()
+                .click()
+                .then(() => {
+                    cy.wait(6000);
+                    cy.get('.cdk-overlay-pane', {
+                        timeout: 6000,
+                    }).should('be.visible');
+                });
+        });
+    });
+
+    it('should display an input search field to add user when Add button is clicked', () => {
+        cy.get('global-loading');
+        cy.get('app-landing', { timeout: 6000 });
+        cy.get('landing-colleagues').then(($childComponent) => {
+            const childComponent = $childComponent[0];
+            cy.get('button[name="open-colleague-search"]', {
+                withinSubject: childComponent,
             }).each(($btn) => {
-                cy.wrap($btn).click({ force: true });
+                cy.wrap($btn)
+                    .click({ force: true })
+                    .then(() => {
+                        cy.get('input').should('be.visible');
+                    });
+            });
+        });
+    });
+
+    it('should close the search modal when the close button is clicked', () => {
+        cy.get('global-loading');
+        cy.get('app-landing', { timeout: 6000 });
+        cy.get('landing-colleagues').then(($childComponent) => {
+            const childComponent = $childComponent[0];
+            cy.get('button[name="open-colleague-search"]', {
+                withinSubject: childComponent,
+            }).each(($btn) => {
+                cy.wrap($btn)
+                    .click({ force: true })
+                    .then(() => {
+                        cy.get('button[name="close-colleague-search"]')
+                            .click()
+                            .then(() => {
+                                cy.get('input').should('not.be.visible');
+                            });
+                    });
             });
         });
     });
