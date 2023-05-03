8ca46b05e6efd507737de3fe5f3724aa358c2d37
julie <julie_j_giles@hotmail.com>
2023-05-03 10:45:32 +1000

    PPT-472: add spec for colleague tab displaying by default

--- apps/workplace-e2e/src/integration/app.cy.ts
+++ apps/workplace-e2e/src/integration/app.cy.ts
@@ -5,4 +5,12 @@ describe('workplace', () => {
         cy.get('global-loading');
         cy.get('app-landing', { timeout: 6000 });
     });
+
+    //Side bar - colleagues tab
+    it('should display the colleague tab by default', () => {
+        cy.get('global-loading');
+        cy.get('app-landing', { timeout: 6000 });
+        cy.get('app-landing landing-colleagues').as('colleagueComponent');
+        cy.get('@colleagueComponent').should('be.visible');
+    });
 });
