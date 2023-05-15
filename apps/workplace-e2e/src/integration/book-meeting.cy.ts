b87d93b5b1dab24c820af19fd3da8fd812cc6386
julie <julie_j_giles@hotmail.com>
2023-05-15 14:02:31 +1000

    PPT-473 spec: meeting title to be required field spec failing as it is not required in app

--- apps/workplace-e2e/src/integration/book-meeting.cy.ts
+++ apps/workplace-e2e/src/integration/book-meeting.cy.ts
@@ -5,4 +5,13 @@ describe('Booking Meetings', () => {
         cy.get('global-loading');
         cy.get('meeting-flow-form');
     });
+
+    it('should require a meeting title to be entered before proceeding to booking confirmation', () => {
+        cy.get('global-loading');
+        cy.get('meeting-flow-form');
+        cy.get('input[name="title"]')
+            .invoke('val')
+            .should((value) => expect(value).to.not.be.null);
+        cy.get('input[name="title"]').should('have.attr', 'required');
+    });
 });
