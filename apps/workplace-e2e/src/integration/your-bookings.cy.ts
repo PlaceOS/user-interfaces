619d4ca1cd7b9425660e6b88b41e95459c44200a
julie <julie_j_giles@hotmail.com>
2023-06-14 14:15:33 +1000

    PPT-476 spec: add test to check Booking Details modal is visible when clicked and is close-able

--- apps/workplace-e2e/src/integration/your-bookings.cy.ts
+++ apps/workplace-e2e/src/integration/your-bookings.cy.ts
@@ -98,85 +98,104 @@ describe('Your Bookings', () => {
     //         });
     // });
 
-    it('should allow a future date to be selected in the calendar view to view a future booking', () => {
-        cy.visit('/#/book/meeting?mock=true');
-        cy.get('input[name="title"]')
-            .type('Cypress Future Booking')
-            .then(() => {
-                cy.get('mat-datepicker-toggle')
-                    .find('button')
-                    .first()
-                    .click({ force: true })
-                    .then(() => {
-                        cy.get('mat-calendar-header')
-                            .find('button[aria-label="Next month"]')
-                            .click({ force: true })
-                            .then(() => {
-                                cy.get('mat-month-view')
-                                    .contains('button', '20')
-                                    .click({
-                                        force: true,
-                                    });
-                            });
-                    });
+    // it('should allow a future date to be selected in the calendar view to view a future booking', () => {
+    //     cy.visit('/#/book/meeting?mock=true');
+    //     cy.get('input[name="title"]')
+    //         .type('Cypress Future Booking')
+    //         .then(() => {
+    //             cy.get('mat-datepicker-toggle')
+    //                 .find('button')
+    //                 .first()
+    //                 .click({ force: true })
+    //                 .then(() => {
+    //                     cy.get('mat-calendar-header')
+    //                         .find('button[aria-label="Next month"]')
+    //                         .click({ force: true })
+    //                         .then(() => {
+    //                             cy.get('mat-month-view')
+    //                                 .contains('button', '20')
+    //                                 .click({
+    //                                     force: true,
+    //                                 });
+    //                         });
+    //                 });
 
-                cy.get('button[name="add-space"]')
-                    .click({ force: true })
-                    .then(() => {
-                        cy.wait(6000);
-                        cy.get('button[name="select-space"]')
-                            .first()
-                            .click({ force: true })
-                            .then(() => {
-                                cy.get('button[name="toggle-space"]')
-                                    .click({ force: true })
-                                    .then(() => {
-                                        cy.get(
-                                            'button[name="open-meeting-confirm"]'
-                                        )
-                                            .click({ force: true })
-                                            .then(() => {
-                                                cy.get(
-                                                    'button[name="confirm-meeting"]'
-                                                )
-                                                    .click({ force: true })
-                                                    .then(() => {
-                                                        cy.wait(3000);
-                                                        cy.get(
-                                                            'a[name="meeting-created-continue"]'
-                                                        ).click({
-                                                            force: true,
-                                                        });
-                                                    });
-                                            });
-                                    });
-                            });
-                    });
-            });
+    //             cy.get('button[name="add-space"]')
+    //                 .click({ force: true })
+    //                 .then(() => {
+    //                     cy.wait(6000);
+    //                     cy.get('button[name="select-space"]')
+    //                         .first()
+    //                         .click({ force: true })
+    //                         .then(() => {
+    //                             cy.get('button[name="toggle-space"]')
+    //                                 .click({ force: true })
+    //                                 .then(() => {
+    //                                     cy.get(
+    //                                         'button[name="open-meeting-confirm"]'
+    //                                     )
+    //                                         .click({ force: true })
+    //                                         .then(() => {
+    //                                             cy.get(
+    //                                                 'button[name="confirm-meeting"]'
+    //                                             )
+    //                                                 .click({ force: true })
+    //                                                 .then(() => {
+    //                                                     cy.wait(3000);
+    //                                                     cy.get(
+    //                                                         'a[name="meeting-created-continue"]'
+    //                                                     ).click({
+    //                                                         force: true,
+    //                                                     });
+    //                                                 });
+    //                                         });
+    //                                 });
+    //                         });
+    //                 });
+    //         });
 
-        cy.visit('/#/your-bookings?mock=true');
-        cy.get('schedule-calendar')
-            .find('button')
-            .eq(1)
+    //     cy.visit('/#/your-bookings?mock=true');
+    //     cy.get('schedule-calendar')
+    //         .find('button')
+    //         .eq(1)
+    //         .click({ force: true })
+    //         .then(() => {
+    //             cy.contains('button[name="schedule-set-date"]', '20')
+
+    //                 .click({ force: true })
+    //                 .then(() => {
+    //                     cy.wait(3000);
+    //                     cy.get('event-card')
+    //                         .contains('Cypress Future Booking')
+    //                         .click({ force: true })
+    //                         .then(() => {
+    //                             cy.get('event-details-modal').should(
+    //                                 'be.visible'
+    //                             );
+    //                             cy.contains('Cypress Future Booking').should(
+    //                                 'exist'
+    //                             );
+    //                         });
+    //                 });
+    //         });
+    // });
+
+    it('should open the booking details modal when a booking is clicked and should be close-able', () => {
+        cy.get('.cdk-overlay-container').should('not.be.visible');
+        cy.get('booking-card')
+            .find('a')
+            .first()
             .click({ force: true })
             .then(() => {
-                cy.contains('button[name="schedule-set-date"]', '20')
+                cy.wait(3000);
+                cy.get('.cdk-overlay-container').should('be.visible');
+            });
 
-                    .click({ force: true })
-                    .then(() => {
-                        cy.wait(3000);
-                        cy.get('event-card')
-                            .contains('Cypress Future Booking')
-                            .click({ force: true })
-                            .then(() => {
-                                cy.get('event-details-modal').should(
-                                    'be.visible'
-                                );
-                                cy.contains('Cypress Future Booking').should(
-                                    'exist'
-                                );
-                            });
-                    });
+        cy.get('booking-details-modal')
+            .find('button[mat-dialog-close]')
+            .click({ force: true })
+            .then(() => {
+                cy.get('.cdk-overlay-container').should('not.be.visible');
             });
     });
 
