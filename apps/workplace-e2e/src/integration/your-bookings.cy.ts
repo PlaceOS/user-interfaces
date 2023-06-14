12e6c0bd4e6fa8cb6d6af5f9f28aa3d061d9e16a
julie <julie_j_giles@hotmail.com>
2023-06-14 13:59:03 +1000

    PPT-476 spec: add test to check a booking made 1 month in the future shows up in Your Bookings under the correct date

--- apps/workplace-e2e/src/integration/your-bookings.cy.ts
+++ apps/workplace-e2e/src/integration/your-bookings.cy.ts
@@ -73,28 +73,110 @@ describe('Your Bookings', () => {
     //         });
     // });
 
-    it('should remove and add a filter to view bookings', () => {
-        cy.get('schedule-filters').should('be.visible');
-        cy.get('schedule-filters').contains('Lockers').should('be.visible');
-        cy.get('button[name="schedule-remove-locker-filter"]')
-            .click({
-                force: true,
-            })
+    // it('should remove and add a filter to view bookings', () => {
+    //     cy.get('schedule-filters').should('be.visible');
+    //     cy.get('schedule-filters').contains('Lockers').should('be.visible');
+    //     cy.get('button[name="schedule-remove-locker-filter"]')
+    //         .click({
+    //             force: true,
+    //         })
+    //         .then(() => {
+    //             cy.contains('Lockers').should('not.be.visible');
+    //         });
+
+    //     cy.get('button[name="schedule-toggle-locker-filter"]')
+    //         .find('mat-checkbox')
+    //         .should('not.be.checked');
+
+    //     cy.get('button[name="schedule-toggle-locker-filter"]')
+    //         .find('mat-checkbox')
+    //         .click({ force: true })
+    //         .then(() => {
+    //             cy.get('schedule-filters')
+    //                 .contains('Lockers')
+    //                 .should('be.visible');
+    //         });
+    // });
+
+    it('should allow a future date to be selected in the calendar view to view a future booking', () => {
+        cy.visit('/#/book/meeting?mock=true');
+        cy.get('input[name="title"]')
+            .type('Cypress Future Booking')
             .then(() => {
-                cy.contains('Lockers').should('not.be.visible');
+                cy.get('mat-datepicker-toggle')
+                    .find('button')
+                    .first()
+                    .click({ force: true })
+                    .then(() => {
+                        cy.get('mat-calendar-header')
+                            .find('button[aria-label="Next month"]')
+                            .click({ force: true })
+                            .then(() => {
+                                cy.get('mat-month-view')
+                                    .contains('button', '20')
+                                    .click({
+                                        force: true,
+                                    });
+                            });
+                    });
+
+                cy.get('button[name="add-space"]')
+                    .click({ force: true })
+                    .then(() => {
+                        cy.wait(6000);
+                        cy.get('button[name="select-space"]')
+                            .first()
+                            .click({ force: true })
+                            .then(() => {
+                                cy.get('button[name="toggle-space"]')
+                                    .click({ force: true })
+                                    .then(() => {
+                                        cy.get(
+                                            'button[name="open-meeting-confirm"]'
+                                        )
+                                            .click({ force: true })
+                                            .then(() => {
+                                                cy.get(
+                                                    'button[name="confirm-meeting"]'
+                                                )
+                                                    .click({ force: true })
+                                                    .then(() => {
+                                                        cy.wait(3000);
+                                                        cy.get(
+                                                            'a[name="meeting-created-continue"]'
+                                                        ).click({
+                                                            force: true,
+                                                        });
+                                                    });
+                                            });
+                                    });
+                            });
+                    });
             });
 
-        cy.get('button[name="schedule-toggle-locker-filter"]')
-            .find('mat-checkbox')
-            .should('not.be.checked');
-
-        cy.get('button[name="schedule-toggle-locker-filter"]')
-            .find('mat-checkbox')
+        cy.visit('/#/your-bookings?mock=true');
+        cy.get('schedule-calendar')
+            .find('button')
+            .eq(1)
             .click({ force: true })
             .then(() => {
-                cy.get('schedule-filters')
-                    .contains('Lockers')
-                    .should('be.visible');
+                cy.contains('button[name="schedule-set-date"]', '20')
+
+                    .click({ force: true })
+                    .then(() => {
+                        cy.wait(3000);
+                        cy.get('event-card')
+                            .contains('Cypress Future Booking')
+                            .click({ force: true })
+                            .then(() => {
+                                cy.get('event-details-modal').should(
+                                    'be.visible'
+                                );
+                                cy.contains('Cypress Future Booking').should(
+                                    'exist'
+                                );
+                            });
+                    });
             });
     });
 
