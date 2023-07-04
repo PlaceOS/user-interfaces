5d3c0ebb158b7b8e90bb61bc8c617b90cb7e7cd8
Alex Sorafumo <alex@yuion.net>
2023-07-04 16:09:15 +1000

    fix(events): fix formatting for month_day recurrence

--- libs/events/src/lib/helpers.ts
+++ libs/events/src/lib/helpers.ts
@@ -78,6 +78,7 @@ export function parseRecurrence(data: RecurrenceDetails) {
                     (data.occurrences - 1) * data.interval
                 ).valueOf();
                 break;
+            case 'month_day':
             case 'monthly':
                 end = addMonths(
                     start || Date.now(),
