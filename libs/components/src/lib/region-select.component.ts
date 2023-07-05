35290d8a71b334752e811aa1f0b79f8fe8248c17
Alex Sorafumo <alex@yuion.net>
2023-07-06 09:51:07 +1000

    fix: add overflow to building list (PPT-705)

--- libs/components/src/lib/region-select.component.ts
+++ libs/components/src/lib/region-select.component.ts
@@ -6,21 +6,18 @@ import { OrganisationService } from '@placeos/organisation';
     selector: 'region-select',
     template: `
         <div
-            class="flex flex-col w-[18.5rem] rounded bg-white shadow p-2 relative -top-12 -right-1 dark:bg-neutral-700 dark:text-white"
+            class="flex flex-col w-[18.5rem] rounded bg-white shadow p-2 relative -top-12 -right-1 dark:bg-neutral-700 dark:text-white overflow-auto max-h-[65vh]"
             (click)="close()"
         >
             <div class="flex items-center space-x-2">
                 <app-icon class="text-2xl">arrow_back</app-icon>
                 <div class="">
                     {{
-                        (region | async)?.display_name ||
-                            (region | async)?.name
+                        (region | async)?.display_name || (region | async)?.name
                     }}
                 </div>
             </div>
-            <div class="text-xs opacity-60 mb-4 px-2" i18n>
-                Select a region
-            </div>
+            <div class="text-xs opacity-60 mb-4 px-2" i18n>Select a region</div>
             <mat-radio-group
                 [ngModel]="(region | async)?.id"
                 class="flex flex-col space-y-2 px-2"
