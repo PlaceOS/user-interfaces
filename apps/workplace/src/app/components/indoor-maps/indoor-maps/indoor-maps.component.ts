8586a21e1a6b049477a890349e330ae46147d12c
julie <julie_j_giles@hotmail.com>
2023-09-01 15:45:16 +1000

    chore: include google.maps type definition

--- apps/workplace/src/app/components/indoor-maps/indoor-maps/indoor-maps.component.ts
+++ apps/workplace/src/app/components/indoor-maps/indoor-maps/indoor-maps.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
+declare let mapsindoors: any;
 @Component({
-  selector: 'placeos-indoor-maps',
-  templateUrl: './indoor-maps.component.html',
-  styleUrls: ['./indoor-maps.component.css']
+    selector: 'indoor-maps',
+    templateUrl: './indoor-maps.component.html',
+    styles: [``],
 })
 export class IndoorMapsComponent {
+    mapViewOptions: any;
+    mapViewInstance: any;
+    mapsIndoorsInstance: any;
+    googleMapsInstance: any;
 
+    @ViewChild('searchInput', { static: true }) searchElement: ElementRef;
+    @ViewChild('searchResultItems') searchResults: ElementRef;
+
+    async ngOnInit() {
+        await this.initMapView();
+    }
+
+    initMapView(): Promise<void> {
+        this.mapViewOptions = {
+            element: document.getElementById('map'),
+            center: { lat: 30.3603774, lng: -97.7426772 },
+            zoom: 21,
+            maxZoom: 26,
+        };
+        this.mapViewInstance = new mapsindoors.mapView.GoogleMapsView(
+            this.mapViewOptions
+        );
+
+        this.mapsIndoorsInstance = new mapsindoors.MapsIndoors({
+            mapView: this.mapViewInstance,
+        });
+
+        return (this.googleMapsInstance = this.mapViewInstance.getMap());
+    }
 }
