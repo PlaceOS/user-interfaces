48e38403a132143d06d9d934864f385055c0caa4
Alex Sorafumo <alex@yuion.net>
2023-09-04 10:22:47 +1000

    fix: allow user to use new version if they don't use the notification (PPT-865)

--- libs/common/src/lib/application.ts
+++ libs/common/src/lib/application.ts
@@ -4,6 +4,11 @@ import { log } from './general';
 import { notifyInfo } from './notifications';
 
 let _timer: number;
+let _new_version = false;
+
+export function hasNewVersion() {
+    return _new_version;
+}
 
 /**
  * Setup handler for cache change events
@@ -11,10 +16,7 @@ let _timer: number;
  * @param notify Function to call on changes to the cache
  * @param interval Time interval to check the cache for changes
  */
-export function setupCache(
-    cache: SwUpdate,
-    interval: number = 5 * 60 * 1000
-) {
+export function setupCache(cache: SwUpdate, interval: number = 5 * 60 * 1000) {
     if (cache.isEnabled) {
         if (_timer) clearInterval(_timer);
         _timer = <any>setInterval(() => {
@@ -33,11 +35,14 @@ export function clearCacheCheck() {
  *
  */
 async function activateUpdate(cache: SwUpdate) {
-    if (cache.isEnabled && await cache.checkForUpdate()) {
+    if (cache.isEnabled && (await cache.checkForUpdate())) {
         log('CACHE', `Activating changes to the cache...`);
-        if (!await cache.activateUpdate()) return;
-        notifyInfo('Newer version of the application is available', 'Refresh', () =>
-            location.reload()
+        if (!(await cache.activateUpdate())) return;
+        _new_version = true;
+        notifyInfo(
+            'Newer version of the application is available',
+            'Refresh',
+            () => location.reload()
         );
     }
 }
