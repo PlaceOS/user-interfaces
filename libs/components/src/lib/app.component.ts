84161c29a59a11a4ef2eb1fcd7604dce0e18eafb
Alex Sorafumo <alex@yuion.net>
2023-09-07 14:04:53 +1000

    fix: fix upload auth on iOS

--- libs/components/src/lib/app.component.ts
+++ libs/components/src/lib/app.component.ts
@@ -28,6 +28,7 @@ import {
     setupPlace,
     log,
     GoogleAnalyticsService,
+    isMobileSafari,
 } from '@placeos/common';
 import { OrganisationService } from 'libs/organisation/src/lib/organisation.service';
 import { setInternalUserDomain } from 'libs/users/src/lib/user.utilities';
@@ -151,6 +152,14 @@ export class AppComponent extends AsyncHandler implements OnInit {
             location.origin.includes('demo.place.tech');
         /** Wait for authentication details to load */
         await setupPlace(settings).catch((_) => console.error(_));
+        const tkn = token();
+        if (isMobileSafari()) {
+            setCustomHeaders(
+                tkn === 'x-api-key'
+                    ? { 'x-api-key': apiKey() }
+                    : { Authorization: `Bearer ${tkn}` }
+            );
+        }
         await this._org.initialised.pipe(first((_) => _)).toPromise();
         setupCache(this._cache);
         if (!settings.local_login) {
