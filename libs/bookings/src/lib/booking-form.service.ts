eb8d3ef9d8b4eb7af20e9dc9bc65dc82ebcf174d
Alex Sorafumo <alex@yuion.net>
2023-09-19 10:50:19 +1000

    fix(bookings): fix available resources

--- libs/bookings/src/lib/booking-form.service.ts
+++ libs/bookings/src/lib/booking-form.service.ts
@@ -189,53 +189,61 @@ export class BookingFormService extends AsyncHandler {
                 type: options.type,
                 zones: options.zone_id,
             }).pipe(
-                map((bookings) => {
-                    const start = this.form.getRawValue().date;
-                    const end = addMinutes(
-                        start,
-                        this.form.getRawValue().duration
-                    ).valueOf();
-                    const restriction = restrictions.find(
-                        (_) =>
-                            (start >= _.start && start < _.end) ||
-                            (end <= _.end && end > _.start)
-                    );
-                    this._resource_use = {};
-                    bookings.forEach(
-                        (_) => (this._resource_use[_.asset_id] = _.user_name)
-                    );
-                    return resources.filter((asset) => {
-                        const restriction_list = restrictions.filter((_) =>
-                            _.assets.includes(asset.id)
+                map(
+                    (bookings) => {
+                        const start = this.form.getRawValue().date;
+                        const end = addMinutes(
+                            start,
+                            this.form.getRawValue().duration
+                        ).valueOf();
+                        const restriction = restrictions.find(
+                            (_) =>
+                                (start >= _.start && start < _.end) ||
+                                (end <= _.end && end > _.start)
                         );
-                        const is_restricted = restriction_list.find(
-                            (rest) =>
-                                (start >= rest.start && start < rest.end) ||
-                                (end <= rest.end && end > rest.start)
+                        this._resource_use = {};
+                        bookings.forEach(
+                            (_) =>
+                                (this._resource_use[_.asset_id] = _.user_name)
                         );
-                        !is_restricted &&
-                            (!asset.groups?.length ||
-                                asset.groups.some((grp) =>
-                                    currentUser().groups.includes(grp)
-                                )) &&
-                            asset.bookable !== false &&
-                            (!options.features ||
-                                options.features?.every((_) =>
-                                    asset.features.includes(_)
-                                )) &&
-                            (!options.zone_id ||
-                                options.zone_id === asset.zone?.id ||
-                                options.zone_id === asset.zone?.parent_id) &&
-                            !bookings.find(
-                                (bkn) =>
-                                    bkn.asset_id === asset.id &&
-                                    bkn.status !== 'declined'
+                        const available = resources.filter((asset) => {
+                            const restriction_list = restrictions.filter((_) =>
+                                _.assets.includes(asset.id)
                             );
-                    });
-                })
+                            const is_restricted = restriction_list.find(
+                                (rest) =>
+                                    (start >= rest.start && start < rest.end) ||
+                                    (end <= rest.end && end > rest.start)
+                            );
+                            return (
+                                !is_restricted &&
+                                (!asset.groups?.length ||
+                                    asset.groups.some((grp) =>
+                                        currentUser().groups.includes(grp)
+                                    )) &&
+                                asset.bookable !== false &&
+                                (!options.features ||
+                                    options.features?.every((_) =>
+                                        asset.features.includes(_)
+                                    )) &&
+                                (!options.zone_id ||
+                                    options.zone_id === asset.zone?.id ||
+                                    options.zone_id ===
+                                        asset.zone?.parent_id) &&
+                                !bookings.find(
+                                    (bkn) =>
+                                        bkn.asset_id === asset.id &&
+                                        bkn.status !== 'declined'
+                                )
+                            );
+                        });
+                        return available;
+                    },
+                    catchError((_) => of([]))
+                )
             )
         ),
-        tap(() => this._loading.next('')),
+        tap((_) => this._loading.next('')),
         shareReplay(1)
     );
 
