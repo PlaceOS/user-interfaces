b111024c3b403490ee6934245605966aca2ae32c
julie <julie_j_giles@hotmail.com>
2023-10-05 13:47:57 +1100

    refactor: inject script inserting api keys into body instead of head
    chore: update inject-map-api service to inject script into body

--- libs/common/src/lib/inject-map-api.service.ts
+++ libs/common/src/lib/inject-map-api.service.ts
@@ -23,14 +23,14 @@ export class InjectMapApiService {
         if (maps_key) {
             const script = document.createElement('script');
             script.src = `https://app.mapsindoors.com/mapsindoors/js/sdk/4.21.4/mapsindoors-4.21.4.js.gz?apikey=${maps_key}`;
-            document.head.appendChild(script);
+            document.body.appendChild(script);
         }
 
         const google_key = this._settings.get('app.google_maps_api_key');
         if (google_key) {
             const script = document.createElement('script');
             script.src = `https://maps.googleapis.com/maps/api/js?libraries=geometry&key=${google_key}`;
-            document.head.appendChild(script);
+            document.body.appendChild(script);
         }
 
         if (maps_key && google_key) {
