647b395d431e531a925b0e4d5abfd497b45771c8
Alex Sorafumo <alex@yuion.net>
2023-10-24 12:20:11 +1100

    fix(workplace): fix overflow of flow success views (PPT-1000)

--- apps/workplace/src/app/book/new-parking-flow/parking-flow-success.component.ts
+++ apps/workplace/src/app/book/new-parking-flow/parking-flow-success.component.ts
@@ -4,40 +4,44 @@ import { SettingsService } from '@placeos/common';
 
 @Component({
     selector: 'parking-flow-success',
-    template: ` <div class="absolute inset-0 bg-white flex flex-col z-50">
-        <main
-            class="flex-1 flex flex-col items-center justify-center space-y-2 p-8"
-        >
-            <h2 class="text-2xl font-medium" i18n>Parking Space Confirmed!</h2>
-            <p class="text-center max-w-[32rem]" i18n>
-                Your
-                <span>
-                    parking space reservation for
-                    {{ last_event.asset_name }} at {{ location }}, </span
-                >has been successfully booked for the
-                {{ last_event.date | date: 'mediumDate' }} at
-                {{ last_event.date | date: time_format }}-{{
-                    last_event.date + last_event.duration * 60 * 1000
-                        | date: time_format
-                }}.
-            </p>
-            <img src="assets/icons/parking-success.svg" />
-        </main>
-        <footer
-            class="p-2 w-full border-t border-gray-200 mt-4 flex items-center justify-center"
-        >
-            <a
-                button
-                btn
-                matRipple
-                class="w-full max-w-[32rem]"
-                [routerLink]="['/']"
-                i18n
+    template: `
+        <div class="absolute inset-0 bg-white flex flex-col z-50 overflow-auto">
+            <main
+                class="flex-1 flex flex-col items-center justify-center space-y-2 p-8"
             >
-                Great, thanks!
-            </a>
-        </footer>
-    </div>`,
+                <h2 class="text-2xl font-medium" i18n>
+                    Parking Space Confirmed!
+                </h2>
+                <p class="text-center max-w-[32rem]" i18n>
+                    Your
+                    <span>
+                        parking space reservation for
+                        {{ last_event.asset_name }} at {{ location }}, </span
+                    >has been successfully booked for the
+                    {{ last_event.date | date: 'mediumDate' }} at
+                    {{ last_event.date | date: time_format }}-{{
+                        last_event.date + last_event.duration * 60 * 1000
+                            | date: time_format
+                    }}.
+                </p>
+                <img src="assets/icons/parking-success.svg" />
+            </main>
+            <footer
+                class="sticky bottom-0 bg-white p-2 w-full border-t border-gray-200 mt-4 flex items-center justify-center"
+            >
+                <a
+                    button
+                    btn
+                    matRipple
+                    class="w-full max-w-[32rem]"
+                    [routerLink]="['/']"
+                    i18n
+                >
+                    Great, thanks!
+                </a>
+            </footer>
+        </div>
+    `,
     styles: [``],
 })
 export class ParkingFlowSuccessComponent {
