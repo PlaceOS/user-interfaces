725a00e851ecc4972e049d3905b9817c057fd2ec
Alex Sorafumo <alex@yuion.net>
2023-12-04 14:44:31 +1100

    fix(control): simplify shutdown for joined rooms (PPT-1094)

--- apps/control/src/app/ui/power-tooltip.component.ts
+++ apps/control/src/app/ui/power-tooltip.component.ts
@@ -12,17 +12,8 @@ import { ControlStateService } from '../control-state.service';
             <h3 class="mb-2 font-medium text-center">
                 Are you sure you want to shutdown <br />the system?
             </h3>
-            <button btn matRipple class="w-64" (click)="shutdown()">
+            <button btn matRipple class="w-64" (click)="shutdown(true)">
                 Yes, I'm sure
-            </button>
-            <button
-                btn
-                matRipple
-                class="w-64"
-                *ngIf="(joined | async)?.room_ids?.length > 1"
-                (click)="shutdown(true)"
-            >
-                Yes, also unlink joined rooms
             </button>
             <button btn matRipple class="w-64 inverse" (click)="close()">
                 No, go back
