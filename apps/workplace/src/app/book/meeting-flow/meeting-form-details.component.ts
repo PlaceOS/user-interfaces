16ea4168be6db3ef8b17e55bcc127c653408cba1
Alex Sorafumo <alex@yuion.net>
2023-12-13 11:36:50 +1100

    fix(workplace): fix min duration offset on multiday end time

--- apps/workplace/src/app/book/meeting-flow/meeting-form-details.component.ts
+++ apps/workplace/src/app/book/meeting-flow/meeting-form-details.component.ts
@@ -109,7 +109,7 @@ import { addDays, endOfDay, set, startOfDay } from 'date-fns';
                         [ngModel]="form.value.date_end"
                         (ngModelChange)="form.patchValue({ date_end: $event })"
                         [ngModelOptions]="{ standalone: true }"
-                        [from]="form?.value?.date"
+                        [from]="form?.value?.date + 30 * 60 * 1000"
                         [use_24hr]="use_24hr"
                     ></a-time-field>
                 </div>
