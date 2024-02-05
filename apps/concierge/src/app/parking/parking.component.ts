932209bcf518fb02e81e1922daced93a4580fe0c
Alex Sorafumo <alex@yuion.net>
2024-02-06 10:37:40 +1100

    fix(concierge): fix background color for parking disabled overlay (PPT-1188)

--- apps/concierge/src/app/parking/parking.component.ts
+++ apps/concierge/src/app/parking/parking.component.ts
@@ -42,8 +42,9 @@ import { ParkingStateService } from './parking-state.service';
             </div>
             <div
                 *ngIf="!(levels | async)?.length"
-                class="absolute inset-0 bg-base-100/80/20 flex flex-col items-center justify-center z-50"
+                class="absolute inset-0 flex flex-col items-center justify-center z-50"
             >
+                <div class="absolute inset-0 bg-base-100 opacity-50 z-0"></div>
                 <p>No parking floors for the currently selected building</p>
             </div>
         </main>
