37a3955a143a41b5a9e9553a58401544ef356dd8
Alex Sorafumo <alex@yuion.net>
2024-02-29 12:57:24 +1100

    chore: update nx project to latest

---
+++ apps/workplace/e2e/landing.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4200/#/?mock=true');
+    await page.locator('app-landing').waitFor();
+});
+
+test('has layout', async ({ page }) => {
+    expect(await page.locator('app-landing')).toBeVisible();
+    expect(await page.locator('topbar')).toBeVisible();
+    expect(await page.locator('landing-colleagues')).toBeVisible();
+    expect(await page.locator('landing-upcoming')).toBeVisible();
+    expect(await page.locator('landing-availability')).toBeVisible();
+});
