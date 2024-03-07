d362b8af0bbcf4cf180b43ca04572847a3526553
MrYuion <alex@yuion.net>
2024-03-07 02:05:39 +0000

    build(workplace): [03-07-feat_test] CI build from 918501d

--- main.js
+++ main.js
@@ -19648,15 +19648,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "f5a859c",
-  "hash": "f5a859c",
+  "raw": "918501d",
+  "hash": "918501d",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "f5a859c",
+  "suffix": "918501d",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1709775982129
+  "time": 1709776940826
 };
 /* tslint:enable */
 
