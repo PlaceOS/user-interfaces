aa9b908eaa3141a4e220184d750973289a61ed34
MrYuion <alex@yuion.net>
2024-03-07 02:04:43 +0000

    build(caterer-ui): [03-07-feat_test] CI build from 918501d

--- main.js
+++ main.js
@@ -12453,15 +12453,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "f5a859c",
-  "hash": "f5a859c",
+  "raw": "918501d",
+  "hash": "918501d",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "f5a859c",
+  "suffix": "918501d",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1709775943801
+  "time": 1709776895796
 };
 /* tslint:enable */
 
