57ba6d2c208518af7b78068a943b257c5bba98cc
MrYuion <alex@yuion.net>
2024-03-12 00:17:47 +0000

    build(signage): [03-08-fix_workplace_fix_auto-release_checkin_modal_PPT-1155_] CI build from 76d2be3

--- main.js
+++ main.js
@@ -5815,15 +5815,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "9920398",
-  "hash": "9920398",
+  "raw": "76d2be3",
+  "hash": "76d2be3",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "9920398",
+  "suffix": "76d2be3",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1709875677310
+  "time": 1710202485645
 };
 /* tslint:enable */
 
