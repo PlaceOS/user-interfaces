15a0889e480cf8d46a34fe91b9314890f2e5c7f0
MrYuion <alex@yuion.net>
2024-03-12 00:09:31 +0000

    build(control): [03-11-feat_workplace_allow_using_regions_for_resource_availability_PPT-1184_] CI build from 5948de9

--- main.js
+++ main.js
@@ -13133,15 +13133,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "200b3f8",
-  "hash": "200b3f8",
+  "raw": "5948de9",
+  "hash": "5948de9",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "200b3f8",
+  "suffix": "5948de9",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710140624316
+  "time": 1710201982400
 };
 /* tslint:enable */
 
