cceb7d6d3e4df7862099287146e4c928aa6a7eb5
MrYuion <alex@yuion.net>
2024-03-12 00:19:02 +0000

    build(outlook-addin): [03-08-fix_workplace_fix_auto-release_checkin_modal_PPT-1155_] CI build from 76d2be3

--- main.js
+++ main.js
@@ -30911,15 +30911,15 @@ exports.VERSION = void 0;
 /* tslint:disable */
 exports.VERSION = {
   "dirty": false,
-  "raw": "9920398",
-  "hash": "9920398",
+  "raw": "76d2be3",
+  "hash": "76d2be3",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "9920398",
+  "suffix": "76d2be3",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1709875679653
+  "time": 1710202489575
 };
 /* tslint:enable */
 
