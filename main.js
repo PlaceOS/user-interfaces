c8ebe38962af5987a70753fe901f13663e21e5fc
MrYuion <alex@yuion.net>
2024-03-14 04:27:59 +0000

    build(control): [03-08-fix_workplace_fix_auto-release_checkin_modal_PPT-1155_] CI build from deea764

--- main.js
+++ main.js
@@ -22913,15 +22913,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "acadd0d",
-  "hash": "acadd0d",
+  "raw": "deea764",
+  "hash": "deea764",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "acadd0d",
+  "suffix": "deea764",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710389532481
+  "time": 1710390262524
 };
 /* tslint:enable */
 
