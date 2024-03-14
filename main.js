42cb5134fe36bed705f68387640d99b855dc93ec
MrYuion <alex@yuion.net>
2024-03-14 04:27:50 +0000

    build(booking-panel): [03-08-fix_workplace_fix_auto-release_checkin_modal_PPT-1155_] CI build from deea764

--- main.js
+++ main.js
@@ -20590,15 +20590,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "acadd0d",
-  "hash": "acadd0d",
+  "raw": "deea764",
+  "hash": "deea764",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "acadd0d",
+  "suffix": "deea764",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710389531413
+  "time": 1710390271902
 };
 /* tslint:enable */
 
