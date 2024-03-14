9eced1da22e9abc9b262b956ae18ac3c6cda4fab
MrYuion <alex@yuion.net>
2024-03-14 04:44:58 +0000

    build(booking-panel): [03-11-feat_workplace_allow_using_regions_for_resource_availability_PPT-1184_] CI build from ec2324b

--- main.js
+++ main.js
@@ -20732,15 +20732,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "7dee729",
-  "hash": "7dee729",
+  "raw": "ec2324b",
+  "hash": "ec2324b",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "7dee729",
+  "suffix": "ec2324b",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710378553355
+  "time": 1710391301390
 };
 /* tslint:enable */
 
