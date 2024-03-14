91273db3f7a8b95fb1bab10e41147309b7c09cb7
MrYuion <alex@yuion.net>
2024-03-14 04:31:58 +0000

    build(caterer-ui): [03-13-feat_workplace_add_events_UI_PPT-1217_] CI build from 644189e

--- main.js
+++ main.js
@@ -22233,15 +22233,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "c8cd2e8",
-  "hash": "c8cd2e8",
+  "raw": "644189e",
+  "hash": "644189e",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "c8cd2e8",
+  "suffix": "644189e",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710383495864
+  "time": 1710390520033
 };
 /* tslint:enable */
 
