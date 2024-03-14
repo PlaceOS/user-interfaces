3af6f83ba2f20557f9451434974dfafe5371af98
MrYuion <alex@yuion.net>
2024-03-14 04:17:04 +0000

    build(outlook-addin): [03-08-fix_workplace_fix_auto-release_checkin_modal_PPT-1155_] CI build from acadd0d

--- main.js
+++ main.js
@@ -5295,7 +5295,9 @@ var ScheduleStateService = /*#__PURE__*/function (_common_1$AsyncHandle) {
                   content: "You have indicated you are not in the office. \n                                Your booking will be cancelled in about ".concat(diff, " minutes. \n                                Do you wish to keep this booking?"),
                   icon: {
                     content: 'cancel'
-                  }
+                  },
+                  confirm_text: 'Keep Booking',
+                  cancel_text: 'Dismiss'
                 }, _this._dialog);
               case 26:
                 result = _context2.sent;
@@ -30913,15 +30915,15 @@ exports.VERSION = void 0;
 /* tslint:disable */
 exports.VERSION = {
   "dirty": false,
-  "raw": "fe52ac9",
-  "hash": "fe52ac9",
+  "raw": "acadd0d",
+  "hash": "acadd0d",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "fe52ac9",
+  "suffix": "acadd0d",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710377807921
+  "time": 1710389535442
 };
 /* tslint:enable */
 
