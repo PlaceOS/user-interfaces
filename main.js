eb0d087e09621e4d6f5b1d2292c36bdfa0d40f71
MrYuion <alex@yuion.net>
2024-03-20 03:45:56 +0000

    build(concierge): [03-13-feat_workplace_add_events_UI_PPT-1217_] CI build from b327870

--- main.js
+++ main.js
@@ -12503,15 +12503,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "5276797",
-  "hash": "5276797",
+  "raw": "b327870",
+  "hash": "b327870",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "5276797",
+  "suffix": "b327870",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710809555370
+  "time": 1710906093730
 };
 /* tslint:enable */
 
