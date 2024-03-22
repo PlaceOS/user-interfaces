da4b5276514ffc9d4b895943dbf2f68e7a58e5e9
MrYuion <alex@yuion.net>
2024-03-22 01:41:01 +0000

    build(control): [03-08-fix_assets_fix_handling_of_clashes_on_edit_PROJ-658_] CI build from 6dede05

--- main.js
+++ main.js
@@ -23199,15 +23199,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "47c955c",
-  "hash": "47c955c",
+  "raw": "6dede05",
+  "hash": "6dede05",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "47c955c",
+  "suffix": "6dede05",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710999645885
+  "time": 1711071464402
 };
 /* tslint:enable */
 
