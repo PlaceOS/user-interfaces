411951b50d2bf70c60becdf5a14e8c05dd82e379
MrYuion <alex@yuion.net>
2024-03-22 01:57:18 +0000

    build(map-kiosk): [03-08-fix_assets_fix_handling_of_clashes_on_edit_PROJ-658_] CI build from efaf036

--- main.js
+++ main.js
@@ -18383,15 +18383,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "6dede05",
-  "hash": "6dede05",
+  "raw": "efaf036",
+  "hash": "efaf036",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "6dede05",
+  "suffix": "efaf036",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1711071468353
+  "time": 1711072447359
 };
 /* tslint:enable */
 
