64e8ffc5ff16cfbb3001d6c18a321083afb3efb0
MrYuion <alex@yuion.net>
2024-03-22 01:43:47 +0000

    build(caterer-ui): [03-11-feat_concierge_add_event_UI_PPT-1216_] CI build from 644bc92

--- main.js
+++ main.js
@@ -22474,15 +22474,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "61bb044",
-  "hash": "61bb044",
+  "raw": "644bc92",
+  "hash": "644bc92",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "61bb044",
+  "suffix": "644bc92",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710906040050
+  "time": 1711071586791
 };
 /* tslint:enable */
 
