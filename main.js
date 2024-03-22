435f9081479debc1b546bea1ef08d3c378f18dcc
MrYuion <alex@yuion.net>
2024-03-22 01:48:08 +0000

    build(visitor-kiosk): [03-20-fix_concierge_improve_auto-release_settings_ux_ppt-1288_] CI build from f5652bc

--- main.js
+++ main.js
@@ -17920,15 +17920,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "936510d",
-  "hash": "936510d",
+  "raw": "f5652bc",
+  "hash": "f5652bc",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "936510d",
+  "suffix": "f5652bc",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710894515722
+  "time": 1711071849092
 };
 /* tslint:enable */
 
