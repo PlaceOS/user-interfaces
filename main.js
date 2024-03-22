607516af6e07424e3ea8679aa4309754f1c608fd
MrYuion <alex@yuion.net>
2024-03-22 01:42:35 +0000

    build(booking-panel): [03-11-feat_concierge_add_event_UI_PPT-1216_] CI build from 644bc92

--- main.js
+++ main.js
@@ -20831,15 +20831,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "61bb044",
-  "hash": "61bb044",
+  "raw": "644bc92",
+  "hash": "644bc92",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "61bb044",
+  "suffix": "644bc92",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1710906034483
+  "time": 1711071560852
 };
 /* tslint:enable */
 
