70abd78cf65a265aa8b2c4549e744eeceb60b1ce
MrYuion <alex@yuion.net>
2024-03-22 02:10:01 +0000

    build(concierge): [03-08-fix_assets_fix_handling_of_clashes_on_edit_PROJ-658_] CI build from af80854

--- main.js
+++ main.js
@@ -12549,15 +12549,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "efaf036",
-  "hash": "efaf036",
+  "raw": "af80854",
+  "hash": "af80854",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "efaf036",
+  "suffix": "af80854",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1711072389919
+  "time": 1711073145989
 };
 /* tslint:enable */
 
@@ -16988,6 +16988,13 @@ class IndoorMapsComponent extends _placeos_common__WEBPACK_IMPORTED_MODULE_1__.A
         yield _this2.renderSpaceStatus();
         yield _this2.mapActions();
       }
+      if (change.locate && _this2.locate) {
+        const searchParams = {
+          q: _this2.searchElement.nativeElement.value
+        };
+        const locations = yield mapsindoors?.services.LocationsService.getLocations(searchParams);
+        if (locations.length) _this2.getRoute(locations[0]);
+      }
       _this2.mapFloorsToIndex();
       _this2.loading = false;
     })();
@@ -17256,7 +17263,8 @@ class IndoorMapsComponent extends _placeos_common__WEBPACK_IMPORTED_MODULE_1__.A
     inputs: {
       styles: "styles",
       actions: "actions",
-      custom_coordinates: "custom_coordinates"
+      custom_coordinates: "custom_coordinates",
+      locate: "locate"
     },
     features: [_angular_core__WEBPACK_IMPORTED_MODULE_6__["ɵɵInheritDefinitionFeature"], _angular_core__WEBPACK_IMPORTED_MODULE_6__["ɵɵNgOnChangesFeature"]],
     decls: 12,
