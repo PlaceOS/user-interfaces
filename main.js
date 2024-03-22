eb09edc7ef9f001925c3868e98b0f27542dc4bbb
MrYuion <alex@yuion.net>
2024-03-22 01:56:34 +0000

    build(booking-panel): [03-08-fix_assets_fix_handling_of_clashes_on_edit_PROJ-658_] CI build from efaf036

--- main.js
+++ main.js
@@ -20876,15 +20876,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "6dede05",
-  "hash": "6dede05",
+  "raw": "efaf036",
+  "hash": "efaf036",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "6dede05",
+  "suffix": "efaf036",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1711071468697
+  "time": 1711072403237
 };
 /* tslint:enable */
 
