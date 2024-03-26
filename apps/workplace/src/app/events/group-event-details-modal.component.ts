f321514cb5fa9abfc96fa4ac1a407fff4f5dbd47
Alex Sorafumo <alex@yuion.net>
2024-03-26 14:37:17 +1100

    chore(workplace): finalise details modal

--- apps/workplace/src/app/events/group-event-details-modal.component.ts
+++ apps/workplace/src/app/events/group-event-details-modal.component.ts
@@ -1,7 +1,10 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Booking } from '@placeos/bookings';
-import { SettingsService } from '@placeos/common';
+import { MapsPeopleService, SettingsService } from '@placeos/common';
+import { MapPinComponent } from '@placeos/components';
+import { BuildingLevel, OrganisationService } from '@placeos/organisation';
+import { ViewerFeature } from '@placeos/svg-viewer';
 
 @Component({
     selector: `group-event-details-modal`,
@@ -21,6 +24,7 @@ import { SettingsService } from '@placeos/common';
             </div>
             <div
                 class="absolute top-0 left-0 rounded-br py-2 pl-2 pr-4 space-x-2 bg-info text-info-content flex items-center text-sm"
+                *ngIf="featured"
             >
                 <app-icon class="text-base">star</app-icon>
                 <div class="uppercase">Featured</div>
@@ -61,7 +65,7 @@ import { SettingsService } from '@placeos/common';
                 </div>
             </div>
             <div class="flex overflow-y-auto overflow-x-hidden p-8">
-                <div class="flex flex-col space-y-2">
+                <div class="flex flex-1 flex-col space-y-2 w-1/2">
                     <div class="flex items-center space-x-4">
                         <div
                             class="flex items-center justify-center w-10 h-10 bg-base-200 rounded-full"
@@ -122,30 +126,118 @@ import { SettingsService } from '@placeos/common';
                         </span>
                     </div>
                 </div>
-                <div class="flex"></div>
+                <div class="flex w-[20rem]" *ngIf="level">
+                    <div class="border border-base-300 w-full">
+                        <button
+                            matRipple
+                            class="relative w-full h-40 bg-base-200"
+                        >
+                            <i-map
+                                *ngIf="!(use_mapspeople | async)"
+                                [src]="level?.map_id"
+                                [features]="features"
+                            ></i-map>
+                            <indoor-maps
+                                *ngIf="use_mapspeople | async"
+                                [locate]="locate"
+                            ></indoor-maps>
+                        </button>
+                        <div class=" p-4 space-y-2">
+                            <div>
+                                {{ booking.location }}
+                                <span
+                                    *ngIf="!booking.location"
+                                    class="opacity-30"
+                                >
+                                    Remote Event
+                                </span>
+                            </div>
+                            <div class="opacity-30 text-sm">
+                                {{ booking.location }}
+                                <span
+                                    *ngIf="!booking.location"
+                                    class="opacity-30"
+                                >
+                                    No location set for this event
+                                </span>
+                            </div>
+                            <div class="flex items-center space-x-2 pt-4">
+                                <div
+                                    class="flex items-center justify-center h-10 w-10 rounded-full bg-base-200"
+                                    matTooltip="WiFi available"
+                                >
+                                    <app-icon class="text-2xl">wifi</app-icon>
+                                </div>
+                                <div
+                                    class="flex items-center justify-center h-10 w-10 rounded-full bg-base-200"
+                                    matTooltip="Cafe available"
+                                >
+                                    <app-icon class="text-2xl"
+                                        >local_cafe</app-icon
+                                    >
+                                </div>
+                                <div
+                                    class="flex items-center justify-center h-10 w-10 rounded-full bg-base-200"
+                                    matTooltip=""
+                                >
+                                    <app-icon class="text-2xl">chat</app-icon>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     `,
     styles: [``],
 })
 export class GroupEventDetailsModalComponent {
-    public booking: any = this._data;
+    public booking: Booking = this._data;
+    public level: BuildingLevel;
+    public features: ViewerFeature[] = [];
+    public locate = '';
 
     public get time_format() {
         return this._settings.time_format;
     }
 
+    public get featured() {
+        return (
+            (this.booking as any).featured ||
+            this.booking.extension_data?.featured
+        );
+    }
+
     public get attendance() {
         return (
-            this.booking.attendees?.filter((_) => _.status === 'accepted')
+            this.booking.attendees?.filter((_: any) => _.status === 'accepted')
                 ?.length || 0
         );
     }
 
+    public readonly use_mapspeople = this._maps_people.available$;
+
     constructor(
         @Inject(MAT_DIALOG_DATA) private _data: Booking,
-        private _settings: SettingsService
+        private _org: OrganisationService,
+        private _settings: SettingsService,
+        private _maps_people: MapsPeopleService
     ) {}
 
-    public ngOnInit(): void {}
+    public ngOnInit(): void {
+        this.level = this._org.levelWithID(this.booking.zones);
+        this._maps_people.setCustomZone(this.level?.id);
+        this.locate = this.booking.extension_data?.map_id || '';
+        this.features = [
+            {
+                location: this.locate,
+                content: MapPinComponent,
+                data: {},
+            },
+        ];
+    }
+
+    public ngOnDestroy(): void {
+        this._maps_people.setCustomZone('');
+    }
 }
