e97f9722b4c35824f17b12b7b2b9b62297f25b2f
Alex Sorafumo <alex@yuion.net>
2024-04-18 19:47:05 +1000

    fix(explore): change sensor overlay to tooltip

--- libs/explore/src/lib/explore-sensor-info.component.ts
+++ libs/explore/src/lib/explore-sensor-info.component.ts
@@ -16,39 +16,40 @@ let shown_id = '';
         <button
             icon
             matRipple
+            customTooltip
+            [content]="stats"
+            yPosition="center"
+            xPosition="center"
             class="absolute top-1/2 left-1/2 w-7 h-7 min-w-0 -translate-x-1/2 -translate-y-1/2 bg-base-100 shadow pointer-events-auto"
-            (touchend)="show = true"
-            (click)="show = true"
         >
-            <app-icon className="material-symbols-rounded"> help </app-icon>
+            <app-icon> visibility </app-icon>
         </button>
-        <button
-            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-base-100 rounded-lg border border-base-200 p-2 text-xl"
-            *ngIf="show"
-            (window:touchend)="show = false"
-            (window:click)="show = false"
-        >
+        <ng-template #stats>
             <div
-                class="flex items-center space-x-2 whitespace-nowrap pr-2"
-                *ngIf="temp"
+                class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-base-100 rounded-lg border border-base-200 p-2 text-xl"
             >
-                <app-icon>thermostat</app-icon>
-                <div class="">{{ temp }}˚F</div>
+                <div
+                    class="flex items-center space-x-2 whitespace-nowrap pr-2"
+                    *ngIf="temp"
+                >
+                    <app-icon>thermostat</app-icon>
+                    <div class="">{{ temp }}˚F</div>
+                </div>
+                <div
+                    class="flex items-center space-x-2 whitespace-nowrap pr-2"
+                    *ngIf="humidity"
+                >
+                    <app-icon>opacity</app-icon>
+                    <div class="">{{ humidity }}%</div>
+                </div>
+                <div
+                    *ngIf="temp > 82"
+                    class="absolute top-0 right-0 rounded-full translate-x-1/2 -translate-y-1/2 bg-base-100 border border-base-200"
+                >
+                    <app-icon class="text-error text-xl">error</app-icon>
+                </div>
             </div>
-            <div
-                class="flex items-center space-x-2 whitespace-nowrap pr-2"
-                *ngIf="humidity"
-            >
-                <app-icon>opacity</app-icon>
-                <div class="">{{ humidity }}%</div>
-            </div>
-            <div
-                *ngIf="temp > 82"
-                class="absolute top-0 right-0 rounded-full translate-x-1/2 -translate-y-1/2 bg-base-100 border border-base-200"
-            >
-                <app-icon class="text-error text-xl">error</app-icon>
-            </div>
-        </button>
+        </ng-template>
     `,
     styles: [``],
 })
