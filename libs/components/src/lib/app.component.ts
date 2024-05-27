bb4b77c33bc64e1f54b194aed25700af9534a6fb
Alex Sorafumo <alex@yuion.net>
2024-05-27 15:22:46 +1000

    ci: fix get affected checks

--- libs/components/src/lib/app.component.ts
+++ libs/components/src/lib/app.component.ts
@@ -288,6 +288,7 @@ export class AppComponent extends AsyncHandler implements OnInit {
     private _checkReload() {
         if (!hasNewVersion()) return;
         location.reload();
+
         this.timeout(
             'reload',
             () => (location.href = `${location.origin}${location.pathname}`)
