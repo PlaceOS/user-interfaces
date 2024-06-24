1316972914db9cd94684a78ddc842d5b99d63038
Alex Sorafumo <alex@yuion.net>
2024-06-24 17:07:08 +1000

    chore(assistant): move back to using browser for speech recognition

--- apps/assistant-panel/src/app/panel-view.component.ts
+++ apps/assistant-panel/src/app/panel-view.component.ts
@@ -13,7 +13,8 @@ declare let loadVosklet: any;
     template: `
         <div class="flex items-center justify-center h-full w-full">
             <button
-                class="relative flex items-center justify-center flex-1 h-full bg-base-100 p-8"
+                class="relative flex items-center justify-center flex-1 h-full bg-base-300 p-8"
+                (click)="startListening()"
             >
                 <div
                     class="h-[18vmin] w-[18vmin] m-4 rounded-full bg-base-content"
@@ -26,7 +27,7 @@ declare let loadVosklet: any;
 
                 <div class="absolute bottom-0 inset-x-0 p-4 text-center"></div>
                 <div
-                    class="absolute top-2 left-1/2 -translate-x-1/2 p-4 text-center rounded-3xl bg-error text-error-content text-xs"
+                    class="absolute top-2 left-1/2 -translate-x-1/2 px-4 py-2 text-center rounded-3xl bg-error text-error-content text-xs"
                     *ngIf="error.speech_recognition || error.speech_synthesis"
                 >
                     <div
@@ -50,6 +51,16 @@ declare let loadVosklet: any;
                     class="absolute bottom-4 left-4 w-48 h-48 object-cover rounded-xl bg-base-200 border-2 border-base-200"
                     [class.!border-success]="person_in_view"
                 ></video>
+                <div
+                    class="absolute bottom-4 left-4 bg-warning h-2 w-2 rounded-full"
+                    [class.!bg-success]="person_in_view"
+                ></div>
+                <div
+                    class="absolute bottom-4 right-4 bg-success text-success-content h-12 w-12 rounded-full flex items-center justify-center"
+                    *ngIf="listening"
+                >
+                    <app-icon class="text-2xl">mic</app-icon>
+                </div>
                 <canvas
                     #canvas
                     width="640"
@@ -57,95 +68,106 @@ declare let loadVosklet: any;
                     class="absolute opacity-0 pointer-events-none"
                 ></canvas>
             </button>
-            <div
-                class="relative flex flex-col items-center justify-end h-full bg-base-200 p-4 w-[24rem]"
-            >
-                <div
-                    class="absolute inset-0 flex flex-col items-center justify-center space-y-4"
-                    *ngIf="!(messages | async)?.length"
-                >
-                    <img
-                        class="h-32 w-32 object-contain"
-                        src="assets/icons/no-pending.svg"
-                    />
-                    <p class="opacity-30">
-                        {{ 'PANEL.NO_MESSAGES' | translate }}
-                    </p>
-                </div>
-                <div
-                    class="flex flex-col m-2"
-                    *ngFor="let message of messages | async"
-                    [class.pr-4]="message.user_id !== user.id"
-                    [class.pl-4]="message.user_id === user.id"
-                    [class.items-left]="message.user_id !== user.id"
-                    [class.items-end]="message.user_id === user.id"
-                    (click)="show_time[message.id] = !show_time[message.id]"
-                    [class.waiting-margin]="waiting | async"
-                >
-                    <div class="flex items-center space-x-2">
-                        <div
-                            class="text-sm text-base-content opacity-60 px-2 py-1"
-                            *ngIf="message.user_id !== user.id"
-                        >
-                            Assistant
-                        </div>
-                        <div
-                            class="text-xs  text-base-content opacity-40 px-2 py-1"
-                        >
-                            {{ message.timestamp + offset | dateFrom }}
-                        </div>
+            <div class="w-[24rem] h-full overflow-auto bg-base-100">
+                <div class="relative flex flex-col justify-end h-full">
+                    <div
+                        class="absolute inset-0 flex flex-col items-center justify-center space-y-4"
+                        *ngIf="!(messages | async)?.length"
+                    >
+                        <img
+                            class="h-32 w-32 object-contain"
+                            src="assets/icons/no-pending.svg"
+                        />
+                        <p class="opacity-30">
+                            {{ 'PANEL.NO_MESSAGES' | translate }}
+                        </p>
                     </div>
                     <div
-                        message
-                        class="bg-base-100 border-base-300 p-4 rounded shadow text-sm markdown selectable"
-                        [innerHTML]="message.content | sanitize"
-                    ></div>
-                </div>
-                <div class="p-4" *ngIf="progress | async">
-                    <button
-                        class="block p-2 rounded border-base-300 bg-info text-info-content w-full"
-                        (click)="show_info = !show_info"
+                        class="my-2 p-2 flex space-x-4 hover:bg-base-200"
+                        *ngFor="let message of messages | async"
+                        (click)="show_time[message.id] = !show_time[message.id]"
+                        [class.waiting-margin]="waiting | async"
                     >
-                        <div class=" flex items-center space-x-2">
-                            <app-icon class="text-2xl">{{
-                                icons[(progress | async).function] || 'info'
-                            }}</app-icon>
-                            <p class="text-sm">
-                                {{
-                                    (progress | async).message ||
-                                        (progress | async).function
-                                }}
-                            </p>
-                        </div>
-                        <div class="relative overflow-hidden w-full rounded">
+                        <a-user-avatar
+                            [user]="{
+                                name: message.user_name || '',
+                                photo:
+                                    message.user_id !== user.id
+                                        ? '/assets/icons/ai-avatar.jpg'
+                                        : '/assets/icons/user-avatar.jpg'
+                            }"
+                            class="text-xl"
+                        ></a-user-avatar>
+                        <div class="flex flex-1 flex-col space-y-1">
+                            <div class="flex items-center space-x-4">
+                                <div>
+                                    {{
+                                        message.user_id !== user.id
+                                            ? 'Assistant'
+                                            : 'You'
+                                    }}
+                                </div>
+                                <div
+                                    class="text-xs text-base-content opacity-40 px-2 py-1 w-full text-right"
+                                >
+                                    {{ message.timestamp + offset | dateFrom }}
+                                </div>
+                            </div>
                             <div
-                                class="absolute inset-0 bg-base-100 opacity-10"
-                            ></div>
-                            <div
-                                class="text-xs text-mono text-left p-2 break-words"
-                                *ngIf="show_info"
-                                [innerHTML]="
-                                    (progress | async).content | sanitize
-                                "
+                                message
+                                class="markdown selectable text-sm"
+                                [innerHTML]="message.content | sanitize"
                             ></div>
                         </div>
-                    </button>
-                </div>
-                <div
-                    class="absolute right-2 flex items-center justify-center space-x-2 p-1 rounded-2xl bg-base-100 border border-neutral"
-                    [style.bottom]="'8px'"
-                    *ngIf="waiting | async"
-                >
+                    </div>
+                    <div class="p-4" *ngIf="progress | async">
+                        <button
+                            class="block p-2 rounded border-base-300 bg-info text-info-content w-full"
+                            (click)="show_info = !show_info"
+                        >
+                            <div class=" flex items-center space-x-2">
+                                <app-icon class="text-2xl">{{
+                                    icons[(progress | async).function] || 'info'
+                                }}</app-icon>
+                                <p class="text-sm">
+                                    {{
+                                        (progress | async).message ||
+                                            (progress | async).function
+                                    }}
+                                </p>
+                            </div>
+                            <div
+                                class="relative overflow-hidden w-full rounded"
+                            >
+                                <div
+                                    class="absolute inset-0 bg-base-100 opacity-10"
+                                ></div>
+                                <div
+                                    class="text-xs text-mono text-left p-2 break-words"
+                                    *ngIf="show_info"
+                                    [innerHTML]="
+                                        (progress | async).content | sanitize
+                                    "
+                                ></div>
+                            </div>
+                        </button>
+                    </div>
                     <div
-                        class="h-2 w-2 bg-neutral rounded-full animate-bounce"
-                    ></div>
-                    <div
-                        class="h-2 w-2 bg-neutral rounded-full animate-bounce anim-delay-1"
-                    ></div>
-                    <div
-                        class="h-2 w-2 bg-neutral rounded-full animate-bounce anim-delay-2"
-                    ></div>
-                    <span class="sr-only">Waiting for reply...</span>
+                        class="absolute right-2 flex items-center justify-center space-x-2 p-1 rounded-2xl bg-base-100 border border-neutral"
+                        [style.bottom]="'8px'"
+                        *ngIf="waiting | async"
+                    >
+                        <div
+                            class="h-2 w-2 bg-neutral rounded-full animate-bounce"
+                        ></div>
+                        <div
+                            class="h-2 w-2 bg-neutral rounded-full animate-bounce anim-delay-1"
+                        ></div>
+                        <div
+                            class="h-2 w-2 bg-neutral rounded-full animate-bounce anim-delay-2"
+                        ></div>
+                        <span class="sr-only">Waiting for reply...</span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -153,7 +175,7 @@ declare let loadVosklet: any;
             splash
             matRipple
             class="absolute inset-0 text-white flex flex-col items-center justify-center z-20"
-            *ngIf="!listening"
+            *ngIf="!setup"
         >
             <h2 class="font-light text-4xl mb-4">Touch to Start</h2>
         </button>
@@ -174,9 +196,11 @@ export class PanelViewComponent extends AsyncHandler {
     public listening = false;
     public person_in_view = false;
     public debug = false;
+    public setup = false;
     public error: Record<string, boolean> = {};
     private _time = 0;
     private _last_message = '';
+    private _previous_message = '';
     private _context: any;
 
     public readonly icons = {
@@ -240,6 +264,12 @@ export class PanelViewComponent extends AsyncHandler {
         );
     }
 
+    public startListening() {
+        if (this.listening || !this.person_in_view) return;
+        this._recognition.start();
+        this.listening = true;
+    }
+
     private _model: tf.GraphModel;
 
     private async _loadModel() {
@@ -257,11 +287,21 @@ export class PanelViewComponent extends AsyncHandler {
             const detections = this._processPredictions(predictions, {
                 0: 'person',
             });
+            const old_state = this.person_in_view;
             this.person_in_view = false;
             for (const { box, label } of detections) {
                 if (label === 'person') {
                     this.person_in_view = true;
                     return;
+                }
+            }
+            if (old_state !== this.person_in_view && this._recognition) {
+                if (this.person_in_view) {
+                    this._recognition.start();
+                    this.listening = true;
+                } else {
+                    this._recognition.stop();
+                    this.listening = false;
                 }
             }
         });
@@ -335,51 +375,47 @@ export class PanelViewComponent extends AsyncHandler {
             );
         }
         console.log('Setup Speech Recognition');
-        // Make sure sample rate matches that in the training data
-        let ctx = new AudioContext({ sampleRate: 16000 });
-        // Setup mic with correct sample rate
-        let mic_node = ctx.createMediaStreamSource(
-            await navigator.mediaDevices.getUserMedia({
-                video: false,
-                audio: {
-                    echoCancellation: true,
-                    noiseSuppression: true,
-                    channelCount: 1,
-                    sampleRate: 16000,
-                },
-            })
-        );
-        // Load Vosklet module, model and recognizer
-        let module = await loadVosklet();
-        let model = await module.createModel(
-            `${location.origin}${location.pathname}assets/vosk-model-small-en-us-0.15.tar.gz`,
-            'model',
-            'ID'
-        );
-        let recognizer = await module.createRecognizer(model, 16000);
-        // Listen for result and partial result
-        recognizer.addEventListener('result', (ev) => {
-            const { text } = JSON.parse(ev.detail || '{ "text": "" }');
-            this.last_text = text || '';
-            this.current_text = '';
-            this.clearInterval('scale');
-            this.scale = 1;
-            if (this.last_text.length <= 3) return;
-            console.log('Sending message:', this.last_text);
-            this._chat.sendMessage(this.last_text);
-        });
-        recognizer.addEventListener('partialResult', (ev) => {
-            this.current_text = ev.detail?.partial || '';
-        });
-        // Create a transferer node to get audio data on the main thread
-        let transferer = await module.createTransferer(ctx, 128 * 150);
-        // Recognize data on arrival
-        transferer.port.onmessage = (ev) => {
-            recognizer.acceptWaveform(ev.data);
+        const SpeechRecognition =
+            (window as any).SpeechRecognition ||
+            (window as any).webkitSpeechRecognition;
+
+        const recognition = new SpeechRecognition();
+        recognition.interimResults = true;
+        recognition.lang = 'en-US';
+
+        recognition.onresult = (event) => {
+            const { transcript } = event.results[0][0];
+            // do something with transcript
+            this.current_text = transcript;
+            console.log('Speech Recognition:', transcript);
+            this.timeout('on_end', () => this._handleEnd(), 3000);
         };
-        // Connect to microphone
-        mic_node.connect(transferer);
+
+        recognition.onerror = (event) => {
+            console.log('Speech Recognition Error:', event);
+            if (event.error === 'no-speech') {
+                this.current_text = '';
+                this.listening = false;
+                return;
+            }
+            this.error.speech_recognition = true;
+        };
+
+        recognition.onend = (event) => {
+            // const { transcript } = event.results[0][0];
+            // do something with transcript
+            this._handleEnd();
+            this.listening = false;
+        };
+        this._recognition = recognition;
+        recognition.start();
+        this.setup = true;
         this.listening = true;
+        this.interval('check_listening', () => this.startListening(), 500);
+    }
+
+    public get speech() {
+        return window.speechSynthesis;
     }
 
     private _speakText(text: string) {
@@ -394,9 +430,7 @@ export class PanelViewComponent extends AsyncHandler {
             return;
         }
 
-        const synth = window.speechSynthesis;
-
-        const voices = synth.getVoices();
+        const voices = this.speech.getVoices();
         const preferredVoice = voices.find(
             (voice) => voice.voiceURI === 'Karen'
         );
@@ -409,10 +443,19 @@ export class PanelViewComponent extends AsyncHandler {
             utterance.voice = preferredVoice;
         }
 
-        utterance.onend = () => {
-            // do something once all the text has been spoken
-        };
+        this.speech.speak(utterance);
+    }
 
-        window.speechSynthesis.speak(utterance);
+    private _handleEnd() {
+        this.last_text = this.current_text;
+        this.current_text = '';
+        this.clearInterval('scale');
+        this.scale = 1;
+        if (this.last_text.length <= 3) return;
+        if (this.last_text === this._previous_message) return;
+        console.log('Sending message:', this.last_text);
+        this._chat.sendMessage(this.last_text);
+        this._previous_message = this.last_text;
+        this.last_text = '';
     }
 }
