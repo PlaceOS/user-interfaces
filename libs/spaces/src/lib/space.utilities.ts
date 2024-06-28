88f7379d018c7ade3fa7e606d4017737d3168660
Alex Sorafumo <alex@yuion.net>
2024-06-28 17:30:44 +1000

    fix(concierge): add level selector to day view when use region set (PPT-1434)

--- libs/spaces/src/lib/space.utilities.ts
+++ libs/spaces/src/lib/space.utilities.ts
@@ -65,7 +65,7 @@ export function generateSystemsFormFields(system?: PlaceSystem) {
     return new FormGroup(fields);
 }
 
-export function requestSpacesForZone(id: string) {
+export function requestSpacesForZone(id: string): Observable<Space[]> {
     if (!id) return of([]);
     if (SPACE_LIST_REQUESTS[id]) return SPACE_LIST_REQUESTS[id];
     SPACE_LIST_REQUESTS[id] = querySystems({ zone_id: id, limit: 500 }).pipe(
