9a566a958e261aa3f3f62cbd580fa4c29f83e8a4
Alex Sorafumo <alex@yuion.net>
2024-07-24 10:19:15 +1000

    chore(concierge): add API calls for handling Email template data

--- apps/concierge/src/app/email-templates/email-templates-state.service.ts
+++ apps/concierge/src/app/email-templates/email-templates-state.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
-import { randomString } from '@placeos/common';
+import { notifySuccess, randomString, SettingsService } from '@placeos/common';
+import { OrganisationService } from '@placeos/organisation';
+import {
+    PlaceMetadata,
+    showMetadata,
+    updateMetadata,
+} from '@placeos/ts-client';
 import { getUnixTime } from 'date-fns';
-import { BehaviorSubject, combineLatest } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, forkJoin, of } from 'rxjs';
+import {
+    catchError,
+    filter,
+    map,
+    shareReplay,
+    switchMap,
+    take,
+} from 'rxjs/operators';
 
 export interface EmailTemplate {
     id: string;
@@ -16,6 +29,13 @@ export interface EmailTemplate {
     updated_at: number;
 }
 
+export interface EmailTemplateDefinition {
+    id: string;
+    name: string;
+    description: string;
+    fields: { name: string; description: string }[];
+}
+
 export interface EmailTemplatesFilters {
     category?: 'internal' | 'external';
 }
@@ -24,10 +44,84 @@ export interface EmailTemplatesFilters {
     providedIn: 'root',
 })
 export class EmailTemplatesStateService {
-    private _templates = new BehaviorSubject<EmailTemplate[]>([]);
     private _filters = new BehaviorSubject<EmailTemplatesFilters>({});
+    private _change = new BehaviorSubject(0);
 
-    public readonly templates = this._templates.asObservable();
+    public readonly available_template_definitions = combineLatest([
+        this._org.active_building,
+        this._org.active_region,
+        this._change,
+    ]).pipe(
+        filter(([bld]) => !!bld),
+        switchMap(([bld, region]) =>
+            showMetadata(
+                this._settings.get('app.use_region') ? region.id : bld.id,
+                'email_template_fields'
+            )
+                .pipe(
+                    map((_) => {
+                        const definitions =
+                            (_ as any)?.details ||
+                            ({} as Record<string, EmailTemplateDefinition>);
+                        return Object.keys(definitions).map(
+                            (key) =>
+                                ({
+                                    id: key,
+                                    name: definitions[key].name,
+                                    description:
+                                        definitions[key].description || '',
+                                    fields: definitions[key].fields.map(
+                                        (field) => ({
+                                            name: field.name,
+                                            description:
+                                                field.description || '',
+                                        })
+                                    ),
+                                } as EmailTemplateDefinition)
+                        );
+                    })
+                )
+                .pipe(catchError(() => of([] as EmailTemplateDefinition[])))
+        ),
+        shareReplay(1)
+    );
+
+    private _processTemplates(metadata: PlaceMetadata, zone_id: string) {
+        const data = metadata.details;
+        return ((data instanceof Array ? data : '') || []).map(
+            (template) => ({ ...template, zone_id } as EmailTemplate)
+        );
+    }
+
+    public readonly templates = combineLatest([
+        this._org.active_building,
+        this._org.active_region,
+        this._change,
+    ]).pipe(
+        filter(([bld]) => !!bld),
+        switchMap(([bld, region]) =>
+            forkJoin([
+                showMetadata(this._org.organisation.id, 'email_templates').pipe(
+                    map((_) =>
+                        this._processTemplates(_, this._org.organisation.id)
+                    ),
+                    catchError(() => of([] as EmailTemplate[]))
+                ),
+                showMetadata(bld.id, 'email_templates').pipe(
+                    map((_) => this._processTemplates(_, bld.id)),
+                    catchError(() => of([] as EmailTemplate[]))
+                ),
+                showMetadata(region.id, 'email_templates').pipe(
+                    map((_) => this._processTemplates(_, region.id)),
+                    catchError(() => of([] as EmailTemplate[]))
+                ),
+            ])
+        ),
+        map(([org_templates, bld_templates, region_templates]) =>
+            org_templates.concat(bld_templates).concat(region_templates)
+        ),
+        shareReplay(1)
+    );
     public readonly filters = this._filters.asObservable();
 
     public readonly filtered_templates = combineLatest([
@@ -42,16 +136,10 @@ export class EmailTemplatesStateService {
         })
     );
 
-    constructor() {
-        this.loadTemplates();
-    }
-
-    public async loadTemplates() {
-        const session_templates = JSON.parse(
-            sessionStorage.getItem('PLACEOS.email_templates') || '[]'
-        );
-        this._templates.next(session_templates);
-    }
+    constructor(
+        private _org: OrganisationService,
+        private _settings: SettingsService
+    ) {}
 
     public async loadTemplate(id: string) {
         const template_list = await this.templates.pipe(take(1)).toPromise();
@@ -59,34 +147,45 @@ export class EmailTemplatesStateService {
     }
 
     public async saveTemplate(template: EmailTemplate) {
+        if (!template.zone_id) return;
         const template_list = await this.templates.pipe(take(1)).toPromise();
         if (!template.id) {
             template.id = `template-${randomString(8)}`;
             template.created_at = getUnixTime(Date.now());
         }
         template.updated_at = getUnixTime(Date.now());
+        const zone_templates = template_list.filter(
+            (_) => _.zone_id === template.zone_id
+        );
+        const template_value = { ...template };
+        delete template_value.zone_id;
         console.log('Templates:', template_list);
         const new_template_list = [
-            ...template_list.filter((_) => _.id !== template.id),
+            ...zone_templates.filter((_) => _.id !== template.id),
             template,
         ];
-        console.log('New Templates:', new_template_list);
-        this._templates.next(new_template_list);
-        sessionStorage.setItem(
-            'PLACEOS.email_templates',
-            JSON.stringify(new_template_list)
-        );
+        await updateMetadata(template.zone_id, {
+            name: `email_templates`,
+            details: new_template_list,
+            description: 'Email Templates for Zone',
+        }).toPromise();
+        notifySuccess('Successfully saved template');
     }
 
-    public removeTemplate(template: EmailTemplate) {
-        const new_template_list = this._templates
-            .getValue()
-            .filter((_) => _.id !== template.id);
-        this._templates.next(new_template_list);
-        sessionStorage.setItem(
-            'PLACEOS.email_templates',
-            JSON.stringify(new_template_list)
+    public async removeTemplate(template: EmailTemplate) {
+        const template_list = await this.templates.pipe(take(1)).toPromise();
+        const zone_templates = template_list.filter(
+            (_) => _.zone_id === template.zone_id
         );
+        const new_template_list = zone_templates.filter(
+            (_) => _.id !== template.id
+        );
+        await updateMetadata(template.zone_id, {
+            name: `email_templates`,
+            details: new_template_list,
+            description: 'Email Templates for Zone',
+        }).toPromise();
+        notifySuccess('Successfully removed template');
     }
 
     public setFilters(filters: Partial<EmailTemplatesFilters>) {
