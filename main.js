667f5e459262fc2979cfabf8e4522b08e069cde2
MrYuion <alex@yuion.net>
2024-10-03 05:12:13 +0000

    build(caterer-ui): [custom/7E62F9ZJ] CI build from 193f6c5

--- main.js
+++ main.js
@@ -23932,15 +23932,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "bdd0407",
-  "hash": "bdd0407",
+  "raw": "193f6c5",
+  "hash": "193f6c5",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "bdd0407",
+  "suffix": "193f6c5",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1727323617748
+  "time": 1727931987985
 };
 /* tslint:enable */
 
