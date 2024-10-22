1f3a3621847062385a40222ce2a857d590009ba5
MrYuion <alex@yuion.net>
2024-10-22 03:12:48 +0000

    build(caterer-ui): [feat/workplace-schedule-week] CI build from 1c22ad5

--- main.js
+++ main.js
@@ -24039,15 +24039,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "d53196f",
-  "hash": "d53196f",
+  "raw": "1c22ad5",
+  "hash": "1c22ad5",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "d53196f",
+  "suffix": "1c22ad5",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1729564286271
+  "time": 1729566407785
 };
 /* tslint:enable */
 
