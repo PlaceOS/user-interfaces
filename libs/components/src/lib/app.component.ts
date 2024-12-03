2b30f4739b515cdc98ebe8f537409f20e826e0b4
Alex Sorafumo <alex@yuion.net>
2024-12-03 21:22:28 +1100

    chore: force builds

--- libs/components/src/lib/app.component.ts
+++ libs/components/src/lib/app.component.ts
@@ -283,7 +283,6 @@ export class AppComponent extends AsyncHandler implements OnInit {
     private _checkReload() {
         if (!hasNewVersion()) return;
         location.reload();
-
         this.timeout(
             'reload',
             () => (location.href = `${location.origin}${location.pathname}`),
