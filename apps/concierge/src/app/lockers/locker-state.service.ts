49f7298acea4e3024d5e72adbfc980aa49b6a620
Alex Sorafumo <alex@yuion.net>
2024-12-09 17:18:41 +1100

    chore(concierge): reduce page size to 300 for locker requests

--- apps/concierge/src/app/lockers/locker-state.service.ts
+++ apps/concierge/src/app/lockers/locker-state.service.ts
@@ -126,7 +126,7 @@ export class LockersStateService extends AsyncHandler {
                     type: 'locker',
                     zones: zones.join(','),
                     include_checked_out: true,
-                    limit: 500,
+                    limit: 300,
                 }),
             );
             this._call_next_page.next(`RESET_${Date.now()}`);
