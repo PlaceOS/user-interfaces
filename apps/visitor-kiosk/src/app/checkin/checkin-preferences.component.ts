f3c3c0f4c764580bc4ff2b6532ca7a453efdcdac
Alex Sorafumo <alex@yuion.net>
2025-01-08 20:14:15 +1100

    chore(visitor-kiosk): fix saving beverages

--- apps/visitor-kiosk/src/app/checkin/checkin-preferences.component.ts
+++ apps/visitor-kiosk/src/app/checkin/checkin-preferences.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import {
@@ -17,6 +17,7 @@ import { updateBooking } from '@placeos/bookings';
     template: `
         <div
             class="bg-base-100 rounded shadow overflow-hidden relative flex flex-col items-center w-[36rem] p-4"
+            *ngIf="!loading; else load_state"
         >
             <h3 class="text-xl mb-2 w-full">
                 {{ 'APP.VISITOR_KIOSK.BEVERAGE_MSG' | translate }}
@@ -25,7 +26,9 @@ import { updateBooking } from '@placeos/bookings';
                 <mat-form-field appearance="outline" class="w-full">
                     <mat-select
                         [(ngModel)]="beverage"
-                        placeholder="Select beverage"
+                        [placeholder]="
+                            'APP.VISITOR_KIOSK.BEVERAGE_SELECT' | translate
+                        "
                     >
                         <mat-option
                             *ngFor="let item of menu | async"
@@ -36,23 +39,35 @@ import { updateBooking } from '@placeos/bookings';
                     </mat-select>
                 </mat-form-field>
             </div>
-            <button btn matRipple class="w-32" (click)="update()">
-                {{
-                    (beverage
-                        ? 'APP.VISITOR_KIOSK.SAVE'
-                        : 'APP.VISITOR_KIOSK.CONTINUE'
-                    ) | translate
-                }}
-            </button>
+            <div class="flex items-center justify-end w-full">
+                <button btn matRipple class="w-32" (click)="update()">
+                    {{
+                        (beverage
+                            ? 'APP.VISITOR_KIOSK.SAVE'
+                            : 'APP.VISITOR_KIOSK.CONTINUE'
+                        ) | translate
+                    }}
+                </button>
+            </div>
             <a
                 icon
                 matRipple
-                class="absolute top-0 right-0"
+                class="absolute top-2 right-2"
                 [routerLink]="['/welcome']"
             >
                 <app-icon>close</app-icon>
             </a>
         </div>
+        <ng-template #load_state>
+            <div
+                class="bg-base-100 rounded shadow overflow-hidden relative flex flex-col items-center justify-center w-[28rem] h-[20rem] p-8 space-y-2"
+            >
+                <mat-spinner [diameter]="32"></mat-spinner>
+                <div>
+                    {{ 'APP.VISITOR_KIOSK.BEVERAGE_LOADING' | translate }}
+                </div>
+            </div>
+        </ng-template>
     `,
     styles: [
         `
@@ -62,8 +77,10 @@ import { updateBooking } from '@placeos/bookings';
         `,
     ],
 })
-export class CheckinPreferencesComponent {
+export class CheckinPreferencesComponent implements OnInit {
+    public loading = false;
     public beverage: CateringItem;
+    public readonly event = this._checkin.event;
 
     public readonly menu = this._catering.menu.pipe(
         map((l) => {
@@ -88,8 +105,15 @@ export class CheckinPreferencesComponent {
         private _catering: CateringStateService,
     ) {}
 
+    public ngOnInit(): void {
+        this.event
+            .pipe(first())
+            .subscribe((event) => (!event ? this.next() : null));
+    }
+
     public async update() {
         if (!this.beverage) return this.next();
+        this.loading = true;
         const booking = await this._checkin.event
             .pipe(first((_) => !!_))
             .toPromise();
@@ -102,15 +126,39 @@ export class CheckinPreferencesComponent {
             },
         });
         if (booking.linked_event) {
-            const event = await showEvent(booking.linked_event.id)
+            const event = await showEvent(booking.linked_event.event_id)
                 .toPromise()
                 .catch(() => null);
+            console.log('Event:', event);
             if (event) {
-                const order =
-                    (event.ext('catering') ? event.ext('catering')[0] : null) ||
-                    new CateringOrder();
+                const order_list = event.ext('catering') || [];
+                let order =
+                    order_list.find(
+                        (_) => _.caterer == this.beverage.caterer,
+                    ) || new CateringOrder({ caterer: this.beverage.caterer });
+                if (
+                    order.items.find(
+                        (_) => _.custom_id === this.beverage.custom_id,
+                    )
+                ) {
+                    const existing_item = order.items.find(
+                        (_) => _.custom_id === this.beverage.custom_id,
+                    );
+                    existing_item.quantity += 1;
+                } else {
+                    order = new CateringOrder({
+                        ...order,
+                        items: [
+                            ...order.items,
+                            new CateringItem({
+                                ...this.beverage,
+                                quantity: 1,
+                            }),
+                        ],
+                    });
+                }
                 await updateEvent(
-                    booking.id,
+                    event.id,
                     new CalendarEvent({
                         ...event,
                         extension_data: {
@@ -119,23 +167,16 @@ export class CheckinPreferencesComponent {
                                 ...(event.extension_data.catering?.filter(
                                     (_) => _.id !== order.id,
                                 ) || []),
-                                new CateringOrder({
-                                    ...order,
-                                    items: [
-                                        ...order.items,
-                                        new CateringItem({
-                                            ...this.beverage,
-                                            quantity: 1,
-                                        }),
-                                    ],
-                                }),
+                                order,
                             ],
                         },
                     }),
+                    { calendar: event.host },
                 ).toPromise();
             }
         }
-        notifySuccess(i18n('APP.VISITOR_KIOSK.UPDATE_SUCCESS'));
+        notifySuccess(i18n('APP.VISITOR_KIOSK.BEVERAGE_SUCCESS'));
+        this.loading = false;
         this.next();
     }
 
