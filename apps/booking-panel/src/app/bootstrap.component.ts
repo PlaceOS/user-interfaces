fa59392bdd54c8023f020efe8e0234964f71aca8
Alex Sorafumo <alex@yuion.net>
2025-05-15 12:16:57 +1000

    chore: add more details to bootstrap system selections

--- apps/booking-panel/src/app/bootstrap.component.ts
+++ apps/booking-panel/src/app/bootstrap.component.ts
@@ -45,9 +45,26 @@ import { BehaviorSubject, combineLatest, of } from 'rxjs';
                         *ngFor="let option of space_list | async"
                         [value]="option.id"
                     >
-                        <div class="leading-tight">
-                            <div class="name">{{ option.name }}</div>
-                            <div class="text-dark-fade text-xs">
+                        <div
+                            class="flex w-full items-center space-x-4 leading-tight"
+                        >
+                            <div class="flex flex-1 flex-col">
+                                <div>
+                                    {{ option.display_name || option.name }}
+                                </div>
+                                <div
+                                    class="text-xs opacity-30"
+                                    *ngIf="
+                                        option.display_name &&
+                                        option.display_name !== option.name
+                                    "
+                                >
+                                    {{ option.name }}
+                                </div>
+                            </div>
+                            <div
+                                class="rounded bg-base-200 px-2 py-1 font-mono text-[0.625rem]"
+                            >
                                 {{ option.id }}
                             </div>
                         </div>
@@ -137,6 +154,7 @@ export class BootstrapComponent extends AsyncHandler implements OnInit {
                 : querySystems({
                       q: search,
                       limit: 20,
+                      fields: ['id', 'name', 'display_name', 'email'].join(','),
                       zone_id: this._org.organisation.id,
                   });
         }),
