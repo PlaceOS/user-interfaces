70520250f60eb2a00b209e4bcef08d7d62594d6f
Alex Sorafumo <alex@yuion.net>
2025-06-05 12:59:23 +1000

    chore(stagehand): tweaks

--- apps/stagehand/src/app/analytics.component.ts
+++ apps/stagehand/src/app/analytics.component.ts
@@ -18,46 +18,6 @@ import Chart from 'chart.js/auto';
                     <button btn matRipple>Generate report</button>
                 </header>
                 <main class="w-full flex-1 overflow-auto">
-                    <div
-                        class="grid w-full flex-1 grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3"
-                    >
-                        <div
-                            class="rounded-lg border border-base-300 bg-base-100 p-4 shadow"
-                        >
-                            <div class="flex items-center justify-between">
-                                <h3 class="text-xl font-medium">Total Rooms</h3>
-                                <icon class="text-3xl text-info">sensors</icon>
-                            </div>
-                            <div class="text-4xl font-bold">24</div>
-                            <div class="text-sm opacity-40">+2 this month</div>
-                        </div>
-                        <div
-                            class="rounded-lg border border-base-300 bg-base-100 p-4 shadow"
-                        >
-                            <div class="flex items-center justify-between">
-                                <h3 class="text-xl font-medium">
-                                    Active Alerts
-                                </h3>
-                                <icon class="text-3xl text-error">warning</icon>
-                            </div>
-                            <div class="text-4xl font-bold">3</div>
-                            <div class="text-sm opacity-40">2 critical</div>
-                        </div>
-                        <div
-                            class="rounded-lg border border-base-300 bg-base-100 p-4 shadow"
-                        >
-                            <div class="flex items-center justify-between">
-                                <h3 class="text-xl font-medium">
-                                    System Uptime
-                                </h3>
-                                <icon class="text-3xl text-success"
-                                    >show_chart</icon
-                                >
-                            </div>
-                            <div class="text-4xl font-bold">99.9%</div>
-                            <div class="text-sm opacity-40">Last 30 days</div>
-                        </div>
-                    </div>
                     <div class="flex items-center space-x-4 p-4">
                         <date-range-field class="rounded bg-base-100">
                             <input
