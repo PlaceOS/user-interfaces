91af0c8d628f65d6a59a05a3e547316015ef65fb
Alex Sorafumo <alex@yuion.net>
2025-06-12 12:33:19 +1000

    chore: add initial implementation for cisco maps

---
+++ libs/components/src/lib/cisco-map.component.ts
@@ -0,0 +1,85 @@
+import {
+    Component,
+    ElementRef,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    ViewChild,
+} from '@angular/core';
+import { AsyncHandler, SettingsService } from '@placeos/common';
+import { BuildingLevel, OrganisationService } from '@placeos/organisation';
+import { MapMetadata } from './interactive-map.component';
+
+declare class SpacesRichMap {}
+
+const DEFAULT_ZOOM = 18.5;
+
+@Component({
+    selector: 'cisco-map',
+    template: `
+        <div
+            #map_container
+            id="cisco-map-container"
+            class="absolute inset-0 z-0"
+        ></div>
+    `,
+    styles: [``],
+})
+export class CiscoMapComponent extends AsyncHandler implements OnInit {
+    @Input() public zone: BuildingLevel;
+    @Input() public metadata: MapMetadata;
+    @Input() public options: any;
+    @Input() public focus: string;
+    @Input() public zoom = DEFAULT_ZOOM;
+    @Input() public reset: number;
+    @Output() public zoomChange = new EventEmitter<number>();
+    @Output() public zoneChange = new EventEmitter<BuildingLevel>();
+
+    @ViewChild('map_container', { static: true })
+    private _mapContainer!: ElementRef<HTMLDivElement>;
+    private _map: SpacesRichMap;
+
+    constructor(
+        private _org: OrganisationService,
+        private _settings: SettingsService,
+    ) {
+        super();
+    }
+
+    public ngOnInit(): void {
+        this._injectScript();
+        this.timeout('init', () => this._initialiseMap());
+    }
+
+    private _injectScript(): void {
+        if (document.getElementById('cisco-spaces-rich-maps-script')) return;
+        const script = document.createElement('script');
+        script.id = 'cisco-spaces-rich-maps-script';
+        script.src =
+            'https://maps.ciscospaces.io/js/spaces-rich-maps-2.0-beta.min.js';
+        document.body.appendChild(script);
+    }
+
+    private _initialiseMap(): void {
+        if (!SpacesRichMap) {
+            console.error('Cisco Spaces Rich Map is not defined');
+            return;
+        }
+        const config = this._settings.get('app.explore.cisco_maps');
+        this._map = new (SpacesRichMap as any)({
+            mapContainer: 'cisco-map-container',
+            token: config.token,
+            tenantId: config.tenant_id,
+            locationId: config.location_id,
+            defaultFloor: 0,
+            initialPos: [0, 0],
+            initialZoom: 20,
+            initialPitch: 65,
+            initialBearing: 118,
+            poiLegendHolder: 'poi-switch',
+            hideNavigationControls: true,
+        });
+        console.log('Map initialized', this._map);
+    }
+}
