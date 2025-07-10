bae6c9ec8939f3c4ef56b4bd9289c5f2d2dbec73
Alex Sorafumo <alex@yuion.net>
2025-07-10 15:25:29 +1000

    chore(control): add message listener to zoom phone iframe

--- apps/control/src/app/ui/zoom-phone-controls.component.ts
+++ apps/control/src/app/ui/zoom-phone-controls.component.ts
@@ -1,10 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, input, OnInit, viewChild } from '@angular/core';
+import { AsyncHandler } from '@placeos/common';
 
 @Component({
     selector: 'zoom-phone-controls',
     template: `
         <div class="h-full w-full overflow-hidden">
             <iframe
+                #zoom_phone
                 src="https://applications.zoom.us/integration/phone/embeddablephone/home"
                 id="zoom-embeddable-phone-iframe"
                 allow="clipboard-read; clipboard-write https://applications.zoom.us"
@@ -14,6 +16,22 @@ import { Component, input } from '@angular/core';
     `,
     styles: [``],
 })
-export class ZoomPhoneControlsComponent {
+export class ZoomPhoneControlsComponent extends AsyncHandler implements OnInit {
     public readonly mod = input('');
+    public readonly zoom_phone_el =
+        viewChild<ElementRef<HTMLIFrameElement>>('zoom_phone');
+
+    public ngOnInit() {
+        const on_event = (e) => this._handleMessage(e);
+        window.addEventListener('message', on_event);
+        this.subscription('zoom_frame_messages', () =>
+            window.removeEventListener('message', on_event),
+        );
+    }
+
+    private _handleMessage(event: MessageEvent) {
+        const data = event.data;
+        if (!data) return;
+        console.log('Zoom Phone Message Received:', data);
+    }
 }
