57c103591037101523e8003bc20229ec2fc33ad6
Alex Sorafumo <alex@yuion.net>
2025-07-16 21:46:24 +1000

    chore(explore): tweak user inaccurate location message

--- libs/explore/src/lib/explore-map-view.component.ts
+++ libs/explore/src/lib/explore-map-view.component.ts
@@ -337,7 +337,9 @@ export class ExploreMapViewComponent extends AsyncHandler implements OnInit {
         );
         if (!loc) {
             loc = locations[0];
-            notifyWarn(i18n(`EXPLORE.LOCATE_USER_FOUND_NO_PIN`));
+            notifyWarn(
+                i18n(`EXPLORE.LOCATE_USER_FOUND_NO_PIN`, { type: loc.type }),
+            );
         }
         this._state.setLevel(this._org.levelWithID([locations[0]?.level])?.id);
         const pos: any = loc.position;
