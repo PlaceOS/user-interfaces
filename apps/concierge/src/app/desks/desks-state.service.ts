6ff96ef786feb434f1af20ab34ecd12e1e0bf88b
Alex Sorafumo <alex@yuion.net>
2025-07-30 11:26:15 +1000

    chore(concierge): fix to previous commit

--- apps/concierge/src/app/desks/desks-state.service.ts
+++ apps/concierge/src/app/desks/desks-state.service.ts
@@ -287,6 +287,11 @@ export class DesksStateService extends AsyncHandler {
         const idx = desk_list.findIndex((_) => _.id === desk.id);
         if (idx >= 0) desk_list[idx] = new_desk;
         else desk_list.push(new_desk);
+        if (!new_desk.bookable) {
+            new_desk.assigned_to = '';
+            delete new_desk.assigned_name;
+            delete new_desk.assigned_user;
+        }
         await lastValueFrom(
             updateMetadata(zone, {
                 name: 'desks',
@@ -298,7 +303,6 @@ export class DesksStateService extends AsyncHandler {
             ref.componentInstance.loading = false;
             throw e;
         });
-        if (!new_desk.bookable) (new_desk as any).assigned_to = '';
         let recreate = false;
         if (
             desk.assigned_to &&
