c383abd6ddf5e98a45fe865d782d0a3eba9f86e1
Alex Sorafumo <alex@yuion.net>
2025-10-13 20:52:42 +1100

    chore(stagehand): add logic for handling alerts

--- apps/stagehand/src/app/dashboards/dashboards.service.ts
+++ apps/stagehand/src/app/dashboards/dashboards.service.ts
@@ -1,6 +1,12 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { i18n, notifySuccess } from '@placeos/common';
+import {
+    AsyncHandler,
+    firstTruthyValueFrom,
+    i18n,
+    log,
+    notifySuccess,
+} from '@placeos/common';
 import {
     listDashboardAlerts,
     PlaceAlert,
@@ -10,21 +16,155 @@ import {
     removeAlertDashboard,
     showAlert,
     showAlertDashboard,
+    token,
+    TriggerComparison,
+    TriggerConditionOperator,
 } from '@placeos/ts-client';
 import { openConfirmModal } from 'libs/components/src/lib/confirm-modal.component';
 import { lastValueFrom } from 'rxjs';
 
+import { OrganisationService } from '@placeos/organisation';
+import mqtt from 'mqtt';
+import { Alert } from '../alerts.service';
+
+export interface StateTopic {
+    region_id?: string;
+    building_id?: string;
+    level_id?: string;
+    area_id?: string;
+    system_id?: string;
+    driver_id?: string;
+    module_name?: string;
+    module_index?: string;
+    state_key?: string;
+}
+
+let _org_id: string = '';
+
+function topicToString(topic: StateTopic): string {
+    return `placeos/${_org_id || '+'}/+/${topic.region_id || '+'}/${topic.building_id || '+'}/${topic.level_id || '+'}/${topic.area_id || '+'}/${topic.system_id || '+'}/${topic.driver_id || '+'}/${topic.module_name || '+'}/${topic.module_index || '+'}/${topic.state_key || '+'}`;
+}
+
+function stringToTopic(topic: string): StateTopic {
+    const [
+        _,
+        _o,
+        _s,
+        bld_id,
+        lvl_id,
+        area_id,
+        _b,
+        sys_id,
+        dev_id,
+        mod_id,
+        mod_idx,
+        key,
+    ] = topic.split('/');
+    return {
+        building_id: bld_id,
+        level_id: lvl_id,
+        area_id: area_id,
+        system_id: sys_id,
+        driver_id: dev_id,
+        module_name: mod_id,
+        module_index: mod_idx,
+        state_key: key,
+    };
+}
+
+function compareAsTopic(item: TriggerComparison): string {
+    if (!(item.left instanceof Object)) return;
+    const parts = item.left.mod.split('_');
+    const module_index = parts.pop();
+    const module_name = parts.join('_');
+    const topic = topicToString({
+        module_name,
+        module_index,
+        state_key: item.left.status,
+    });
+    return topic;
+}
+
+function matchTopics(topic: string, comp: string): boolean {
+    const t_state = topic.split('/');
+    const comp_t_state = comp.split('/');
+    for (let i = 0; i < t_state.length; i++) {
+        if (
+            t_state[i] !== comp_t_state[i] &&
+            t_state[i] !== '+' &&
+            comp_t_state[i] !== '+'
+        ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+interface BrokerAlert {
+    time: number;
+    topic: StateTopic;
+    topic_str: string;
+    value: any;
+}
+
+function findCompareMatches(alerts: BrokerAlert[], item: TriggerComparison) {
+    const topic = compareAsTopic(item);
+    const matches: BrokerAlert[] = [];
+    for (const alert of alerts) {
+        if (matchTopics(topic, alert.topic_str)) {
+            switch (item.operator) {
+                case TriggerConditionOperator.EQ:
+                    item.right === alert.value ? matches.push(alert) : '';
+                    break;
+                case TriggerConditionOperator.NEQ:
+                    item.right !== alert.value ? matches.push(alert) : '';
+                    break;
+                case TriggerConditionOperator.GT:
+                    item.right > alert.value ? matches.push(alert) : '';
+                    break;
+                case TriggerConditionOperator.LT:
+                    item.right < alert.value ? matches.push(alert) : '';
+                    break;
+                case TriggerConditionOperator.GTE:
+                    item.right >= alert.value ? matches.push(alert) : '';
+                    break;
+                case TriggerConditionOperator.LTE:
+                    item.right <= alert.value ? matches.push(alert) : '';
+                    break;
+                case TriggerConditionOperator.AND:
+                    item.right && alert.value ? matches.push(alert) : '';
+                    break;
+                case TriggerConditionOperator.OR:
+                    item.right || alert.value ? matches.push(alert) : '';
+                    break;
+                case TriggerConditionOperator.XOR:
+                    (item.right as number) | alert.value
+                        ? matches.push(alert)
+                        : '';
+                    break;
+            }
+        }
+    }
+    return matches;
+}
+
 @Injectable({
     providedIn: 'root',
 })
-export class DashboardsService {
+export class DashboardsService extends AsyncHandler {
+    private _org = inject(OrganisationService);
     private _dialog = inject(MatDialog);
+
+    private _mqtt_broker;
+    private _alerts = signal([]);
 
     public readonly loading = signal<string[]>([]);
     public readonly dashboard = signal<PlaceAlertDashboard>(undefined);
     public readonly alert = signal<PlaceAlert>(undefined);
     public readonly dashboard_list = signal<PlaceAlertDashboard[]>([]);
     public readonly alerts_list = signal<PlaceAlert[]>([]);
+
+    public readonly dashboard_alerts = signal<Alert[]>([]);
 
     public async setAlert(id: string) {
         this.loading.update((l) => [...l, 'ALERT']);
@@ -100,5 +240,101 @@ export class DashboardsService {
         const alerts = await lastValueFrom(listDashboardAlerts(dashboard.id));
         this.alerts_list.set(alerts.data);
         this.loading.update((l) => l.filter((i) => i !== 'ALERT_LIST'));
+    }
+
+    public async listenForDashboardAlerts() {
+        this.dashboard_alerts.set([]);
+        const dashboard = this.dashboard();
+        if (!dashboard) return;
+        if (!this._mqtt_broker) await this._initialiseBroker();
+    }
+
+    private async _initialiseBroker() {
+        await firstTruthyValueFrom(this._org.initialised);
+        _org_id = this._org.organisation.id;
+        const jwt = token();
+        const url = `${location.host}/api/mqtt/`;
+        const secure = location.protocol === 'https:';
+        this._mqtt_broker = mqtt.connect(`ws${secure ? 's' : ''}://${url}`, {
+            username: jwt,
+            password: jwt,
+        });
+        this._mqtt_broker.on('connect', () => {
+            const alert_list = this.alerts_list();
+            for (const alert of alert_list) {
+                for (const item of alert.conditions.comparisons) {
+                    const topic = compareAsTopic(item);
+                    if (!topic) continue;
+                    log('ALERTS', 'Listening to topic:', topic);
+                    this._mqtt_broker.subscribe(topic);
+                }
+            }
+        });
+        this._mqtt_broker.on('message', (t, b) =>
+            this._handleBrokerMessage(t, b),
+        );
+    }
+
+    private _handleBrokerMessage(t: string, msg_buffer: Buffer) {
+        const topic = stringToTopic(t);
+        const data = JSON.parse(msg_buffer.toString());
+        const details = { ...data, topic, topic_str: t };
+        this._alerts.update((l) => [...l.filter((_) => _), details]);
+        console.log('Topic Message:', details);
+        this._updateDashboardAlertList();
+    }
+
+    private _updateDashboardAlertList() {
+        this.timeout(
+            'update_alerts',
+            () => {
+                const alert_list = this.alerts_list();
+                const alert_out: Alert[] = [];
+                for (const alert of alert_list) {
+                    const comparision_matches: any[][] = [];
+                    for (const item of alert.conditions.comparisons) {
+                        const matches = findCompareMatches(
+                            this._alerts(),
+                            item,
+                        );
+                        comparision_matches.push(matches);
+                    }
+                    console.log('Matches:', alert, comparision_matches);
+                    if (!comparision_matches.length) continue;
+                    const match_groups: any[][] = [];
+                    for (const match of comparision_matches[0]) {
+                        const group = [match];
+                        for (let i = 1; i < comparision_matches.length; i++) {
+                            const other_match = comparision_matches[i].find(
+                                (_) =>
+                                    Math.abs(_.time - match.time) <
+                                    alert.debounce_period,
+                            );
+                            if (other_match) group.push(other_match);
+                        }
+                        match_groups.push(group);
+                    }
+                    for (const group of match_groups) {
+                        if (
+                            group.length >= alert.conditions.comparisons.length
+                        ) {
+                            const { time, topic_str } = group[1];
+                            alert_out.push({
+                                id: `${alert.id}+${time}`,
+                                severity: alert.severity as any,
+                                type: alert.alert_type as any,
+                                location: topic_str,
+                                subject: alert.name,
+                                body: alert.description,
+                                status: 'open',
+                                timestamp: time * 1000,
+                            });
+                        }
+                    }
+                }
+                this.dashboard_alerts.set(alert_out);
+            },
+            10,
+        );
     }
 }
