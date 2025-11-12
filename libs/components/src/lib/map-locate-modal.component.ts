80bc1778f270997f8ec7725fbba40b20f9c62a42
Alex Sorafumo <alex@yuion.net>
2025-11-12 13:30:16 +1100

    chore: update styling for the map locate modal

--- libs/components/src/lib/map-locate-modal.component.ts
+++ libs/components/src/lib/map-locate-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, output } from '@angular/core';
+import { Component, OnInit, inject, output, signal } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { ViewerFeature, ViewerStyles } from '@placeos/svg-viewer';
 
@@ -27,22 +27,29 @@ export interface Locatable {
     selector: 'map-locate-modal',
     template: `
         <div class="h-[calc(100vh-4rem)] w-screen sm:h-auto sm:w-auto">
-            <header class="p-4">
+            <header
+                class="sticky top-0 z-10 m-2 h-14 w-[calc(100%-1rem)] min-w-[20rem] rounded border-none bg-base-200 p-2 flex items-center space-x-2"
+            >
                 <icon class="text-2xl">place</icon>
-                <h1 class="ml-2 text-xl font-medium">
-                    {{ item.display_name || item.name }}
-                </h1>
+                <h2 class="text-xl font-medium">
+                    {{ item().display_name || item().name }}
+                </h2>
+                <div class="flex-1"></div>
+                <button icon matRipple mat-dialog-close>
+                    <icon>close</icon>
+                </button>
             </header>
-            @if (level) {
+            <div class="px-2 pb-2">
+            @if (level()) {
                 <div
                     body
-                    class="relative h-[65vh] w-full overflow-hidden sm:max-h-[65vh]"
+                    class="relative h-[65vh] w-full overflow-hidden sm:max-h-[65vh] rounded-lg border border-base-300"
                 >
                     <interactive-map
                         class="pointer-events-none"
-                        [src]="level?.map_id"
-                        [focus]="item?.map_id"
-                        [features]="features"
+                        [src]="level().map_id"
+                        [focus]="item().map_id"
+                        [features]="features()"
                         [options]="{
                             disable_pan: true,
                             disable_zoom: true,
@@ -51,19 +58,13 @@ export interface Locatable {
                         <mat-spinner diameter="64"></mat-spinner
                     ></interactive-map>
                     <div
-                        class="absolute right-2 top-2 rounded-3xl border border-base-200 bg-base-100 px-4 py-2 shadow"
+                        class="absolute right-2 top-2 rounded-xl bg-base-200 border border-base-300 px-4 py-2 font-medium"
                     >
-                        {{ level?.display_name || level?.name }}
+                        {{ level().display_name || level().name }}
                     </div>
                 </div>
             }
-            <footer
-                class="flex w-full items-center justify-center border-t border-base-200 p-2"
-            >
-                <button btn matRipple class="inverse w-32" mat-dialog-close>
-                    {{ 'COMMON.BACK' | translate }}
-                </button>
-            </footer>
+                </div>
         </div>
     `,
     styles: [
@@ -77,7 +78,6 @@ export interface Locatable {
         MatRippleModule,
         MatDialogModule,
         MatProgressSpinnerModule,
-        TranslatePipe,
         IconComponent,
         InteractiveMapComponent,
     ],
@@ -91,24 +91,20 @@ export class MapLocateModalComponent extends AsyncHandler implements OnInit {
     /** Emitter for user action on the modal */
     public readonly event = output();
     /** Space to show the location of on the map */
-    public item: Locatable = this._data.item;
+    public readonly item = signal<Locatable>({} as any);
     /** Features of the map */
-    public features: ViewerFeature[];
+    public readonly features = signal<ViewerFeature[]>([]);
     /** Mapping of elements to CSS styles */
-    public style_map: ViewerStyles = {};
-
-    public get level(): BuildingLevel {
-        return this.item.level || this._org.levelWithID(this.item.zones || []);
-    }
-
-    constructor() {
-        super();
-        if (!this.item.level?.id) {
-            delete this.item.level;
-        }
-    }
+    public readonly style_map = signal<ViewerStyles>({});
+    /** Level that the locatable item is to be on **/
+    public readonly level = signal<BuildingLevel>(new BuildingLevel());
 
     public ngOnInit(): void {
+        this.item.set(this._data.item);
+        this.level.set(this.item().level || this._org.levelWithID(this.item().zones || []))
+        if (!this.level().id) {
+            this.level.set(this._org.levelWithID(this.item().zones || []))
+        }
         this.timeout(
             'init',
             () => {
@@ -121,26 +117,26 @@ export class MapLocateModalComponent extends AsyncHandler implements OnInit {
 
     public processStyles(): void {
         const styles: ViewerStyles = {};
-        if (this.item?.map_id) {
+        if (this.item().map_id) {
             styles[`#zones`] = { display: 'none' };
             styles[`#Zones`] = { display: 'none' };
         }
-        this.style_map = styles;
+        this.style_map.set(styles);
     }
 
     /** Point on map to focus on */
     public processFeature(): void {
-        if (!this.item) return null;
+        if (!this.item()) return null;
         const focus = {
-            location: this.item.map_id,
+            location: this.item().map_id,
             track_id: `focus_item`,
             content: MapPinComponent,
             data: {
-                name: this.item.name,
+                name: this.item().name,
             },
             z_index: 99,
             zoom: 100,
         };
-        this.features = [focus];
+        this.features.set([focus]);
     }
 }
