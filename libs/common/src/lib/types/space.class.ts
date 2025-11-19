afc2b424a323774fb69faaa734dd5016c4a9529e
Alex Sorafumo <alex@yuion.net>
2025-11-19 16:22:27 +1100

    chore: add new system fields to space class

--- libs/common/src/lib/types/space.class.ts
+++ libs/common/src/lib/types/space.class.ts
@@ -28,6 +28,10 @@ export class Space {
     public readonly feature_list: string[];
     /** URL to a supporting site for space */
     public readonly support_url: string;
+    /** URL to a supporting site for space */
+    public readonly camera_url: string;
+    /** URL to a supporting site for space */
+    public readonly camera_support_url: string;
     /** URL to an external booking service */
     public readonly room_booking_url: string;
     /** ID of the space on the associated map */
@@ -61,6 +65,8 @@ export class Space {
         this.bookable = !!data.bookable;
         this.zones = data.zones || [];
         this.support_url = data.support_url || '';
+        this.camera_url = data.camera_url || '';
+        this.camera_support_url = data.camera_support_url || '';
         this.room_booking_url = data.room_booking_url || '';
         this.map_id = data.map_id || '';
         this.images = data.images || [];
