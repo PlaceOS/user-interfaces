f6aa2e01372e28a4f0ad218a1e426af323e4aebc
Alex Sorafumo <alex@yuion.net>
2025-12-04 16:09:11 +1100

    chore(concierge): remove permission modal for uploads

--- apps/concierge/src/app/app.module.ts
+++ apps/concierge/src/app/app.module.ts
@@ -32,12 +32,11 @@ import localeFr from '@angular/common/locales/fr';
 import localeIt from '@angular/common/locales/it';
 import localeJa from '@angular/common/locales/ja';
 import localeZh from '@angular/common/locales/zh';
-import { LocaleService, UPLOAD_PERMISSIONS_MODAL } from '@placeos/common';
+import { LocaleService } from '@placeos/common';
 import {
     GlobalBannerComponent,
     GlobalLoadingComponent,
 } from '@placeos/components';
-import { UploadPermissionsModalComponent } from 'libs/components/src/lib/upload-permissions-modal.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -77,10 +76,6 @@ import { AppComponent } from './app.component';
             provide: LOCALE_ID,
             deps: [LocaleService],
             useFactory: (localeService: LocaleService) => localeService.locale,
-        },
-        {
-            provide: UPLOAD_PERMISSIONS_MODAL,
-            useValue: UploadPermissionsModalComponent,
         },
         provideHttpClient(withInterceptorsFromDi()),
     ],
