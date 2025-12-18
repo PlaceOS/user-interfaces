6b16bf13e3495fd087106931d13aeb5831480a32
MrYuion <alex@yuion.net>
2025-12-18 05:44:36 +0000

    build(concierge): [custom/HIO687AS] CI build from 76a7e8d

---
+++ chunk-6WLBNKT2.js
@@ -0,0 +1,13 @@
+import {
+  addMonths
+} from "./chunk-LG5FGLUS.js";
+
+// node_modules/date-fns/subMonths.js
+function subMonths(date, amount, options) {
+  return addMonths(date, -amount, options);
+}
+
+export {
+  subMonths
+};
+//# sourceMappingURL=chunk-6WLBNKT2.js.map
