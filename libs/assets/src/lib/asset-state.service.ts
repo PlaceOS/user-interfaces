69ec7c4432cf1c216de8aeeb34e7a1babb60b70e
Alex Sorafumo <alex@yuion.net>
2026-02-25 13:25:13 +1100

    fix(assets): hide assets from being selected if category is hidden

--- libs/assets/src/lib/asset-state.service.ts
+++ libs/assets/src/lib/asset-state.service.ts
@@ -127,22 +127,41 @@ export class AssetStateService {
 
     public readonly category_list = this._org.active_building.pipe(
         switchMap((bld) => queryAssetCategories({ zone_id: bld.id })),
-        map((_) => _.sort((a, b) => a.name.localeCompare(b.name))),
+        map((_) =>
+            _.sort((a, b) => a.name.localeCompare(b.name)).filter(
+                (c) => !c.hidden,
+            ),
+        ),
+        shareReplay(1),
+    );
+
+    public readonly visible_category_ids = this.category_list.pipe(
+        map((list) => list.map((item) => item.id)),
+        tap((visible_ids) => {
+            const selected_categories = this._category.getValue();
+            const valid_categories = selected_categories.filter((item) =>
+                visible_ids.includes(item),
+            );
+            if (valid_categories.length !== selected_categories.length) {
+                this._category.next(valid_categories);
+            }
+        }),
         shareReplay(1),
     );
 
     public readonly filtered_assets = combineLatest([
         this._search,
         this._category,
+        this.visible_category_ids,
         this.available_groups,
         this.rules,
     ]).pipe(
-        map(([search, category, assets, rules]) => {
+        map(([search, category, visible_categories, assets, rules]) => {
             const s = search.toLowerCase();
-            console.log('Rules:', rules);
             const list = assets.filter(
                 (_) =>
                     _.assets?.length &&
+                    visible_categories.includes(_.category_id) &&
                     (!category.length || category.includes(_.category_id)) &&
                     (_.name.toLowerCase().includes(s) ||
                         _.description.toLowerCase().includes(s)) &&
