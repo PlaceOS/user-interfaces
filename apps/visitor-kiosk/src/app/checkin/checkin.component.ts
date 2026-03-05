a08ee7c3e35b834a944ff4795e37717e0052efd7
Alex Sorafumo <alex@yuion.net>
2026-03-05 16:05:26 +1100

    fix(visitor-kiosk): fix navigating to right view when induction no enabled

--- apps/visitor-kiosk/src/app/checkin/checkin.component.ts
+++ apps/visitor-kiosk/src/app/checkin/checkin.component.ts
@@ -1,24 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SettingsService } from '@placeos/common';
+import { settingSignal } from '@placeos/common';
+import { AuthenticatedImageDirective } from '@placeos/components';
 import { startOfMinute } from 'date-fns';
 
 @Component({
     selector: '[app-checkin]',
     template: `
         <!-- <a-topbar-header class="w-full screen-only"></a-topbar-header> -->
-        <div
-            class="absolute inset-0 flex items-center p-8 print:hidden"
-            [style.background-image]="'url(' + background + ')'"
-        >
+        <div class="absolute inset-0 flex items-center p-8 print:hidden">
+            <img
+                auth
+                [source]="background()"
+                class="absolute top-1/2 left-1/2 min-h-full min-w-full -translate-x-1/2 -translate-y-1/2"
+            />
             <div class="z-10 flex w-full flex-col justify-center space-y-8">
                 <router-outlet></router-outlet>
             </div>
             <div class="absolute top-4 right-4 text-2xl text-white">
                 {{ now | date: 'mediumDate' }} {{ now | date: 'shortTime' }}
             </div>
-            @if (!hide_building_image) {
+            @if (!hide_building_image()) {
                 <img
                     src="assets/img/building.png"
                     class="absolute right-0 bottom-0 w-[60%]"
@@ -36,20 +39,13 @@ import { startOfMinute } from 'date-fns';
             }
         `,
     ],
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, AuthenticatedImageDirective],
 })
 export class CheckinComponent {
-    private _settings = inject(SettingsService);
+    public readonly background = settingSignal('welcome_background');
+    public readonly hide_building_image = settingSignal('hide_building_image');
 
     public get now() {
         return startOfMinute(new Date());
-    }
-
-    public get background() {
-        return this._settings.get('app.welcome_background');
-    }
-
-    public get hide_building_image() {
-        return this._settings.get('app.hide_building_image');
     }
 }
