de8123c19a3d55c6a1f3bf0c5e7763577f828ae1
Alex Sorafumo <alex@yuion.net>
2026-04-03 17:57:53 +1100

    chore(map-kiosk): update components to use signals

--- apps/map-kiosk/src/tests/explore.component.spec.ts
+++ apps/map-kiosk/src/tests/explore.component.spec.ts
@@ -1,3 +1,4 @@
+import { signal } from '@angular/core';
 import { createRoutingFactory, SpectatorRouting } from '@ngneat/spectator/jest';
 import {
     Building,
@@ -54,12 +55,23 @@ describe('ExploreComponent', () => {
             MockProvider(ExploreStateService, {
                 options: of({}),
                 level: of({}) as any,
+                positions: { zoom: 1, center: { x: 0.5, y: 0.5 } },
+                map_url: of(''),
+                map_styles: of({ text: { display: 'none' } }),
+                map_positions: of({ zoom: 1, center: { x: 0.5, y: 0.5 } }),
+                map_features: of([]),
+                map_actions: of([]),
+                map_labels: of([]),
                 setPositions: jest.fn(),
                 setFeatures: jest.fn(),
+                setOptions: jest.fn(),
+                setLevel: jest.fn(),
             } as any),
             MockProvider(SettingsService, {
                 get: jest.fn(),
                 initialised: of(true),
+                theme_signal: signal('light'),
+                signal: jest.fn(() => signal(undefined)),
             }),
             MockProvider(SpacesService, {
                 initialised: of(true),
@@ -67,6 +79,8 @@ describe('ExploreComponent', () => {
             } as any),
             MockProvider(OrganisationService, {
                 initialised: of(true),
+                buildings: [new Building({ id: '1' })],
+                levelsForBuilding: jest.fn(() => []),
                 active_region: new BehaviorSubject(new Region({})),
                 active_building: new BehaviorSubject(new Building({ id: '1' })),
                 active_levels: new BehaviorSubject([]),
