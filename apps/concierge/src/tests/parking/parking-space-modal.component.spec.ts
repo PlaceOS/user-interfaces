ba19c8a03c9eecc881d4470744d7a537e0a8866d
Alex Sorafumo <alex@yuion.net>
2026-05-04 11:04:03 +1000

    feat(concierge): show parking space features

---
+++ apps/concierge/src/tests/parking/parking-space-modal.component.spec.ts
@@ -0,0 +1,39 @@
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { MockProvider } from 'ng-mocks';
+
+import { ParkingSpaceModalComponent } from '../../app/parking/parking-space-modal.component';
+
+describe('ParkingSpaceModalComponent', () => {
+    let spectator: Spectator<ParkingSpaceModalComponent>;
+    const data = {
+        id: 'space-1',
+        identifier: 'Bay 1',
+        map_id: 'bay-1',
+        features: ['EV Charger'],
+        place_groups: ['staff'],
+    };
+
+    const createComponent = createComponentFactory({
+        component: ParkingSpaceModalComponent,
+        providers: [
+            { provide: MAT_DIALOG_DATA, useValue: data },
+            MockProvider(MatDialogRef, { disableClose: false } as any),
+        ],
+    });
+
+    beforeEach(() => (spectator = createComponent()));
+
+    it('should not reuse the source features array', () => {
+        spectator.component.form.controls.features.setValue([
+            ...(spectator.component.form.controls.features.value || []),
+            'Covered',
+        ]);
+
+        expect(data.features).toEqual(['EV Charger']);
+        expect(spectator.component.form.controls.features.value).toEqual([
+            'EV Charger',
+            'Covered',
+        ]);
+    });
+});
