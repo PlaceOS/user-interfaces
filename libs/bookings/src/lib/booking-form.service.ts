14910b89c65ffe0a86b25ca0c5ef253cfee2cdba
Alex Sorafumo <alex@yuion.net>
2026-05-20 13:13:26 +1000

    chore(bookings): add plural version for group booking error

--- libs/bookings/src/lib/booking-form.service.ts
+++ libs/bookings/src/lib/booking-form.service.ts
@@ -2011,7 +2011,7 @@ export class BookingFormService extends AsyncHandler {
             throw i18n('BOOKINGS.GROUP_INSUFFICIENT_RESOURCES', {
                 available,
                 members: group_members.length,
-            });
+            }, available);
         }
         return final_resources;
     }
