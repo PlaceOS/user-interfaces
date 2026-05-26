b12862fb0788a842a81564d2f5bfefcde5e0c2f7
Alex Sorafumo <alex@yuion.net>
2026-05-26 12:17:35 +1000

    Revert "fix(bookings): preserve monthly room recurrence PPT-2509"
    This reverts commit 8294a49fbe1af3a35c92e29ccf8b881924b5d710.

--- libs/bookings/src/lib/booking.utilities.ts
+++ libs/bookings/src/lib/booking.utilities.ts
@@ -320,8 +320,7 @@ export async function findNearbyFeature(
 }
 
 export function newBookingFromCalendarEvent(event: CalendarEvent) {
-    const recurrence_start = event.recurrence?.start;
-    const date = recurrence_start || event.date || event.event_start * 1000;
+    const date = event.date || event.event_start * 1000;
     const recurrence = event.recurrence?.pattern
         ? toBookingRecurrence(fromEventRecurrence(event.recurrence), date)
         : {};
