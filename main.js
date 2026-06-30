ba76e6fa0a83b0df31a1cebcf84f769019bc4669
MrYuion <alex@yuion.net>
2026-06-30 02:15:23 +0000

    build(assistant-panel): [feat/ppt-2556-flex-catering][96fea51] CI build

--- main.js
+++ main.js
@@ -56814,15 +56814,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "135f53d",
-  "hash": "135f53d",
+  "raw": "96fea51",
+  "hash": "96fea51",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "135f53d",
+  "suffix": "96fea51",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1782455644957
+  "time": 1782785697039
 };
 
 // libs/common/src/lib/settings.service.ts
