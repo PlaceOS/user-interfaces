84d7b4eda1a6a780c0a86fa6775bfb6c23349604
MrYuion <alex@yuion.net>
2026-07-01 09:05:27 +0000

    build(assistant-panel): [custom/HIO687AS][f0e9ff2] CI build

--- main.js
+++ main.js
@@ -56829,15 +56829,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "517dbea",
-  "hash": "517dbea",
+  "raw": "f0e9ff2",
+  "hash": "f0e9ff2",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "517dbea",
+  "suffix": "f0e9ff2",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1782809133641
+  "time": 1782896700481
 };
 
 // libs/common/src/lib/settings.service.ts
