b1e991ad4e900a7d49d6598335665a537bf07dac
Alex Sorafumo <alex@yuion.net>
2026-07-02 16:36:26 +1000

    chore(signage-manager): style tweaks for distribution playlists

--- apps/signage-manager/src/app/playlists/playlist-items.component.ts
+++ apps/signage-manager/src/app/playlists/playlist-items.component.ts
@@ -308,10 +308,10 @@ import { SignageService } from '../signage.service';
                 </div>
             } @else if (items().length > 0) {
                 <div class="w-full flex-1 overflow-auto px-3 py-2" role="list">
-                    <div class="mb-2 flex justify-end">
+                    <div class="-mt-2 mb-2 flex justify-end">
                         <button
                             type="button"
-                            class="text-base-content/70 hover:bg-base-200 flex items-center gap-1 rounded px-2 py-1 text-xs font-medium"
+                            class="text-base-content/70 hover:bg-base-200 flex w-28 items-center justify-center gap-1 rounded px-2 py-1 text-xs font-medium"
                             matRipple
                             (click)="toggleAllSchedules($event)"
                         >
