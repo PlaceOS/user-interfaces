acfcad74d3a02ec45eae0c3c1752adcb8e015c71
Alex Sorafumo <alex@yuion.net>
2026-07-02 11:23:28 +1000

    feat(workplace): add ability to set meeting space from query parameter

--- apps/workplace/src/tests/book/meeting-flow.component.spec.ts
+++ apps/workplace/src/tests/book/meeting-flow.component.spec.ts
@@ -1,7 +1,9 @@
 import { signal } from '@angular/core';
+import { fakeAsync, flushMicrotasks } from '@angular/core/testing';
 import { createRoutingFactory, SpectatorRouting } from '@ngneat/spectator/jest';
+import { Space } from '@placeos/common';
 import { mockComponent } from '@placeos/common/tests';
-import { EventFormService } from '@placeos/events';
+import { EventFormService, SpacePipe } from '@placeos/events';
 import { MockProvider } from 'ng-mocks';
 import { BookMeetingFlowComponent } from '../../app/book/meeting-flow.component';
 import { MeetingFlowConfirmComponent } from '../../app/book/meeting-flow/meeting-flow-confirm.component';
@@ -11,6 +13,8 @@ import { MeetingFlowSuccessComponent } from '../../app/book/meeting-flow/meeting
 describe('BookMeetingFlowComponent', () => {
     let spectator: SpectatorRouting<BookMeetingFlowComponent>;
     const view = signal('form');
+    const model = signal({ resources: [] });
+    const transform_space = jest.fn();
     const createComponent = createRoutingFactory({
         component: BookMeetingFlowComponent,
         providers: [
@@ -22,7 +26,13 @@ describe('BookMeetingFlowComponent', () => {
                 listenForStatusChanges: jest.fn(),
                 last_success: signal(null),
                 available_spaces: signal([]),
+                model,
             } as any),
+        ],
+        componentProviders: [
+            MockProvider(SpacePipe, {
+                transform: transform_space,
+            }),
         ],
         declarations: [
             mockComponent(MeetingFlowFormComponent),
@@ -39,6 +49,8 @@ describe('BookMeetingFlowComponent', () => {
             spectator.detectChanges();
         });
         event_service.setView('form');
+        model.set({ resources: [] });
+        transform_space.mockReset();
     });
 
     it('should create component', () =>
@@ -67,4 +79,13 @@ describe('BookMeetingFlowComponent', () => {
         spectator.setRouteParam('step', 'success');
         expect(spectator.query('meeting-flow-success')).toExist();
     });
+
+    it('should set selected room based on query params', fakeAsync(() => {
+        const room = new Space({ id: 'space-1', email: 'space-1@placeos.com' });
+        transform_space.mockResolvedValue(room);
+        spectator.setRouteQueryParam('space_id', 'space-1');
+        spectator.detectChanges();
+        flushMicrotasks();
+        expect(model().resources).toEqual([room]);
+    }));
 });
