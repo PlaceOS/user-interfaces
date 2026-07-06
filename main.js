d008bd7515da981c12808764845804ee550d4ea3
MrYuion <alex@yuion.net>
2026-07-06 05:50:09 +0000

    build(caterer-ui): [custom/HIO687AS][c4f5699] CI build

--- main.js
+++ main.js
@@ -56588,15 +56588,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "d68f7e6",
-  "hash": "d68f7e6",
+  "raw": "c4f5699",
+  "hash": "c4f5699",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "d68f7e6",
+  "suffix": "c4f5699",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1783148506396
+  "time": 1783316890447
 };
 
 // libs/common/src/lib/settings.service.ts
@@ -82826,6 +82826,7 @@ var Booking = class {
     this.images = data.images || [];
     this.all_day = !!data.all_day || custom_all_day || this.duration >= 24 * 60;
     this.induction = data.induction || void 0;
+    this.created_at = data.created_at || Date.now();
     if (this.all_day) {
       if (!data.duration && !data.date_end && !data.booking_end) {
         this.date = startOfDayInTimezone(this.date, this.timezone);
@@ -82880,6 +82881,7 @@ var Booking = class {
     data.zones = data.zones.filter((_3) => _3);
     delete data.date;
     delete data.duration;
+    delete data.created_at;
     delete data.process_state;
     removeEmptyFields(data);
     return data;
