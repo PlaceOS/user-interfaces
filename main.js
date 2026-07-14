a8aba1f5d5db648291e61c60a2655d15df9a0e3b
MrYuion <alex@yuion.net>
2026-07-14 04:11:38 +0000

    build(map-kiosk): [custom/ACB012M][48e665b] CI build

--- main.js
+++ main.js
@@ -55948,15 +55948,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "72d21fe",
-  "hash": "72d21fe",
+  "raw": "48e665b",
+  "hash": "48e665b",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "72d21fe",
+  "suffix": "48e665b",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1783937267353
+  "time": 1784002266246
 };
 
 // libs/common/src/lib/settings.service.ts
@@ -107799,8 +107799,10 @@ function appName2() {
   return setting("app.name") || setting("app.short_name") || "PlaceOS";
 }
 function withAppVersion2(data) {
-  return __spreadProps(__spreadValues({}, data), {
-    extension_data: __spreadProps(__spreadValues({}, data.extension_data || {}), {
+  const booking_data = __spreadValues({}, data);
+  delete booking_data.created_at;
+  return __spreadProps(__spreadValues({}, booking_data), {
+    extension_data: __spreadProps(__spreadValues({}, booking_data.extension_data || {}), {
       app_name: appName2(),
       app_version: APP_VERSION2
     })
