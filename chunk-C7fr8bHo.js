4925f86e4806782c274fcd3501724f64dc014ef7
MrYuion <alex@yuion.net>
2026-07-14 11:09:16 +0000

    build(concierge): [76fbd54] CI build

---
+++ chunk-C7fr8bHo.js
@@ -0,0 +1,1 @@
+import {m,S as ST,ct as qn}from'./main.js';var f=(()=>{class t{constructor(){this._org=m(ST);}transform(e){return this._org.levelWithID(e instanceof Array?e:[e])}static{this.\u0275fac=function(n){return new(n||t)};}static{this.\u0275pipe=qn({name:"level",type:t,pure:true});}}return t})();export{f};//# sourceMappingURL=chunk-C7fr8bHo.js.map
