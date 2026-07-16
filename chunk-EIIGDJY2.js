7bd1cc03501bdd6a47b49a0c817a7c5f598fc885
MrYuion <alex@yuion.net>
2026-07-16 06:53:53 +0000

    build(visitor-kiosk): [custom/HIO687AS][dbdb776] CI build

---
+++ chunk-EIIGDJY2.js
@@ -0,0 +1,19 @@
+import {
+  encodeQR
+} from "./chunk-Q4DQ62MM.js";
+
+// libs/common/src/lib/qr-code.ts
+function generateQRCode(code, colorLight = "#fff0", colorDark = "#000") {
+  let svg = encodeQR(code, "svg", { ecc: "low", border: 1 });
+  if (colorLight && colorLight !== "#fff0" && colorLight !== "#0000") {
+    svg = svg.replace(">", `><rect width="100%" height="100%" style="fill:${colorLight};"/>`);
+  }
+  svg = svg.replace("<path", `<path style="fill:${colorDark};"`);
+  const encoded_svg = encodeURIComponent(svg);
+  return `data:image/svg+xml,${encoded_svg}`;
+}
+
+export {
+  generateQRCode
+};
+//# sourceMappingURL=chunk-EIIGDJY2.js.map
--- chunk-Q4DQ62MM.js
+++ chunk-Q4DQ62MM.js
@@ -1881,19 +1881,8 @@ function decodeQR(img, opts = {}) {
 }
 var decode_default = decodeQR;
 
-// libs/common/src/lib/qr-code.ts
-function generateQRCode(code, colorLight = "#fff0", colorDark = "#000") {
-  let svg = encodeQR(code, "svg", { ecc: "low", border: 1 });
-  if (colorLight && colorLight !== "#fff0" && colorLight !== "#0000") {
-    svg = svg.replace(">", `><rect width="100%" height="100%" style="fill:${colorLight};"/>`);
-  }
-  svg = svg.replace("<path", `<path style="fill:${colorDark};"`);
-  const encoded_svg = encodeURIComponent(svg);
-  return `data:image/svg+xml,${encoded_svg}`;
-}
-
 export {
-  decode_default,
-  generateQRCode
+  encodeQR,
+  decode_default
 };
-//# sourceMappingURL=chunk-R5XPVHHX.js.map
+//# sourceMappingURL=chunk-Q4DQ62MM.js.map
