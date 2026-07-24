dd4c15724aa18cf43cc4e164f37fe7be092faf68
w-le <will@place.technology>
2026-07-24 07:29:00 +0000

    build(workplace): [custom/HIO687AS-prod][dd30171] CI build

--- chunk-DR-6gGip.js
+++
@@ -1,1 +0,0 @@
-import {m,s as sr,dZ as $n}from'./main-RZM6YU5F.js';var f=(()=>{class t{constructor(){this._org=m(sr);}transform(e){return this._org.levelWithID(e instanceof Array?e:[e])}static{this.\u0275fac=function(n){return new(n||t)};}static{this.\u0275pipe=$n({name:"level",type:t,pure:true});}}return t})();export{f};
