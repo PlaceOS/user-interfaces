bc14e027209875b349d7689952e89e796144b110
stakach <steve@vontaka.ch>
2026-07-25 06:15:00 +0000

    build(concierge): [276f310] CI build

---
+++ chunk-C-7eQbvC.js
@@ -0,0 +1,1 @@
+import {m,D as DT,ct as Yn}from'./main.js';var f=(()=>{class t{constructor(){this._org=m(DT);}transform(e){return this._org.levelWithID(e instanceof Array?e:[e])}static{this.\u0275fac=function(n){return new(n||t)};}static{this.\u0275pipe=Yn({name:"level",type:t,pure:true});}}return t})();export{f};//# sourceMappingURL=chunk-C-7eQbvC.js.map
--- chunk-DTBgc0iW.js
+++
@@ -1,1 +0,0 @@
-import {m,T as TT,ct as qn}from'./main.js';var u=(()=>{class i{constructor(){this._org=m(TT);}transform(r){return this._org.buildings.find(t=>r instanceof Array?r.includes(t.id):t.id===r)}static{this.\u0275fac=function(t){return new(t||i)};}static{this.\u0275pipe=qn({name:"building",type:i,pure:true});}}return i})();export{u};//# sourceMappingURL=chunk-DTBgc0iW.js.map
