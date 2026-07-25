2dde2675623a70f31aa02337552591b2aa910c5e
stakach <steve@vontaka.ch>
2026-07-25 06:13:09 +0000

    build(workplace): [276f310] CI build

--- chunk-BVCJPQWa.js
+++
@@ -1,1 +0,0 @@
-import {m as m$1,v as vd,dy as qn}from'./main.js';var f=(()=>{class t{constructor(){this._org=m$1(vd);}transform(e){return this._org.levelWithID(e instanceof Array?e:[e])}static{this.\u0275fac=function(n){return new(n||t)};}static{this.\u0275pipe=qn({name:"level",type:t,pure:true});}}return t})();export{f};//# sourceMappingURL=chunk-BVCJPQWa.js.map
