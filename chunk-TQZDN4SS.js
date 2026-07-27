689d7ddf4efc139e40189ea4ce9bf7d1ab3e14b8
MrYuion <alex@yuion.net>
2026-07-27 10:58:28 +0000

    build(concierge): [custom/HIO687AS][4315d92] CI build

---
+++ chunk-TQZDN4SS.js
@@ -0,0 +1,38 @@
+import {
+  OrganisationService,
+  Pipe,
+  inject,
+  setClassMetadata,
+  ɵɵdefinePipe
+} from "./chunk-JPIONC7D.js";
+
+// libs/components/src/lib/building.pipe.ts
+var BuildingPipe = class _BuildingPipe {
+  constructor() {
+    this._org = inject(OrganisationService);
+  }
+  transform(id) {
+    return this._org.buildings.find((bld) => id instanceof Array ? id.includes(bld.id) : bld.id === id);
+  }
+  static {
+    this.\u0275fac = function BuildingPipe_Factory(__ngFactoryType__) {
+      return new (__ngFactoryType__ || _BuildingPipe)();
+    };
+  }
+  static {
+    this.\u0275pipe = /* @__PURE__ */ \u0275\u0275definePipe({ name: "building", type: _BuildingPipe, pure: true });
+  }
+};
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(BuildingPipe, [{
+    type: Pipe,
+    args: [{
+      name: "building"
+    }]
+  }], null, null);
+})();
+
+export {
+  BuildingPipe
+};
+//# sourceMappingURL=chunk-TQZDN4SS.js.map
---
+++ chunk-YIU3MQHE.js
@@ -0,0 +1,13 @@
+import {
+  addDays
+} from "./chunk-JPIONC7D.js";
+
+// node_modules/date-fns/subDays.js
+function subDays(date, amount, options) {
+  return addDays(date, -amount, options);
+}
+
+export {
+  subDays
+};
+//# sourceMappingURL=chunk-YIU3MQHE.js.map
