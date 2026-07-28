c5cc44cd665ef960848ccae03bcd6943dd2891e2
MrYuion <alex@yuion.net>
2026-07-28 04:11:10 +0000

    build(concierge): [custom/HIO687AS][675fd61] CI build

---
+++ chunk-V6ABSMDM.js
@@ -0,0 +1,15 @@
+import {
+  toDate
+} from "./chunk-SWNDH6XR.js";
+
+// node_modules/date-fns/setHours.js
+function setHours(date, hours, options) {
+  const _date = toDate(date, options?.in);
+  _date.setHours(hours);
+  return _date;
+}
+
+export {
+  setHours
+};
+//# sourceMappingURL=chunk-V6ABSMDM.js.map
