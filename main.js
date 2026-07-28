0b8ae3872a216a62c34f176e7c51c436ddef820f
MrYuion <alex@yuion.net>
2026-07-28 04:06:29 +0000

    build(outlook-addin): [custom/HIO687AS][675fd61] CI build

--- main.js
+++ main.js
@@ -67919,15 +67919,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION4 = {
   "dirty": false,
-  "raw": "4315d92",
-  "hash": "4315d92",
+  "raw": "675fd61",
+  "hash": "675fd61",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "4315d92",
+  "suffix": "675fd61",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1785149806278
+  "time": 1785211533922
 };
 
 // libs/common/src/lib/settings.service.ts
@@ -128438,13 +128438,14 @@ var BookingFormService = class _BookingFormService extends AsyncHandler {
     if (!booking?.id)
       return [];
     const parent_id = booking.parent_id || booking.id;
+    const legacy_group = `${booking.description || ""}`.startsWith("grp-") ? booking.description : "";
     const { type } = this._options();
     const list = await queryBookings({
       period_start: getUnixTime(booking.date),
       period_end: getUnixTime(addMinutes(booking.date, booking.duration)),
       type
     });
-    return list.filter((b2) => b2.id === parent_id || b2.parent_id === parent_id);
+    return list.filter((b2) => b2.id === parent_id || b2.parent_id === parent_id || !!legacy_group && b2.description === legacy_group);
   }
   async loadGroupMembersForBooking(booking) {
     if (!booking?.id)
