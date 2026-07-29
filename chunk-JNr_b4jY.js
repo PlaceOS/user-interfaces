24aee9474b5a403f5629bdb6f00f8c7e608af9e1
MrYuion <alex@yuion.net>
2026-07-29 10:07:40 +0000

    build(workplace): [1cf398e] CI build

---
+++ chunk-JNr_b4jY.js
@@ -0,0 +1,9 @@
+import {m as m$1,cp as qe,ec as ur,ed as q,ee as YP,B,ef as S,a as Te}from'./main.js';function U(n){n||(n=m$1(qe));let e=new q(t=>{if(n.destroyed){t.next();return}return n.onDestroy(t.next.bind(t))});return t=>t.pipe(ur(e))}function T(n){let e=YP(n);return new q(t=>{let s=e?.onDestroy(()=>t.complete()),i=n.subscribe(r=>t.next(r));return ()=>{i.unsubscribe(),s?.();}})}function F(n,e){let s=!e?.manualCleanup?e?.injector?.get(qe)??m$1(qe):null,i=m(e?.equal),r;e?.requireSync?r=B({kind:0},{equal:i}):r=B({kind:1,value:e?.initialValue},{equal:i});let c,v=n.subscribe({next:o=>r.set({kind:1,value:o}),error:o=>{r.set({kind:2,error:o}),c?.();},complete:()=>{c?.();}});if(e?.requireSync&&r().kind===0)throw new S(601,false);return c=s?.onDestroy(v.unsubscribe.bind(v)),Te(()=>{let o=r();switch(o.kind){case 1:return o.value;case 2:throw o.error;case 0:throw new S(601,false)}},{equal:e?.equal})}function m(n=Object.is){return (e,t)=>e.kind===1&&t.kind===1&&n(e.value,t.value)}/*! Bundled license information:
+
+@angular/core/fesm2022/rxjs-interop.mjs:
+  (**
+   * @license Angular v22.0.1
+   * (c) 2010-2026 Google LLC. https://angular.dev/
+   * License: MIT
+   *)
+*/export{F,T,U};//# sourceMappingURL=chunk-JNr_b4jY.js.map
