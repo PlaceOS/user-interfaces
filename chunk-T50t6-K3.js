ebb008d00afb930c9d8123816b2e829be5d8d9fb
stakach <steve@vontaka.ch>
2026-07-31 14:22:11 +0000

    build(workplace): [ce352a2] CI build

--- chunk-T50t6-K3.js
+++
@@ -1,1 +0,0 @@
-import {m as m$1,k as kd,dy as qn}from'./main.js';var f=(()=>{class t{constructor(){this._org=m$1(kd);}transform(e){return this._org.levelWithID(e instanceof Array?e:[e])}static{this.\u0275fac=function(n){return new(n||t)};}static{this.\u0275pipe=qn({name:"level",type:t,pure:true});}}return t})();export{f};//# sourceMappingURL=chunk-T50t6-K3.js.map
