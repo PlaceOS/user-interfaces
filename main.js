1079d7013550dc0fd93969ec428c87f2d68f3ffb
MrYuion <alex@yuion.net>
2026-08-03 02:46:51 +0000

    build(booking-panel): [custom/ACB012M][8d81a66] CI build

--- main.js
+++ main.js
@@ -55955,15 +55955,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "95f2ecc",
-  "hash": "95f2ecc",
+  "raw": "8d81a66",
+  "hash": "8d81a66",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "95f2ecc",
+  "suffix": "8d81a66",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1785470328183
+  "time": 1785725179944
 };
 
 // libs/common/src/lib/settings.service.ts
