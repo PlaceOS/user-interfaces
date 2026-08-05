917aa19e26beeca27fe490c4b61da3199cbeb7e8
MrYuion <MrYuion@users.noreply.github.com>
2026-08-05 13:21:29 +0000

    build(booking-panel): [custom/HIO687AS][a4c3ecb] CI build

--- main.js
+++ main.js
@@ -49059,6 +49059,8 @@ var BOOKINGS = {
   VISITOR_RECENT: "Recently Booked Visitors"
 };
 var CALENDAR_EVENT = {
+  HOST_CHANGE_WARNING: "The previous host will remain an attendee after changing the host.",
+  HOST_CHANGE_VISITORS_WARNING: "Changing the host will delete and recreate this event. Visitors may receive cancellation and replacement invitations.",
   TEAMS_MEETING: "Allow online attendees (Teams)",
   CATERING_ORDER_AT_DATE: "Order for {{ date }} at {{ time }}",
   CATERING_ORDER_AT: "Order at {{ time }}",
@@ -53957,6 +53959,7 @@ function setupFormTimeSync(model2, options2 = {}, injector) {
     date_end: finiteNumber(snap().date_end),
     all_day: snap().all_day
   };
+  let timed_window;
   const refreshPrev = () => {
     const s = snap();
     prev.date = finiteNumber(s.date);
@@ -54235,8 +54238,17 @@ function setupFormTimeSync(model2, options2 = {}, injector) {
   fieldEffect((v) => v.all_day, () => {
     const all_day = snap().all_day;
     if (all_day) {
+      timed_window = {
+        date: normaliseTimeValue(snap().date),
+        duration: normaliseTimeValue(snap().duration),
+        date_end: normaliseTimeValue(snap().date_end)
+      };
       applyPatch(getAllDayTimeRange(normaliseTimeValue(snap().date), timezone, all_day_start, all_day_end));
+    } else if (timed_window && !isMultiday(timed_window.date, normaliseTimeValue(snap().date))) {
+      applyPatch(timed_window);
+      timed_window = void 0;
     } else {
+      timed_window = void 0;
       const date = normaliseTimeValue(snap().date);
       const duration = normaliseTimeValue(snap().duration);
       const date_end = normaliseTimeValue(snap().date_end);
@@ -55772,15 +55784,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "fcedce6",
-  "hash": "fcedce6",
+  "raw": "a4c3ecb",
+  "hash": "a4c3ecb",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "fcedce6",
+  "suffix": "a4c3ecb",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1785485868853
+  "time": 1785936060962
 };
 
 // libs/common/src/lib/settings.service.ts
@@ -56780,6 +56792,9 @@ function reloadApp() {
     return;
   }
   _reload_timer = setTimeout(reloadApp, RELOAD_RETRY_MS);
+}
+function reloadForNewVersion() {
+  reloadApp();
 }
 function requestInitReload() {
   if (_init_reload) {
@@ -78253,6 +78268,7 @@ function getParameterizedRouteFromSnapshot(route) {
 
 // libs/common/src/lib/placeos.service.ts
 var START_QUERY = location.search;
+var AUTHORITY_WAIT_MS = 10 * 1e3;
 var LOADING_MESSAGE = signal(
   "Loading...",
   ...ngDevMode ? [{ debugName: "LOADING_MESSAGE" }] : (
@@ -78546,7 +78562,10 @@ var _PlaceOS_Service = class _PlaceOS_Service extends AsyncHandler {
     }
     if (!isNativeApp()) {
       setLoadingMessage("Authenticating...");
-      await setupPlace(settings).catch((_2) => console.error(_2));
+      await Promise.race([
+        setupPlace(settings).catch((_2) => console.error(_2)),
+        new Promise((resolve) => setTimeout(resolve, AUTHORITY_WAIT_MS))
+      ]);
     }
     if (this._initial_token)
       li(this._initial_token);
@@ -78633,8 +78652,7 @@ var _PlaceOS_Service = class _PlaceOS_Service extends AsyncHandler {
     if (!hasNewVersion())
       return;
     setLoadingMessage("Checking for updates...");
-    location.reload();
-    this.timeout("reload", () => location.href = `${location.origin}${location.pathname}`);
+    reloadForNewVersion();
   }
   async _initFixedDevice() {
     if (!rs())
@@ -78695,6 +78713,7 @@ var log3 = scoped_log("ORG");
 var ORG_CACHE_PREFIX = "PLACEOS.org";
 var ZONE_CACHE_PREFIX = `${ORG_CACHE_PREFIX}.zones`;
 var AUTHORITY_CACHE_KEY = `${ORG_CACHE_PREFIX}.authority`;
+var OFFLINE_BOOT_DELAY = 10 * 1e3;
 var METADATA_CACHE_PREFIX = `${ORG_CACHE_PREFIX}.metadata`;
 var MAX_CACHE_AGE2 = 7 * 24 * 60 * 60 * 1e3;
 function cachedAuthority() {
@@ -78945,7 +78964,12 @@ var _OrganisationService = class _OrganisationService {
     this._building_settings = {};
     this._skip_auto_selection = false;
     this._override_timer = null;
-    oi(Lr(), (_2) => _2).then(() => setTimeout(() => this.init(), 1e3));
+    const online = oi(Lr(), (_2) => _2);
+    const start = this._service.get("app.offline_boot") ? Promise.race([
+      online,
+      new Promise((resolve) => setTimeout(resolve, OFFLINE_BOOT_DELAY))
+    ]) : online;
+    start.then(() => setTimeout(() => this.init(), 1e3));
     effect(() => {
       this._active_region();
       const building = this._active_building();
@@ -79044,11 +79068,13 @@ var _OrganisationService = class _OrganisationService {
         this._setPublicData();
       });
     } else {
-      await this.load().catch((err) => {
+      try {
+        await this.load();
+      } catch {
         notifyError("Error loading organisation data. Retrying...");
         setTimeout(() => this.init(tries), Math.min(1e4, 300 * ++tries));
-        throw err;
-      });
+        return;
+      }
     }
     if (window.debug) {
       if (!window.app)
@@ -107750,11 +107776,37 @@ var BOOKING_URLS = [
 var PERSISTED_EVENT_CONTEXT_URLS = ["landing"];
 var IGNORED_DETAIL_FIELDS = [
   "attendees",
+  "body",
   "system",
   "date_end",
   "organiser",
+  "recurrence",
   "resources"
 ];
+function normaliseEventBody(body) {
+  const template = document.createElement("template");
+  template.innerHTML = body || "";
+  const serialise = (node) => {
+    if (node.nodeType === Node.TEXT_NODE) {
+      return (node.textContent || "").replace(/\u200b/g, "");
+    }
+    if (node.nodeType !== Node.ELEMENT_NODE)
+      return "";
+    const element = node;
+    if (element.tagName === "BR")
+      return "\n";
+    const content = [...element.childNodes].map(serialise).join("");
+    if (element.tagName === "DIV" || element.tagName === "P") {
+      return `
+${content}
+`;
+    }
+    const tag2 = element.tagName.toLowerCase();
+    const attributes = [...element.attributes].sort((a, b2) => a.name.localeCompare(b2.name)).map(({ name, value }) => ` ${name}="${value}"`).join("");
+    return `<${tag2}${attributes}>${content}</${tag2}>`;
+  };
+  return [...template.content.childNodes].map(serialise).join("").replace(/[ \t]+\n|\n[ \t]+/g, "\n").replace(/\n+/g, "\n").trim();
+}
 var Tags;
 (function(Tags2) {
   Tags2["Availability"] = "AVAILABILITY";
@@ -108620,10 +108672,24 @@ var _EventFormService = class _EventFormService extends AsyncHandler {
   _eventDetails(value) {
     var _a11;
     const details = Object.entries(value).filter(([key]) => !IGNORED_DETAIL_FIELDS.includes(key));
+    const recurrence = value.recurrence;
+    details.push(["body", normaliseEventBody(value.body)]);
     details.push(["host_email", ((_a11 = value.organiser) == null ? void 0 : _a11.email) || ""]);
     details.push([
+      "recurrence",
+      (recurrence == null ? void 0 : recurrence.pattern) && (recurrence == null ? void 0 : recurrence._pattern) !== "none" ? [
+        recurrence.pattern,
+        recurrence.interval || 1,
+        [...recurrence.days_of_week || []].sort(),
+        recurrence.nth_of_month || null,
+        recurrence.start || null,
+        recurrence.end || null,
+        recurrence.occurrences || null
+      ] : null
+    ]);
+    details.push([
       "space_ids",
-      (value.resources || []).map((_2) => _2.id || _2.email || "")
+      (value.resources || []).map((_2) => (_2.email || _2.id || "").toLowerCase()).sort()
     ]);
     details.sort(([a], [b2]) => a > b2 ? 1 : -1);
     return JSON.stringify(details);
