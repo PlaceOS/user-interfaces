5d7547fe613458ee379099e5eee4b753ddd4bdc8
MrYuion <MrYuion@users.noreply.github.com>
2026-08-05 13:21:37 +0000

    build(workplace): [custom/HIO687AS][a4c3ecb] CI build

---
+++ chunk-OOVYQY56.js
@@ -0,0 +1,45 @@
+import {
+  email,
+  form,
+  required,
+  validate
+} from "./chunk-YKP5746A.js";
+import {
+  signal
+} from "./chunk-2XZZDWFL.js";
+
+// libs/users/src/lib/user.utilities.ts
+var PHONE_PATTERN = /^\+?(\d[\d\s\-\(\)]{5,13}\d)$/;
+var USER_DOMAIN = "@place.tech";
+function generateUserForm(user, injector) {
+  if (!user) {
+    throw Error("No user passed");
+  }
+  const model = signal(
+    {
+      name: user.name || "",
+      email: user.email || "",
+      organisation: user.organisation || "",
+      phone: user.phone || "",
+      assistance_required: user.assistance_required || false,
+      visit_expected: user.visit_expected ?? true
+    },
+    ...ngDevMode ? [{ debugName: "model" }] : (
+      /* istanbul ignore next */
+      []
+    )
+  );
+  return form(model, (p) => {
+    required(p.name);
+    required(p.email);
+    email(p.email);
+    required(p.organisation);
+    validate(p.phone, ({ value }) => value() && !PHONE_PATTERN.test(value()) ? { kind: "pattern" } : void 0);
+  }, injector ? { injector } : void 0);
+}
+
+export {
+  USER_DOMAIN,
+  generateUserForm
+};
+//# sourceMappingURL=chunk-OOVYQY56.js.map
