f23ccb4df320fd8f61d60ff5ca5986e1c27b84b4
camreeves <camreeves@users.noreply.github.com>
2026-08-05 10:43:33 +0000

    build(booking-panel): [fix/PPT-2643-remaining-init-paths][cc6bb36] CI build

--- main.js
+++ main.js
@@ -55648,15 +55648,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "041ad2f",
-  "hash": "041ad2f",
+  "raw": "cc6bb36",
+  "hash": "cc6bb36",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "041ad2f",
+  "suffix": "cc6bb36",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1785910904096
+  "time": 1785926585431
 };
 
 // libs/common/src/lib/settings.service.ts
