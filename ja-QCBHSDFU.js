854ae9a555c381429dcb291218983add0589bf5d
camreeves <camreeves@users.noreply.github.com>
2026-08-05 18:22:48 +0000

    build(assistant-panel): [6dcc550] CI build

---
+++ ja-QCBHSDFU.js
@@ -0,0 +1,13 @@
+import"./chunk-HGJ4MBLN.js";function m(u){let n=u;return 5}var s=["ja",[["\u5348\u524D","\u5348\u5F8C"]],void 0,[["\u65E5","\u6708","\u706B","\u6C34","\u6728","\u91D1","\u571F"],void 0,["\u65E5\u66DC\u65E5","\u6708\u66DC\u65E5","\u706B\u66DC\u65E5","\u6C34\u66DC\u65E5","\u6728\u66DC\u65E5","\u91D1\u66DC\u65E5","\u571F\u66DC\u65E5"],["\u65E5","\u6708","\u706B","\u6C34","\u6728","\u91D1","\u571F"]],void 0,[["1","2","3","4","5","6","7","8","9","10","11","12"],["1\u6708","2\u6708","3\u6708","4\u6708","5\u6708","6\u6708","7\u6708","8\u6708","9\u6708","10\u6708","11\u6708","12\u6708"]],void 0,[["BC","AD"],["\u7D00\u5143\u524D","\u897F\u66A6"]],0,[6,0],["y/MM/dd",void 0,"y\u5E74M\u6708d\u65E5","y\u5E74M\u6708d\u65E5EEEE"],["H:mm","H:mm:ss","H:mm:ss z","H\u6642mm\u5206ss\u79D2 zzzz"],["{1} {0}",void 0,void 0,void 0],[".",",",";","%","+","-","E","\xD7","\u2030","\u221E","NaN",":"],["#,##0.###","#,##0%","\xA4#,##0.00","#E0"],"JPY","\uFFE5","\u65E5\u672C\u5186",{BYN:[void 0,"\u0440."],CNY:["\u5143","\uFFE5"],JPY:["\uFFE5"],PHP:[void 0,"\u20B1"],RON:[void 0,"\u30EC\u30A4"],XXX:[]},"ltr",m];export{s as default};
+/*! Bundled license information:
+
+@angular/common/locales/ja.js:
+  (**
+   * @license
+   * Copyright Google LLC All Rights Reserved.
+   *
+   * Use of this source code is governed by an MIT-style license that can be
+   * found in the LICENSE file at https://angular.dev/license
+   *)
+*/
+//# sourceMappingURL=ja-QCBHSDFU.js.map
