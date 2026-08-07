3b64bad82c46d02ce2e358409f9a6aa3828f0aad
MrYuion <MrYuion@users.noreply.github.com>
2026-08-07 06:10:01 +0000

    build(signage): [custom/HIO687AS][c8fecf4] CI build

--- main.js
+++ main.js
@@ -54094,15 +54094,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "a4c3ecb",
-  "hash": "a4c3ecb",
+  "raw": "c8fecf4",
+  "hash": "c8fecf4",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "a4c3ecb",
+  "suffix": "c8fecf4",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1785936061121
+  "time": 1786082973405
 };
 
 // libs/common/src/lib/settings.service.ts
