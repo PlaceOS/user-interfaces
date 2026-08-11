02c1a7805cf7c1a3b3450d43fc2e51246e4a604e
MrYuion <MrYuion@users.noreply.github.com>
2026-08-11 04:17:35 +0000

    build(control): [custom/HIO687AS][8c711a1] CI build

--- main.js
+++ main.js
@@ -63227,6 +63227,7 @@ var SIGNAGE_MANAGER = {
   SEARCH_GROUPS: "Search groups",
   SEARCH_MEDIA_ARIA: "Search media",
   SEARCH_PLAYLISTS: "Search playlists",
+  SEARCH_IN_ZONE: "Search in {{ name }}",
   SEARCH_USERS: "Search users",
   SEARCH_ZONES: "Search zones",
   SEARCH_ZONES_PLAYLISTS: "Search zones or playlists",
@@ -63771,6 +63772,7 @@ var FORM = {
   DURATION: "Duration",
   CLEAR: "Clear form",
   RESET: "Reset form",
+  RESET_NOTES: "Reset notes",
   NOTES: "Notes",
   RECURRENCE: "Recurrence",
   RECURRENCE_REPEAT_EVERY: "Repeat Every",
@@ -70068,15 +70070,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION4 = {
   "dirty": false,
-  "raw": "c8fecf4",
-  "hash": "c8fecf4",
+  "raw": "8c711a1",
+  "hash": "8c711a1",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "c8fecf4",
+  "suffix": "8c711a1",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1786082992365
+  "time": 1786421826540
 };
 
 // libs/common/src/lib/settings.service.ts
