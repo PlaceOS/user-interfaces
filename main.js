795be0c8222925b7993262f562396f99cbb4f246
MrYuion <MrYuion@users.noreply.github.com>
2026-08-12 05:21:46 +0000

    build(map-kiosk): [custom/HIO687AS][dd6b22e] CI build

--- main.js
+++ main.js
@@ -55766,15 +55766,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "8c711a1",
-  "hash": "8c711a1",
+  "raw": "dd6b22e",
+  "hash": "dd6b22e",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "8c711a1",
+  "suffix": "dd6b22e",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1786421829403
+  "time": 1786512077131
 };
 
 // libs/common/src/lib/settings.service.ts
@@ -106520,10 +106520,10 @@ var AuthorisedUserGuard = class _AuthorisedUserGuard {
     return this.checkUser();
   }
   async checkUser() {
-    const state_ready = await resolvedWithin(Promise.all([
+    const state_ready = await this.waitForBackend(Promise.all([
       this._org.waitUntilInitialised(),
       firstValueWhere(user_groups_loaded, Boolean, this._injector)
-    ]), OFFLINE_FALLBACK_DELAY);
+    ]));
     if (!state_ready)
       return this.offlineAccess();
     const groups = this._access?.group ? [this._access.group] : this._settings.get("app.allow_access_groups") || [];
@@ -106552,12 +106552,19 @@ var AuthorisedUserGuard = class _AuthorisedUserGuard {
   }
   /** The active user, or null if the backend could not be reached in time */
   async waitForUser() {
-    const online = await resolvedWithin(oi(Lr(), Boolean), OFFLINE_FALLBACK_DELAY);
+    const online = await this.waitForBackend(oi(Lr(), Boolean));
     if (!online)
       return null;
     let user = null;
-    const loaded = await resolvedWithin(firstTruthyValueFrom(current_user).then((_2) => user = _2), OFFLINE_FALLBACK_DELAY);
+    const loaded = await this.waitForBackend(firstTruthyValueFrom(current_user).then((_2) => user = _2));
     return loaded ? user : null;
+  }
+  async waitForBackend(promise) {
+    if (this._settings.get("app.offline_boot")) {
+      return resolvedWithin(promise, OFFLINE_FALLBACK_DELAY);
+    }
+    await promise;
+    return true;
   }
   /**
    * Access decision for when the backend cannot be reached. Waiting forever
