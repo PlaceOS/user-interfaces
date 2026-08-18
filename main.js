7dd48a4eeb7276d2e2dfb302bb14836003910d4a
MrYuion <MrYuion@users.noreply.github.com>
2026-08-18 03:32:03 +0000

    build(map-kiosk): [custom/HIO687AS][8a82cb4] CI build

--- main.js
+++ main.js
@@ -55815,15 +55815,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "7260d8b",
-  "hash": "7260d8b",
+  "raw": "8a82cb4",
+  "hash": "8a82cb4",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "7260d8b",
+  "suffix": "8a82cb4",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1786946508990
+  "time": 1787023897258
 };
 
 // libs/common/src/lib/settings.service.ts
