536f97a79c54169bef2a656c3d611f4f1b820085
MrYuion <MrYuion@users.noreply.github.com>
2026-08-27 03:38:57 +0000

    build(concierge): [custom/HIO687AS][dea9607] CI build

---
+++ chunk-FJ6FQGT2.js
@@ -0,0 +1,15 @@
+import {
+  toDate
+} from "./chunk-UGAOPV43.js";
+
+// node_modules/date-fns/setHours.js
+function setHours(date, hours, options) {
+  const _date = toDate(date, options?.in);
+  _date.setHours(hours);
+  return _date;
+}
+
+export {
+  setHours
+};
+//# sourceMappingURL=chunk-FJ6FQGT2.js.map
