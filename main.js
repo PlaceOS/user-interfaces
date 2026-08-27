2957156d380d93f802b143fe22ed1dd39496db09
MrYuion <MrYuion@users.noreply.github.com>
2026-08-27 02:10:08 +0000

    build(caterer-ui): [custom/HIO687AS][5e74d5d] CI build

--- main.js
+++ main.js
@@ -56653,15 +56653,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "20f78e5",
-  "hash": "20f78e5",
+  "raw": "5e74d5d",
+  "hash": "5e74d5d",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "20f78e5",
+  "suffix": "5e74d5d",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1787714070703
+  "time": 1787796498217
 };
 
 // libs/common/src/lib/settings.service.ts
