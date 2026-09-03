be3135b5cd232cc4e7a505375b984bb13fb9152c
MrYuion <MrYuion@users.noreply.github.com>
2026-09-03 04:23:04 +0000

    build(signage): [2c82f40] CI build

---
+++ ja-DngrQVLH.js
@@ -0,0 +1,15 @@
+function m(u){return 5}var s=[`ja`,[[`午前`,`午後`]],void 0,[[`日`,`月`,`火`,`水`,`木`,`金`,`土`],void 0,[`日曜日`,`月曜日`,`火曜日`,`水曜日`,`木曜日`,`金曜日`,`土曜日`],[`日`,`月`,`火`,`水`,`木`,`金`,`土`]],void 0,[[`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`10`,`11`,`12`],[`1月`,`2月`,`3月`,`4月`,`5月`,`6月`,`7月`,`8月`,`9月`,`10月`,`11月`,`12月`]],void 0,[[`BC`,`AD`],[`紀元前`,`西暦`]],0,[6,0],[`y/MM/dd`,void 0,`y年M月d日`,`y年M月d日EEEE`],[`H:mm`,`H:mm:ss`,`H:mm:ss z`,`H時mm分ss秒 zzzz`],[`{1} {0}`,void 0,void 0,void 0],[`.`,`,`,`;`,`%`,`+`,`-`,`E`,`×`,`‰`,`∞`,`NaN`,`:`],[`#,##0.###`,`#,##0%`,`¤#,##0.00`,`#E0`],`JPY`,`￥`,`日本円`,{BYN:[void 0,`р.`],CNY:[`元`,`￥`],JPY:[`￥`],PHP:[void 0,`₱`],RON:[void 0,`レイ`],XXX:[]},`ltr`,m];
+/*! Bundled license information:
+
+@angular/common/locales/ja.js:
+(**
+* @license
+* Copyright Google LLC All Rights Reserved.
+*
+* Use of this source code is governed by an MIT-style license that can be
+* found in the LICENSE file at https://angular.dev/license
+*)
+*/
+export{s as default};
+//# debugId=56c53e32-74a4-5023-97ea-9375e0ec6d56
+//# sourceMappingURL=ja-DngrQVLH.js.map
