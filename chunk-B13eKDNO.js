364619859139cb51e1edd1fec83ef159b2c90d3c
MrYuion <MrYuion@users.noreply.github.com>
2026-09-04 08:37:16 +0000

    build(visitor-kiosk): [2f916a1] CI build

--- chunk-B13eKDNO.js
+++
@@ -1,3 +0,0 @@
-import{n as bn}from"./chunk-zkeT9nqV.js";function h(o,e=`#fff0`,n=`#000`){let t=bn(o,`svg`,{ecc:`low`,border:1});return e&&e!==`#fff0`&&e!==`#0000`&&(t=t.replace(`>`,`><rect width="100%" height="100%" style="fill:${e};"/>`)),t=t.replace(`<path`,`<path style="fill:${n};"`),`data:image/svg+xml,${encodeURIComponent(t)}`}export{h as t};
-//# debugId=18066722-022d-5a60-a671-943915e6ab2b
-//# sourceMappingURL=chunk-B13eKDNO.js.map
