7ce6b404374da291e3e35d82677941dcefa01e6e
MrYuion <MrYuion@users.noreply.github.com>
2026-09-08 01:46:40 +0000

    build(workplace): [3a82dcc] CI build

---
+++ chunk-BCwr9o0z.js
@@ -0,0 +1,3 @@
+import{Ba as m,So as qoe,qo as to}from"./chunk-CEffMQ-E.js";var f=(()=>{class t{constructor(){this._locale=m(qoe)}transform(e,r={},s){return this._locale.get(e,r,s)||e}static{this.ɵfac=function(r){return new(r||t)}}static{this.ɵpipe=to({name:`translate`,type:t,pure:!1})}}return t})();export{f as t};
+//# debugId=88db1f9c-4a6a-59fc-b228-00f54c8b7439
+//# sourceMappingURL=chunk-BCwr9o0z.js.map
