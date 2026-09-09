5fadbb6ad751177ec948ed1cd688b06e30fc25a3
w-le <w-le@users.noreply.github.com>
2026-09-09 15:30:43 +0000

    build(outlook-addin): [custom/HIO687AS][13cacd3] CI build

--- main.js
+++ main.js
@@ -68080,15 +68080,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION4 = {
   "dirty": false,
-  "raw": "09622c2",
-  "hash": "09622c2",
+  "raw": "13cacd3",
+  "hash": "13cacd3",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "09622c2",
+  "suffix": "13cacd3",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1788512919127
+  "time": 1788967807916
 };
 
 // libs/common/src/lib/settings.service.ts
@@ -81570,7 +81570,7 @@ var ORG_CACHE_PREFIX = "PLACEOS.org";
 var ZONE_CACHE_PREFIX = `${ORG_CACHE_PREFIX}.zones`;
 var AUTHORITY_CACHE_KEY = `${ORG_CACHE_PREFIX}.authority`;
 var OFFLINE_BOOT_DELAY = 10 * 1e3;
-var ZONE_LOAD_TIMEOUT = 120 * 1e3;
+var ZONE_LOAD_TIMEOUT = 30 * 1e3;
 var METADATA_CACHE_PREFIX = `${ORG_CACHE_PREFIX}.metadata`;
 var MAX_CACHE_AGE2 = 7 * 24 * 60 * 60 * 1e3;
 function cachedAuthority() {
