82ffbcdb5f0499d200a3ed035397b46dce889077
MrYuion <MrYuion@users.noreply.github.com>
2026-09-10 02:45:29 +0000

    build(visitor-kiosk): [0b0c42d] CI build

--- chunk-RG5WlyMH.js
+++
@@ -1,3 +0,0 @@
-import{n as bn}from"./chunk-BlgTAmP_.js";function h(o,e=`#fff0`,n=`#000`){let t=bn(o,`svg`,{ecc:`low`,border:1});return e&&e!==`#fff0`&&e!==`#0000`&&(t=t.replace(`>`,`><rect width="100%" height="100%" style="fill:${e};"/>`)),t=t.replace(`<path`,`<path style="fill:${n};"`),`data:image/svg+xml,${encodeURIComponent(t)}`}export{h as t};
-//# debugId=309fd335-dbcb-5388-b3a9-dd51cb8fbd8b
-//# sourceMappingURL=chunk-RG5WlyMH.js.map
