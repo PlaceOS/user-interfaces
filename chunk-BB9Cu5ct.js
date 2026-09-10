5ffb53b8b46a3d8e1b02f94a9195fc3d54616698
MrYuion <MrYuion@users.noreply.github.com>
2026-09-10 02:45:29 +0000

    build(visitor-kiosk): [0b0c42d] CI build

---
+++ chunk-BB9Cu5ct.js
@@ -0,0 +1,3 @@
+import{n as bn}from"./chunk-Db_1oFcf.js";function h(o,e=`#fff0`,n=`#000`){let t=bn(o,`svg`,{ecc:`low`,border:1});return e&&e!==`#fff0`&&e!==`#0000`&&(t=t.replace(`>`,`><rect width="100%" height="100%" style="fill:${e};"/>`)),t=t.replace(`<path`,`<path style="fill:${n};"`),`data:image/svg+xml,${encodeURIComponent(t)}`}export{h as t};
+//# debugId=1d2b7390-b8fb-524d-b777-38d1d88f613b
+//# sourceMappingURL=chunk-BB9Cu5ct.js.map
