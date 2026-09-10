b46542c88d46b50cbb12d438174b85def78b1c7f
MrYuion <MrYuion@users.noreply.github.com>
2026-09-10 03:17:36 +0000

    build(assistant-panel): [custom/HIO687AS][7db136d] CI build

--- main.js
+++ main.js
@@ -53445,15 +53445,15 @@ setTimeout(() => initialiseUser(), 50);
 // libs/common/src/lib/version.ts
 var VERSION3 = {
   "dirty": false,
-  "raw": "d0873ae",
-  "hash": "d0873ae",
+  "raw": "7db136d",
+  "hash": "7db136d",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "d0873ae",
+  "suffix": "7db136d",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1789008330306
+  "time": 1789010241320
 };
 
 // libs/common/src/lib/settings.service.ts
