f97d963cb7cfd1f030848371da63aab46b91aee0
MrYuion <MrYuion@users.noreply.github.com>
2026-09-10 02:33:44 +0000

    build(workplace): [custom/HIO687AS][c6df0cb] CI build

---
+++ book.routes-33APKJ5O.js
@@ -0,0 +1,71 @@
+import "./chunk-653SOEEV.js";
+
+// apps/workplace/src/app/book/book.routes.ts
+var canDeactivateVisitorFlow = (component) => component.canDeactivate();
+var ROUTES = [
+  {
+    path: "",
+    loadComponent: () => import("./book.component-Y2IBMKQI.js").then((m) => m.BookComponent),
+    children: [
+      {
+        path: "code",
+        loadComponent: () => import("./code-flow.component-2RGI43AB.js").then((m) => m.BookCodeFlowComponent),
+        title: "Code Booking"
+      },
+      {
+        path: "code/success",
+        loadComponent: () => import("./code-flow-success.component-MBC4CSXT.js").then((m) => m.CodeFlowSuccessComponent),
+        title: "Booking Confirmed"
+      },
+      {
+        path: "code/error",
+        loadComponent: () => import("./code-flow-error.component-XJFMMCIP.js").then((m) => m.CodeFlowErrorComponent),
+        title: "Booking Error"
+      },
+      { path: "desk", redirectTo: "desk/form" },
+      { path: "desks", redirectTo: "desk/form" },
+      {
+        path: "desk/:step",
+        loadComponent: () => import("./desk-flow.component-MKEHUJRO.js").then((m) => m.DeskFlowNewComponent),
+        title: "Desk Booking"
+      },
+      { path: "meeting", redirectTo: "meeting/form" },
+      {
+        path: "meeting/:step",
+        loadComponent: () => import("./meeting-flow.component-GDCY7QU2.js").then((m) => m.MeetingFlowNewComponent),
+        title: "Meeting Booking"
+      },
+      { path: "parking", redirectTo: "parking/form" },
+      {
+        path: "parking/:step",
+        loadComponent: () => import("./parking-flow.component-5PSUS7PE.js").then((m) => m.NewParkingFlowComponent),
+        title: "Parking Booking"
+      },
+      { path: "parking-request", redirectTo: "parking-request/form" },
+      {
+        path: "parking-request/:step",
+        loadComponent: () => import("./parking-request-flow.component-NM7HLKRP.js").then((m) => m.ParkingRequestFlowComponent),
+        title: "Parking Request"
+      },
+      { path: "visitor", redirectTo: "visitor/form" },
+      {
+        path: "visitor/:step",
+        loadComponent: () => import("./visitor-flow.component-CKUK22DY.js").then((m) => m.VisitorFlowNewComponent),
+        title: "Visitor Booking",
+        canDeactivate: [canDeactivateVisitorFlow]
+      },
+      { path: "locker", redirectTo: "locker/form" },
+      {
+        path: "locker/:step",
+        loadComponent: () => import("./locker-flow.component-PTM33DLA.js").then((m) => m.BookLockerFlowComponent),
+        title: "Locker Booking"
+      },
+      { path: "*", redirectTo: "meeting/form" }
+    ]
+  },
+  { path: "*", redirectTo: "meeting/form" }
+];
+export {
+  ROUTES
+};
+//# sourceMappingURL=book.routes-33APKJ5O.js.map
