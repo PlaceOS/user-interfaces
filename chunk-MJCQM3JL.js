23169103bd0d3bf5c5cf622a665cd96e07cd0b5a
w-le <w-le@users.noreply.github.com>
2026-09-18 10:40:57 +0000

    build(visitor-kiosk): [feat/HIO687AS-google-analytics][2d4d31e] CI build

---
+++ chunk-MJCQM3JL.js
@@ -0,0 +1,20 @@
+import {
+  encodeQR
+} from "./chunk-SCRIU3HN.js";
+
+// libs/common/src/lib/qr-code.ts
+function generateQRCode(code, colorLight = "#fff0", colorDark = "#000") {
+  let svg = encodeQR(code, "svg", { ecc: "low", border: 1 });
+  if (colorLight && colorLight !== "#fff0" && colorLight !== "#0000") {
+    svg = svg.replace(">", `><rect width="100%" height="100%" style="fill:${colorLight};"/>`);
+  }
+  svg = svg.replace("<path", `<path style="fill:${colorDark};"`);
+  const encoded_svg = encodeURIComponent(svg);
+  return `data:image/svg+xml,${encoded_svg}`;
+}
+
+export {
+  generateQRCode
+};
+//# debugId=b4961b74-24d4-5655-8c2a-af9fc26634bb
+//# sourceMappingURL=chunk-MJCQM3JL.js.map
