1d0279882364b02b69a8a3e099a1d1fa9ff11639
MrYuion <MrYuion@users.noreply.github.com>
2026-09-23 00:07:51 +0000

    build(visitor-kiosk): [2f504d0] CI build

---
+++ chunk-Cnnhwedy.js
@@ -0,0 +1,3 @@
+import{n as bn}from"./chunk-C6I4UTbD.js";function h(o,e=`#fff0`,n=`#000`){let t=bn(o,`svg`,{ecc:`low`,border:1});return e&&e!==`#fff0`&&e!==`#0000`&&(t=t.replace(`>`,`><rect width="100%" height="100%" style="fill:${e};"/>`)),t=t.replace(`<path`,`<path style="fill:${n};"`),`data:image/svg+xml,${encodeURIComponent(t)}`}export{h as t};
+//# debugId=ed33bf14-9166-5730-8ae2-75cd925727a9
+//# sourceMappingURL=chunk-Cnnhwedy.js.map
