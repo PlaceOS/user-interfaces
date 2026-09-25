b5838e6fc185f37fa4e2833e5b1a4eb40529e52e
camreeves <camreeves@users.noreply.github.com>
2026-09-25 03:03:33 +0000

    build(visitor-kiosk): [b60a017] CI build

---
+++ chunk-oxl_c9-j.js
@@ -0,0 +1,3 @@
+import{n as bn}from"./chunk-jf-5EZAo.js";function h(o,e=`#fff0`,n=`#000`){let t=bn(o,`svg`,{ecc:`low`,border:1});return e&&e!==`#fff0`&&e!==`#0000`&&(t=t.replace(`>`,`><rect width="100%" height="100%" style="fill:${e};"/>`)),t=t.replace(`<path`,`<path style="fill:${n};"`),`data:image/svg+xml,${encodeURIComponent(t)}`}export{h as t};
+//# debugId=0ac5f309-5c3a-52a1-997f-eb561403c74a
+//# sourceMappingURL=chunk-oxl_c9-j.js.map
